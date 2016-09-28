59a09ab67292acc5509af97cb8d061e785374632
Pierre-Antoine <brameret@systerel.fr>
2016-09-28 19:07:18 +0200

    Update test crypto -> helpers

--- test/ingopcs/check_crypto.c
+++ test/ingopcs/check_crypto.c
@@ -13,9 +13,10 @@
 #include <stdio.h>
 
 
+// Helper
 // You should allocate strlen(src)*2 in dst. n is strlen(src)
 // Returns n the number of translated chars (< 0 for errors)
-int hexlify(const unsigned char *src, size_t n, char *dst)
+int hexlify(const unsigned char *src, char *dst, size_t n)
 {
     size_t i;
 
@@ -28,9 +29,10 @@ int hexlify(const unsigned char *src, size_t n, char *dst)
     return n;
 }
 
+// Helper
 // You should allocate strlen(src)/2 in dst. n is strlen(dst)
 // Returns n the number of translated couples (< 0 for errors)
-int unhexlify(const char *src, size_t n, unsigned char *dst)
+int unhexlify(const char *src, unsigned char *dst, size_t n)
 {
     size_t i;
 
@@ -56,15 +58,15 @@ START_TEST(test_hexlify)
     for(i=0; i<256; ++i)
     {
         c = (unsigned char)i;
-        ck_assert(hexlify(&c, 1, (char *)buf) == 1);
-        ck_assert(unhexlify((char *)buf, 1, &d) == 1);
+        ck_assert(hexlify(&c, (char *)buf, 1) == 1);
+        ck_assert(unhexlify((char *)buf, &d, 1) == 1);
         ck_assert(c == d);
     }
 
     // Test vector
-    ck_assert(hexlify((unsigned char *)"\x00 Test \xFF", 8, (char *)buf) == 8);
+    ck_assert(hexlify((unsigned char *)"\x00 Test \xFF", (char *)buf, 8) == 8);
     ck_assert(strncmp((char *)buf, "00205465737420ff", 16) == 0);
-    ck_assert(unhexlify((char *)buf, 8, buf+16) == 8);
+    ck_assert(unhexlify((char *)buf, buf+16, 8) == 8);
     ck_assert(strncmp((char *)(buf+16), "\x00 Test \xFF", 8) == 0);
 }
 END_TEST
@@ -92,52 +94,52 @@ START_TEST(test_aes)
     memset(hexoutput, 0, sizeof(hexoutput));
     mbedtls_aes_setkey_enc(&aes, key, 256);
     mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, 16, iv, input, output);
-    ck_assert(hexlify(output, 16, hexoutput) == 16);
+    ck_assert(hexlify(output, hexoutput, 16) == 16);
     ck_assert(memcmp(hexoutput, "dc95c078a2408989ad48a21492842087", 32) == 0);
 
-    ck_assert(unhexlify("c47b0294dbbbee0fec4757f22ffeee3587ca4730c3d33b691df38bab076bc558", 32, key) == 32);
+    ck_assert(unhexlify("c47b0294dbbbee0fec4757f22ffeee3587ca4730c3d33b691df38bab076bc558", key, 32) == 32);
     memset(iv, 0, sizeof(iv));
     memset(input, 0, sizeof(input));
     memset(output, 0, sizeof(output));
     memset(hexoutput, 0, sizeof(hexoutput));
     mbedtls_aes_setkey_enc(&aes, key, 256);
     mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, 16, iv, input, output);
-    ck_assert(hexlify(output, 16, hexoutput) == 16);
+    ck_assert(hexlify(output, hexoutput, 16) == 16);
     ck_assert(memcmp(hexoutput, "46f2fb342d6f0ab477476fc501242c5f", 32) == 0);
 
-    ck_assert(unhexlify("ccd1bc3c659cd3c59bc437484e3c5c724441da8d6e90ce556cd57d0752663bbc", 32, key) == 32);
+    ck_assert(unhexlify("ccd1bc3c659cd3c59bc437484e3c5c724441da8d6e90ce556cd57d0752663bbc", key, 32) == 32);
     memset(iv, 0, sizeof(iv));
     memset(input, 0, sizeof(input));
     memset(output, 0, sizeof(output));
     memset(hexoutput, 0, sizeof(hexoutput));
     mbedtls_aes_setkey_enc(&aes, key, 256);
     mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, 16, iv, input, output);
-    ck_assert(hexlify(output, 16, hexoutput) == 16);
+    ck_assert(hexlify(output, hexoutput, 16) == 16);
     ck_assert(memcmp(hexoutput, "304f81ab61a80c2e743b94d5002a126b", 32) == 0);
 
     memset(key, 0, sizeof(key));
     memset(iv, 0, sizeof(iv));
-    ck_assert(unhexlify("0b24af36193ce4665f2825d7b4749c98", 16, input) == 16);
+    ck_assert(unhexlify("0b24af36193ce4665f2825d7b4749c98", input, 16) == 16);
     memset(output, 0, sizeof(output));
     mbedtls_aes_setkey_enc(&aes, key, 256);
     mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, 16, iv, input, output);
-    ck_assert(hexlify(output, 16, hexoutput) == 16);
+    ck_assert(hexlify(output, hexoutput, 16) == 16);
     ck_assert(memcmp(hexoutput, "a9ff75bd7cf6613d3731c77c3b6d0c04", 32) == 0);
 
     // Decrypt
-    ck_assert(unhexlify("28d46cffa158533194214a91e712fc2b45b518076675affd910edeca5f41ac64", 32, key) == 32);
+    ck_assert(unhexlify("28d46cffa158533194214a91e712fc2b45b518076675affd910edeca5f41ac64", key, 32) == 32);
     memset(iv, 0, sizeof(iv));
-    ck_assert(unhexlify("4bf3b0a69aeb6657794f2901b1440ad4", 16, input) == 16);
+    ck_assert(unhexlify("4bf3b0a69aeb6657794f2901b1440ad4", input, 16) == 16);
     memset(output, 0, sizeof(output));
     mbedtls_aes_setkey_dec(&aes, key, 256);
     mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, 16, iv, input, output);
-    ck_assert(hexlify(output, 16, hexoutput) == 16);
+    ck_assert(hexlify(output, hexoutput, 16) == 16);
     for(i=0; i<16; ++i)
         ck_assert(output[i] == 0);
 
-    ck_assert(unhexlify("07eb03a08d291d1b07408bf3512ab40c91097ac77461aad4bb859647f74f00ee", 32, key) == 32);
+    ck_assert(unhexlify("07eb03a08d291d1b07408bf3512ab40c91097ac77461aad4bb859647f74f00ee", key, 32) == 32);
     memset(iv, 0, sizeof(iv));
-    ck_assert(unhexlify("47cb030da2ab051dfc6c4bf6910d12bb", 16, input) == 16);
+    ck_assert(unhexlify("47cb030da2ab051dfc6c4bf6910d12bb", input, 16) == 16);
     memset(output, 0, sizeof(output));
     mbedtls_aes_setkey_dec(&aes, key, 256);
     mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, 16, iv, input, output);
@@ -146,11 +148,11 @@ START_TEST(test_aes)
 
     memset(key, 0, sizeof(key));
     memset(iv, 0, sizeof(iv));
-    ck_assert(unhexlify("623a52fcea5d443e48d9181ab32c7421", 16, input) == 16);
+    ck_assert(unhexlify("623a52fcea5d443e48d9181ab32c7421", input, 16) == 16);
     memset(output, 0, sizeof(output));
     mbedtls_aes_setkey_dec(&aes, key, 256);
     mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, 16, iv, input, output);
-    ck_assert(hexlify(output, 16, hexoutput) == 16);
+    ck_assert(hexlify(output, hexoutput, 16) == 16);
     ck_assert(memcmp(hexoutput, "761c1fe41a18acf20d241650611d90f1", 32) == 0);
 }
 END_TEST
