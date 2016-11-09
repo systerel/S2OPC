55487db44b7a5bbfcb01dcefdc2c6fd365287a33
monfort <vincent.monfort@systerel.fr>
2016-11-09 18:13:35 +0100

    Tests: modify test-all to avoid LOCAL param and make clean on start (to keep stub_server copied)

--- test-all.sh
+++ test-all.sh
@@ -6,27 +6,8 @@
 #  Use "LOCAL" as first argument to run tests locally
 #
 
-set -e
-
-DOCKER_IMAGE=c706f497db33
-ISLOCAL=$1
-
-mid() {
-if [[ -z $ISLOCAL || $ISLOCAL != "LOCAL" ]]; then
-    sudo /etc/scripts/make-in-docker $DOCKER_IMAGE CC=gcc "$@"
-else
-    make "$@"
-fi
-}
-
-# Build and run tests
-if [[ -z $ISLOCAL || $ISLOCAL != "LOCAL" ]]; then
-    mid cleanall all
-else
-    mid clean all
-fi
 # run unit tests
-mid check CK_TAP_LOG_FILE_NAME=results.tap
+make check CK_TAP_LOG_FILE_NAME=results.tap
 # run client server tests
-mid client_server_test
+make client_server_test
 
