479987c87a2df46b6f40d9bef3cf430675b0da97
monfort <vincent.monfort@systerel.fr>
2016-12-06 09:54:05 +0100

    Delivery script: modify delivery script to generate the delivery branch and archive

--- delivery.sh
+++ delivery.sh
@@ -4,22 +4,27 @@
 #
 
 DELIVERY_NAME=$1
-LOCAL_REPO=`pwd`
-REPO_NAME=$(basename $LOCAL_REPO)
-echo $LOCAL_REPO
-echo $REPO_NAME
 
 if [[ -z $DELIVERY_NAME ]]; then
     echo "Provide the delivery name for which a git branch must exist"
     exit 1
 fi
 
+echo "Checking out master_stack_ingopcs branch"
+git co master_stack_ingopcs
+echo "Creation of $DELIVERY_NAME branch"
+git co -b $DELIVERY_NAME
+echo "Generation of documentation"
+doxygen doxygen/ingopcs-stack.doxyfile
+git add -f apidoc
+git commit -m "Add doxygen documentation for version $DELIVERY_NAME"
+echo "Generation of archive of version $DELIVERY_NAME"
 git archive --prefix=$DELIVERY_NAME/ -o $DELIVERY_NAME.tar.gz $DELIVERY_NAME
 
 if [ $? -eq 0 ]; then
     echo "=============================================================="
     echo "Creation of delivery archive '$DELIVERY_NAME.tar.gz' succeeded"
-    rm -rf $DELIVERY_NAME
+    echo "Please tag the $DELIVERY_NAME branch on bare repository"
 else
     echo "==========================================================="
     echo "Creation of delivery archive '$DELIVERY_NAME.tar.gz' failed"
