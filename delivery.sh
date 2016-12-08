5b3c0162fb34dc0fdc5d8114ee1cef97e05711d3
monfort <vincent.monfort@systerel.fr>
2016-12-08 18:01:43 +0100

    Delivery script: exit in case of errors during execution

--- delivery.sh
+++ delivery.sh
@@ -11,15 +11,15 @@ if [[ -z $DELIVERY_NAME ]]; then
 fi
 
 echo "Checking out master_stack_ingopcs branch"
-git co master_stack_ingopcs
+git co master_stack_ingopcs || exit 1
 echo "Creation of $DELIVERY_NAME branch"
-git co -b $DELIVERY_NAME
+git co -b $DELIVERY_NAME || exit 1
 echo "Generation of documentation"
-doxygen doxygen/ingopcs-stack.doxyfile
-git add -f apidoc
-git commit -m "Add doxygen documentation for version $DELIVERY_NAME"
+doxygen doxygen/ingopcs-stack.doxyfile || exit 1
+git add -f apidoc || exit 1
+git commit -m "Add doxygen documentation for version $DELIVERY_NAME" || exit 1
 echo "Generation of archive of version $DELIVERY_NAME"
-git archive --prefix=$DELIVERY_NAME/ -o $DELIVERY_NAME.tar.gz $DELIVERY_NAME
+git archive --prefix=$DELIVERY_NAME/ -o $DELIVERY_NAME.tar.gz $DELIVERY_NAME || exit 1
 
 if [ $? -eq 0 ]; then
     echo "=============================================================="
@@ -28,5 +28,5 @@ if [ $? -eq 0 ]; then
 else
     echo "==========================================================="
     echo "Creation of delivery archive '$DELIVERY_NAME.tar.gz' failed"
-    rm -f $DELIVERY_NAME.tar.gz
+    rm -f $DELIVERY_NAME.tar.gz || exit 1
 fi
