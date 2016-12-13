1b5e8967792855af6fb540c4479396003c7c21f6
monfort <vincent.monfort@systerel.fr>
2016-12-13 16:05:50 +0100

    Delivery script: add parameter for branch/commit

--- delivery.sh
+++ delivery.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 #
 #  Script to produce stack delivery archive
-#
+#  $1 must be the branch/commit to use for generating the delivery
+#  $2 must be the delivery name (e.g.: INGOPCS_Stack_1.0)
 
-DELIVERY_NAME=$1
+BRANCH_COMMIT=$1
+DELIVERY_NAME=$2
+
+if [[ -z $BRANCH_COMMIT ]]; then
+    echo "Provide the branch name or commit reference to use for generating the delivery"
+    exit 1
+fi
 
 if [[ -z $DELIVERY_NAME ]]; then
     echo "Provide the delivery name for which a git branch must exist"
     exit 1
 fi
 
-echo "Checking out master_stack_ingopcs branch"
-git co master_stack_ingopcs || exit 1
+echo "Checking out $BRANCH_COMMIT"
+git co $BRANCH_COMMIT &> /dev/null || exit 1
 echo "Creation of $DELIVERY_NAME branch"
 git co -b $DELIVERY_NAME || exit 1
-echo "Generation of documentation"
-doxygen doxygen/ingopcs-stack.doxyfile || exit 1
-git add -f apidoc || exit 1
-git commit -m "Add doxygen documentation for version $DELIVERY_NAME" || exit 1
+echo "Generation of documentation with doxygen"
+doxygen doxygen/ingopcs-stack.doxyfile &> /dev/null || exit 1
+echo "Add documentation in delivery branch"
+git add -f apidoc &> /dev/null || exit 1
+git commit -m "Add doxygen documentation for version $DELIVERY_NAME" &> /dev/null || exit 1
 echo "Generation of archive of version $DELIVERY_NAME"
 git archive --prefix=$DELIVERY_NAME/ -o $DELIVERY_NAME.tar.gz $DELIVERY_NAME || exit 1
 
