ccf2e5f85b1e29315ac931a20207b7359674f573
monfort <vincent.monfort@systerel.fr>
2017-01-16 10:51:22 +0100

    Hello msg treatment: server accepts any URL provided but record it for further verification

--- src/stack_core/sopc_tcp_ua_connection.c
+++ src/stack_core/sopc_tcp_ua_connection.c
@@ -346,13 +346,14 @@ SOPC_StatusCode ReceiveHelloMsg(TCP_UA_Connection* connection){
             if(STATUS_OK == status){
                 status = SOPC_String_Read(&url, connection->inputMsgBuffer);
                 if(STATUS_OK == status){
-                    int32_t urlCompare = -1;
-                    //TODO: check URL need to check for equivalent services ?
-                    if(url.Length > TCP_UA_MAX_URL_LENGTH ||
-                       STATUS_OK != SOPC_String_Compare(&url, &connection->url, 1, &urlCompare) ||
-                       urlCompare != 0)
+                    if(url.Length > TCP_UA_MAX_URL_LENGTH)
                     {
                         status = STATUS_INVALID_RCV_PARAMETER; // TcpEndpointUrlInvalid
+                    }else{
+                        // TODO: to be checked on secure channel connection that it corresponds
+                        // to server certificate declared URL(s)
+                        SOPC_String_Clear(&connection->url);
+                        status = SOPC_String_Copy(&connection->url, &url);
                     }
                     SOPC_String_Clear(&url);
                 }
