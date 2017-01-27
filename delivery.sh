6e0d198626ec461de84bfc68718d354cc823b1db
monfort <vincent.monfort@systerel.fr>
2017-01-27 11:35:10 +0100

    Delivery script: check version number and branch, add VERSION file, remove script in delivery

--- delivery.sh
+++ delivery.sh
@@ -2,18 +2,44 @@
 #
 #  Script to produce stack delivery archive
 #  $1 must be the branch/commit to use for generating the delivery
-#  $2 must be the delivery name (e.g.: INGOPCS_Stack_1.0)
+#  $2 must be the delivery version number (e.g.: 1.0.0)
 
+echo "Check branch name is valid (1st param): $1"
+git show-ref refs/heads/$1 &> /dev/null
+if [[ $? != 0 ]]; then
+    echo "Provide the LOCAL branch name or commit reference to use for generating the delivery"
+    exit 1
+fi
 BRANCH_COMMIT=$1
-DELIVERY_NAME=$2
 
 if [[ -z $BRANCH_COMMIT ]]; then
-    echo "Provide the branch name or commit reference to use for generating the delivery"
     exit 1
 fi
 
-if [[ -z $DELIVERY_NAME ]]; then
-    echo "Provide the delivery name for which a git branch must exist"
+echo "Check version number is correct: $2"
+regexp='^[0-9]+\.[0-9]+.[0-9]+$'
+if ! [[ $2 =~ $regexp ]] ; then
+   echo "Error: '$2' is not correct version number X.Y.Z";
+   exit 1
+fi
+DELIVERY_NAME=INGOPCS_Stack_$2
+
+echo "Check branch and tag does not exist: $DELIVERY_NAME"
+git show-ref refs/heads/$DELIVERY_NAME &> /dev/null
+if [[ $? == 0 ]]; then
+    echo "Error: refs/heads/$DELIVERY_NAME already exists";
+    exit 1
+fi
+git show-ref refs/tags/$DELIVERY_NAME &> /dev/null
+if [[ $? == 0 ]]; then
+    echo "Error: refs/tags/$DELIVERY_NAME already exists";
+    exit 1
+fi
+
+echo "Check archive does not exist: $DELIVERY_NAME.tar.gz"
+ls $DELIVERY_NAME.tar.gz &> /dev/null
+if [[ $? == 0 ]]; then
+    echo "Error: $DELIVERY_NAME.tar.gz already exists";
     exit 1
 fi
 
@@ -21,11 +47,18 @@ echo "Checking out $BRANCH_COMMIT"
 git co $BRANCH_COMMIT &> /dev/null || exit 1
 echo "Creation of $DELIVERY_NAME branch"
 git co -b $DELIVERY_NAME || exit 1
-echo "Generation of documentation with doxygen"
+echo "Generate and commit version file 'VERSION' with '$2' content"
+echo "$2" > VERSION
+git add VERSION &> /dev/null || exit 1
+git commit -m "Add VERSION file" &> /dev/null || exit 1
+echo "Generate documentation with doxygen"
 doxygen doxygen/ingopcs-stack.doxyfile &> /dev/null || exit 1
 echo "Add documentation in delivery branch"
 git add -f apidoc &> /dev/null || exit 1
 git commit -m "Add doxygen documentation for version $DELIVERY_NAME" &> /dev/null || exit 1
+echo "Remove delivery script, .gitignore file and commit"
+git rm delivery.sh .gitignore &> /dev/null || exit 1
+git commit -m "Remove delivery script and .gitignore file" &> /dev/null || exit 1
 echo "Generation of archive of version $DELIVERY_NAME"
 git archive --prefix=$DELIVERY_NAME/ -o $DELIVERY_NAME.tar.gz $DELIVERY_NAME || exit 1
 
