625e26198563c3636f7e5b2c8f2f1fcf5ba79f64
Vincent Lacroix <vincent.lacroix@systerel.fr>
2017-09-12 11:20:04 +0200

    Ticket #298: CryptoTests: Bugfix client.py

--- validation/safety_secure_channels.py
+++ validation/safety_secure_channels.py
@@ -4,6 +4,11 @@ from opcua.crypto import security_policies
 
 def safety_secure_channels_test(client):
   #client.set_security_string("Basic256,Sign,trusted/cacert.der,client_private/client.key")
-  client.set_security(security_policies.SecurityPolicyBasic256,'../bin/trusted/cacert.der','../bin/client_private/client.pem','../bin/server_public/server.der',ua.MessageSecurityMode.Sign)
+  client.set_security(security_policies.SecurityPolicyBasic256,
+                      #'../bin/trusted/cacert.der',
+                      '../bin/client_public/client.der',
+                      '../bin/client_private/client.pem',
+                      server_certificate_path='../bin/server_public/server.der',
+                      mode=ua.MessageSecurityMode.Sign)
   client.connect()
 
