0fb443354bfdd266a61f8f2b5cf56db9fe93f4a3
Pierre-Antoine <brameret@systerel.fr>
2017-09-12 15:09:38 +0200

    Ticket #298: CryptoTests: client.py now prints some Values

--- validation/client.py
+++ validation/client.py
@@ -5,7 +5,7 @@
 Simple client to launch validation tests
 """
 
-from opcua import Client
+from opcua import ua, Client
 from attribute_read import attribute_read_tests
 from attribute_write_values import attribute_write_values_tests
 from safety_secure_channels import safety_secure_channels_test
@@ -49,4 +49,12 @@ if __name__=='__main__':
 
     # test secure connexions
     safety_secure_channels_test(client)
+    print('Connected')
+
+    try:
+        for nid in [20, 270, 520, 770]:
+            print('  Node {:03d}:'.format(nid), client.get_node(nid).get_data_value())
+    finally:
+        client.disconnect()
+        print('Disconnected')
 
