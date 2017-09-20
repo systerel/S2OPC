de84a8316a4dd9530bc91fc3e6b226ee036ca387
Pierre-Antoine <brameret@systerel.fr>
2017-09-20 19:08:51 +0200

    Ticket #293: Browse: validation script with Browse request

--- validation/client.py
+++ validation/client.py
@@ -15,7 +15,7 @@ if __name__=='__main__':
     endPoints = client.get_endpoints()
     print('endPoints:', endPoints)
     # Read tests
-    n1 = client.get_node(10)
+    n1 = client.get_node("ns=261;s=Objects.15361.SIGNALs.BALA_RDLS_G019.RM")
     print('n1:', n1.get_value())
     browse_name = n1.get_browse_name()
     print('browse_name: ', browse_name)
@@ -23,10 +23,14 @@ if __name__=='__main__':
     print('display_name: ', display_name)
     class_name = n1.get_node_class()
     print('node_class: ', class_name)
-    n2 = client.get_node(11)
-    print('n2:', n2.get_value())
+    #n2 = client.get_node(11)
+    #print('n2:', n2.get_value())
+    # browse tests
+    print('children:')
+    for n in n1.get_children():
+        print('  ' + str(n.nodeid))
     # write tests
-    n1.set_value(ua.Variant(23, ua.VariantType.Int64))
-    print('n1:', n1.get_value())
+    #n1.set_value(ua.Variant(23, ua.VariantType.Int64))
+    #print('n1:', n1.get_value())
     client.disconnect()
 
