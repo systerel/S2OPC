c21753a7404b784941c0fa5869aae3279b2c03fe
monfort <vincent.monfort@systerel.fr>
2017-09-25 16:56:36 +0200

    Ticket #296: Validation script: update to add write test in addition to browse

--- validation/client.py
+++ validation/client.py
@@ -16,7 +16,6 @@ if __name__=='__main__':
     print('endPoints:', endPoints)
     # Read tests
     n1 = client.get_node("ns=261;s=Objects.15361.SIGNALs.BALA_RDLS_G019.RM")
-    print('n1:', n1.get_value())
     browse_name = n1.get_browse_name()
     print('browse_name: ', browse_name)
     display_name = n1.get_display_name()
@@ -29,8 +28,19 @@ if __name__=='__main__':
     print('children:')
     for n in n1.get_children():
         print('  ' + str(n.nodeid))
+    # read tests
+    n1 = client.get_node("ns=261;s=Objects.15361.SIGNALs.BALA_RDLS_G019.RM.ASK")
+    print('n1:', n1.get_value())
+    browse_name = n1.get_browse_name()
+    print('browse_name: ', browse_name)
+    display_name = n1.get_display_name()
+    print('display_name: ', display_name)
+    class_name = n1.get_node_class()
+    print('node_class: ', class_name)      
     # write tests
-    #n1.set_value(ua.Variant(23, ua.VariantType.Int64))
-    #print('n1:', n1.get_value())
+    n1.set_value(ua.Variant(True, ua.VariantType.Boolean))
+    print('n1:', n1.get_value())
+    n1.set_value(ua.Variant(False, ua.VariantType.Boolean))
+    print('n1:', n1.get_value())
     client.disconnect()
 
