aff87e72237a278cc02d8e86a7992c491b533830
monfort <vincent.monfort@systerel.fr>
2017-10-02 14:29:10 +0200

    Add Copyrights

--- toolkit/src/test/b2c.h
+++ toolkit/src/test/b2c.h
@@ -1,22 +1,20 @@
-/*?***************************************************************************
+/*
+ *  Copyright (C) 2017 Systerel and others.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
- Fichier : b2c.h
-
- Auteur(s) : F. Badeau (Systerel)
-
- Date de creation : 25/09/2009
-
- Version de creation : V1
-
- ******************************************************************************
-
- FONCTIONS :
-
- DESCRIPTION :
- Parties communes pour le traducteur C et diversification pour les operateurs
- elementaires
-
- ***************************************************************************?*/
 #ifndef _b2c_h
 #define _b2c_h
 #include <stdint.h>
