750809689d8b6fc123fc1ebf87c48210b777d1dc
monfort <vincent.monfort@systerel.fr>
2017-10-11 15:56:27 +0200

    Ticket #281: fix log generation in build.sh

--- build.sh
+++ build.sh
@@ -9,49 +9,50 @@
 # - build the library and tests
 # - prepare test execution
 
+CURDIR=`pwd`
 BUILD_DIR=build
 EXEC_DIR=bin
 CERT_DIR=tests/data/cert
 
-echo "Build log" > build.log
+echo "Build log" > $CURDIR/build.log
 
-echo "Build the library and tests with CMake" | tee -a build.log
+echo "Build the library and tests with CMake" | tee -a $CURDIR/build.log
 if [ -f "$BUILD_DIR/CMakeCache.txt" ]; then
-    echo "- CMake already configured" | tee -a build.log
+    echo "- CMake already configured" | tee -a $CURDIR/build.log
 else
-    echo "- Generate ./build directory" | tee -a build.log
+    echo "- Generate ./build directory" | tee -a $CURDIR/build.log
     mkdir -p $BUILD_DIR || exit 1
     cd $BUILD_DIR  > /dev/null || exit 1
-    echo "- Run CMake"
+    echo "- Run CMake" | tee -a $CURDIR/build.log
     if [[ $CROSS_COMPILE_MINGW ]]; then
-        cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-mingw32-w64.cmake .. >> build.log
+        cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain-mingw32-w64.cmake .. >> $CURDIR/build.log
     else
-        cmake .. >> build.log
+        cmake .. >> $CURDIR/build.log
     fi
     cd - > /dev/null || exit 1
 fi
 if [[ $? != 0 ]]; then
-    echo "Error: build configuration failed" | tee -a build.log
+    echo "Error: build configuration failed" | tee -a $CURDIR/build.log
     exit 1
 fi
 
-echo "- Run make" | tee -a build.log
-make -C $BUILD_DIR >> build.log
+echo "- Run make" | tee -a $CURDIR/build.log
+make -C $BUILD_DIR >> $CURDIR/build.log
 if [[ $? != 0 ]]; then
-    echo "Error: build failed" | tee -a build.log
+    echo "Error: build failed" | tee -a $CURDIR/build.log
     exit 1
 else
-    echo "Built library and tests with success" | tee -a build.log
+    echo "Built library and tests with success" | tee -a $CURDIR/build.log
 fi
 
-echo "- Run make test" | tee -a build.log
-make -C $BUILD_DIR test >> build.log
+echo "- Run make test" | tee -a $CURDIR/build.log
+make -C $BUILD_DIR test >> $CURDIR/build.log
 if [[ $? != 0 ]]; then
-    echo "Error: test failed" | tee -a build.log
+    echo "Error: test failed" | tee -a $CURDIR/build.log
     exit 1
 fi
 
 if [[ $? == 0 ]]; then
-    echo "Terminated with SUCCESS" | tee -a build.log
+    echo "Terminated with SUCCESS" | tee -a $CURDIR/build.log
     exit 0
 fi
