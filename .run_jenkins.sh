a985cda4369cf8d86ff258ab216057e455adecbf
monfort <vincent.monfort@systerel.fr>
2017-10-24 18:14:52 +0200

    Ticket #307: Update jenkins script

--- .run_jenkins.sh
+++ .run_jenkins.sh
@@ -8,6 +8,7 @@ git clean -fd
 
 # run build and tests
 export SOPC_DOCKER_NEEDS_SUDO=1
+./clean.sh all && ./.pre-build-in-docker.sh ./pre-build.sh
 ./clean.sh && ./.build-in-docker.sh ./build.sh
 ./.test-in-docker.sh ./test-all.sh
 
