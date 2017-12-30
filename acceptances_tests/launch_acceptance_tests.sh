ee97e7aac1350a99acb2a0686b4f381471e98efa
Vincent Lacroix <vincent.lacroix@systerel.fr>
2017-12-30 19:55:23 +0100

    Ticket #307: Fixup in acceptation tests script

--- acceptances_tests/launch_acceptance_tests.sh
+++ acceptances_tests/launch_acceptance_tests.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-set -x
-
 IFS=$'\n\t'
-DEFAULT_PROJECT_LINE="<project>"
 
 LOG_FILE=server_acceptance_tests.log
 TMP_FILE=`mktemp`
 TAP_FILE=server_acceptance_tests.tap
+#SELECTION=./Acceptation_INGOPCS/Acceptation_INGOPCS_nonreg.selection.xml
+SELECTION=Acceptation_INGOPCS/Acceptation_INGOPCS.selection.xml
 
 rm -f $LOG_FILE $TAP_FILE
 echo -e "Launching Acceptance Test Tool"
 
-/opt/opcfoundation/uactt/uacompliancetest.sh -s ./Acceptation_INGOPCS/Acceptation_INGOPCS.ctt.xml --selection ./Acceptation_INGOPCS/Acceptation_INGOPCS_nonreg.selection.xml -h -c -r ./$LOG_FILE 2>/dev/null
+/opt/opcfoundation/uactt/uacompliancetest.sh -s ./Acceptation_INGOPCS/Acceptation_INGOPCS.ctt.xml --selection $SELECTION -h -c -r ./$LOG_FILE 2>/dev/null
 
 echo -e "End of acceptance tests - Generating Test Report"
 
