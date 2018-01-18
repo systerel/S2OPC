5e81826f952e4fe1c67bd61e74fe96d68c074eaa
Pierre-Antoine <brameret@systerel.fr>
2018-01-18 19:06:37 +0100

    Ticket #307: Update copyrights: Prepend copyright to bgenc files

---
+++ bsrc/prependcopyright.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Systerel and others.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+if [[ -z $1 || -z $2 || ! -f $1 || -f $2 ]] ; then
+    echo Usage: $0 FROM_PATH TO_PATH
+    echo FROM_PATH must exist, TO_PATH must not
+    exit 2
+fi
+
+echo '/*
+ *  Copyright (C) 2018 Systerel and others.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+' > $2
+
+cat $1 >> $2
