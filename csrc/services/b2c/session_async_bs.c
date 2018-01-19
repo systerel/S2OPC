fd68c3b0fbc9603f4a5499060256fbeb13701ae1
monfort <vincent.monfort@systerel.fr>
2018-01-19 16:02:17 +0100

    Ticket #306: fix issues for multiple sessions: do not use a singleton for asynchronous session activation phase

--- csrc/services/b2c/session_async_bs.c
+++ csrc/services/b2c/session_async_bs.c
@@ -21,12 +21,27 @@
 #include "sopc_toolkit_config.h"
 #include "util_b2c.h"
 
-static constants__t_session_i unique_session_to_activate = constants__c_session_indet;
-static constants__t_user_i unique_session_to_activate_user = constants__c_user_indet;
-static constants__t_session_i unique_session_to_create = constants__c_session_indet;
-static constants__t_channel_config_idx_i unique_session_to_create_idx = constants__c_channel_config_idx_indet;
+#include <assert.h>
+#include <string.h>
 
-void session_async_bs__INITIALISATION(void) {}
+static bool session_to_create[SOPC_MAX_SESSIONS + 1];
+static constants__t_channel_config_idx_i session_to_create_idx[SOPC_MAX_SESSIONS + 1];
+static bool session_to_activate[SOPC_MAX_SESSIONS + 1];
+static constants__t_user_i session_to_activate_user[SOPC_MAX_SESSIONS + 1];
+
+void session_async_bs__INITIALISATION(void)
+{
+    assert(SOPC_MAX_SESSIONS + 1 <= SIZE_MAX / sizeof(bool));
+    assert(SOPC_MAX_SESSIONS + 1 <= SIZE_MAX / sizeof(constants__t_channel_config_idx_i));
+    assert(SOPC_MAX_SESSIONS + 1 <= SIZE_MAX / sizeof(constants__t_user_i));
+
+    memset(session_to_create, (int) false, sizeof(bool) * (SOPC_MAX_SESSIONS + 1));
+    memset(session_to_create_idx, (int) constants__c_channel_config_idx_indet,
+           sizeof(constants__t_channel_config_idx_i) * (SOPC_MAX_SESSIONS + 1));
+    memset(session_to_activate, (int) false, sizeof(bool) * (SOPC_MAX_SESSIONS + 1));
+    memset(session_to_activate_user, (int) constants__c_user_indet,
+           sizeof(constants__t_user_i) * (SOPC_MAX_SESSIONS + 1));
+}
 
 /*--------------------
    OPERATIONS Clause
@@ -35,10 +50,11 @@ void session_async_bs__add_session_to_activate(const constants__t_session_i sess
                                                const constants__t_user_i session_async_bs__user,
                                                t_bool* const session_async_bs__ret)
 {
-    if (unique_session_to_activate_user == constants__c_user_indet)
+    if (session_async_bs__session != constants__c_session_indet &&
+        false == session_to_activate[session_async_bs__session])
     {
-        unique_session_to_activate_user = session_async_bs__user;
-        unique_session_to_activate = session_async_bs__session;
+        session_to_activate_user[session_async_bs__session] = session_async_bs__user;
+        session_to_activate[session_async_bs__session] = true;
         *session_async_bs__ret = true;
     }
     else
@@ -52,11 +68,31 @@ void session_async_bs__add_session_to_create(
     const constants__t_channel_config_idx_i session_async_bs__channel_config_idx,
     t_bool* const session_async_bs__ret)
 {
-    if (unique_session_to_create == constants__c_session_indet)
+    bool unique_session_in_channel_config = true;
+    if (session_async_bs__session != constants__c_session_indet &&
+        false == session_to_create[session_async_bs__session])
     {
-        unique_session_to_create_idx = session_async_bs__channel_config_idx;
-        unique_session_to_create = session_async_bs__session;
-        *session_async_bs__ret = true;
+        // Check a session in creation is not already recorded for same channel config index
+        for (constants__t_session_i session = 1;
+             session <= SOPC_MAX_SESSIONS && true == unique_session_in_channel_config; session++)
+        {
+            if (true == session_to_create[session] &&
+                session_async_bs__channel_config_idx == session_to_create_idx[session])
+            {
+                // A session is already waiting on this channel config => not possible with current B model
+                unique_session_in_channel_config = false;
+            }
+        }
+        if (unique_session_in_channel_config != false)
+        {
+            session_to_create_idx[session_async_bs__session] = session_async_bs__channel_config_idx;
+            session_to_create[session_async_bs__session] = true;
+            *session_async_bs__ret = true;
+        }
+        else
+        {
+            *session_async_bs__ret = false;
+        }
     }
     else
     {
@@ -67,10 +103,12 @@ void session_async_bs__add_session_to_create(
 void session_async_bs__get_and_remove_session_user_to_activate(const constants__t_session_i session_async_bs__session,
                                                                constants__t_user_i* const session_async_bs__user)
 {
-    if (session_async_bs__session == unique_session_to_activate)
+    if (session_async_bs__session != constants__c_session_indet &&
+        true == session_to_activate[session_async_bs__session])
     {
-        *session_async_bs__user = unique_session_to_activate_user;
-        unique_session_to_activate_user = constants__c_user_indet;
+        *session_async_bs__user = session_to_activate_user[session_async_bs__session];
+        session_to_activate_user[session_async_bs__session] = constants__c_user_indet;
+        session_to_activate[session_async_bs__session] = false;
     }
     else
     {
@@ -82,14 +120,19 @@ void session_async_bs__get_and_remove_session_to_create(
     const constants__t_channel_config_idx_i session_async_bs__channel_config_idx,
     constants__t_session_i* const session_async_bs__session)
 {
-    if (session_async_bs__channel_config_idx == unique_session_to_create_idx)
+    bool stop = false;
+    *session_async_bs__session = constants__c_session_indet;
+    if (session_async_bs__channel_config_idx != constants__c_channel_config_idx_indet)
     {
-        *session_async_bs__session = unique_session_to_create;
-        unique_session_to_create = constants__c_session_indet;
-    }
-    else
-    {
-        *session_async_bs__session = constants__c_session_indet;
+        for (constants__t_session_i session = 1; session <= SOPC_MAX_SESSIONS && false == stop; session++)
+        {
+            if (true == session_to_create[session] &&
+                session_async_bs__channel_config_idx == session_to_create_idx[session])
+            {
+                *session_async_bs__session = session;
+                stop = true;
+            }
+        }
     }
 }
 
@@ -97,7 +140,6 @@ void session_async_bs__client_gen_activate_orphaned_session_internal_event(
     const constants__t_session_i session_async_bs__session,
     const constants__t_channel_config_idx_i session_async_bs__channel_config_idx)
 {
-    /* TODO: check integer casts */
     SOPC_Services_EnqueueEvent(SE_TO_SE_ACTIVATE_ORPHANED_SESSION, (uint32_t) session_async_bs__session, NULL,
                                (uint32_t) session_async_bs__channel_config_idx);
 }
@@ -106,7 +148,6 @@ void session_async_bs__client_gen_activate_user_session_internal_event(
     const constants__t_session_i session_async_bs__session,
     const constants__t_user_i session_async_bs__user)
 {
-    /* TODO: check integer casts */
     SOPC_Services_EnqueueEvent(SE_TO_SE_ACTIVATE_SESSION, (uint32_t) session_async_bs__session, NULL,
                                (uint32_t) session_async_bs__user);
 }
@@ -115,7 +156,6 @@ void session_async_bs__client_gen_create_session_internal_event(
     const constants__t_session_i session_async_bs__session,
     const constants__t_channel_config_idx_i session_async_bs__channel_config_idx)
 {
-    /* TODO: check integer casts */
     SOPC_Services_EnqueueEvent(SE_TO_SE_CREATE_SESSION, (uint32_t) session_async_bs__session, NULL,
                                (uint32_t) session_async_bs__channel_config_idx);
 }
