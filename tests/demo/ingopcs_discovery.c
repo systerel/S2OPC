ec6fcc2fe87676eab9f71ec897e4ce81e2555c5d
Pierre-Antoine <brameret@systerel.fr>
2018-03-23 11:00:34 +0100

    Ticket #405: Discovery demo prints more information

--- tests/demo/ingopcs_discovery.c
+++ tests/demo/ingopcs_discovery.c
@@ -15,12 +15,14 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <inttypes.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "sopc_builtintypes.h"
+#include "sopc_crypto_provider.h"
 #include "sopc_time.h"
 #include "sopc_toolkit_async_api.h"
 #include "sopc_toolkit_config.h"
@@ -43,6 +45,8 @@ void PrintEndpoints(OpcUa_GetEndpointsResponse* pResp);
 
 int main(int argc, char* argv[])
 {
+    (void) argc;
+    (void) argv;
     SOPC_ReturnStatus status = SOPC_STATUS_OK;
     uint32_t iWait = 0;
 
@@ -124,7 +128,14 @@ void PrintEndpoints(OpcUa_GetEndpointsResponse* pResp)
 {
     int32_t i = 0;
     int32_t j = 0;
+    uint32_t k = 0;
     OpcUa_EndpointDescription* pEndp = NULL;
+    OpcUa_UserTokenPolicy* pPol = NULL;
+    SOPC_ByteString* pBufCert = NULL;
+    SOPC_CryptoProvider* pProvider = NULL;
+    SOPC_Certificate* pCert = NULL;
+    uint32_t lenThmb = 0;
+    char* pThmb = NULL;
 
     if (SOPC_GoodGenericStatus != pResp->ResponseHeader.ServiceResult)
     {
@@ -132,17 +143,116 @@ void PrintEndpoints(OpcUa_GetEndpointsResponse* pResp)
     }
     else
     {
-        printf("Endpoints:\n");
+        printf("%" PRIi32 " endpoints:\n", pResp->NoOfEndpoints);
     }
 
     for (i = 0; i < pResp->NoOfEndpoints; ++i)
     {
         pEndp = &pResp->Endpoints[i];
         printf("- Endpoint %s\n", SOPC_String_GetRawCString(&pEndp->EndpointUrl));
-        printf("  Security policy and mode\n");
-        printf("  AppDescr\n");
-        printf("  CertificateThumbprint\n");
-        printf("  UserIdentityTokens\n");
-        (void) j;
+        printf("  Security policy and mode: %s ", SOPC_String_GetRawCString(&pEndp->SecurityPolicyUri));
+        switch (pEndp->SecurityMode)
+        {
+        default:
+        case OpcUa_MessageSecurityMode_Invalid:
+            printf("OpcUa_MessageSecurityMode_Invalid");
+            break;
+        case OpcUa_MessageSecurityMode_None:
+            printf("OpcUa_MessageSecurityMode_None");
+            break;
+        case OpcUa_MessageSecurityMode_Sign:
+            printf("OpcUa_MessageSecurityMode_Sign");
+            break;
+        case OpcUa_MessageSecurityMode_SignAndEncrypt:
+            printf("OpcUa_MessageSecurityMode_SignAndEncrypt");
+            break;
+        }
+        printf("\n  ApplicationDescription:\n");
+        printf("    ApplicationUri: %s\n    ProductUri: %s\n", SOPC_String_GetRawCString(&pEndp->Server.ApplicationUri),
+               SOPC_String_GetRawCString(&pEndp->Server.ProductUri));
+
+        printf("  CertificateThumbprint: ");
+        pBufCert = &pEndp->ServerCertificate;
+        if (pBufCert->Length <= 0)
+        {
+            printf("<null>\n");
+        }
+        else
+        {
+            pProvider = SOPC_CryptoProvider_Create(SOPC_String_GetRawCString(&pEndp->SecurityPolicyUri));
+            if (NULL == pProvider)
+            {
+                printf("<error creating crypto provider>\n");
+            }
+            else
+            {
+                if (SOPC_KeyManager_Certificate_CreateFromDER(pBufCert->Data, pBufCert->Length, &pCert) !=
+                    SOPC_STATUS_OK)
+                {
+                    printf("<error creating certificate>\n");
+                }
+                else
+                {
+                    if (SOPC_CryptoProvider_CertificateGetLength_Thumbprint(pProvider, &lenThmb) != SOPC_STATUS_OK)
+                    {
+                        printf("<error calculating certificate thumbprint length>\n");
+                    }
+                    else
+                    {
+                        pThmb = calloc(lenThmb + 1, sizeof(char));
+                        if (NULL == pThmb || SOPC_KeyManager_Certificate_GetThumbprint(
+                                                 pProvider, pCert, (uint8_t*) pThmb, lenThmb) != SOPC_STATUS_OK)
+                        {
+                            printf("<failed to compute certificate thumbprint>\n");
+                        }
+                        else
+                        {
+                            for (k = 0; k < lenThmb; ++k)
+                            {
+                                printf("%02" SCNx8, pThmb[k]);
+                                if (k < lenThmb - 1)
+                                {
+                                    printf(":");
+                                }
+                            }
+                            printf("\n");
+                            free(pThmb);
+                            pThmb = NULL;
+                        }
+                    }
+                    SOPC_KeyManager_Certificate_Free(pCert);
+                    pCert = NULL;
+                }
+                SOPC_CryptoProvider_Free(pProvider);
+                pProvider = NULL;
+            }
+        }
+
+        printf("  UserIdentityTokens:\n");
+        for (j = 0; j < pEndp->NoOfUserIdentityTokens; ++j)
+        {
+            pPol = &pEndp->UserIdentityTokens[j];
+            switch (pPol->TokenType)
+            {
+            case OpcUa_UserTokenType_Anonymous:
+                printf("    OpcUa_UserTokenType_Anonymous\n");
+                break;
+            case OpcUa_UserTokenType_UserName:
+                printf("    OpcUa_UserTokenType_UserName\n");
+                break;
+            case OpcUa_UserTokenType_Certificate:
+                printf("    OpcUa_UserTokenType_Certificate\n");
+                break;
+            case OpcUa_UserTokenType_IssuedToken:
+                printf("    OpcUa_UserTokenType_IssuedToken\n");
+                break;
+            case OpcUa_UserTokenType_Kerberos:
+                printf("    OpcUa_UserTokenType_Kerberos\n");
+                break;
+            default:
+                printf("    Invalid/Unrecognized\n");
+                break;
+            }
+        }
     }
 }
