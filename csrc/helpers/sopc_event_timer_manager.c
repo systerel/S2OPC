79a6b13a5d6e3ba738a0e0b198f5ab47c7d9950c
monfort <vincent.monfort@systerel.fr>
2018-04-26 13:29:29 +0200

    Ticket #415: Event timer manager: possible memory illegal access

--- csrc/helpers/sopc_event_timer_manager.c
+++ csrc/helpers/sopc_event_timer_manager.c
@@ -33,7 +33,7 @@ typedef struct SOPC_EventTimer
 
 #define SOPC_MAX_TIMERS UINT16_MAX
 
-static bool usedTimerIds[SOPC_MAX_TIMERS + 1]; // 0 idx value is invalid (max idx = MAX + 1)
+static bool usedTimerIds[SOPC_MAX_TIMERS + 1]; // 0 idx value is invalid (max idx = MAX)
 static uint32_t latestTimerId = 0;
 
 static SOPC_SLinkedList* timers = NULL;
@@ -50,9 +50,9 @@ static uint32_t SOPC_Internal_GetFreshTimerId_WithoutLock(void)
     uint32_t idx = latestTimerId;
     if (SOPC_SLinkedList_GetLength(timers) < SOPC_MAX_TIMERS)
     {
-        if (result < SOPC_MAX_TIMERS + 1) // 0 idx value is invalid (max idx = MAX + 1)
+        if (idx < SOPC_MAX_TIMERS) // 0 idx value is invalid (max idx = MAX)
         {
-            idx = latestTimerId + 1;
+            idx = idx + 1;
         }
         else
         {
@@ -69,7 +69,7 @@ static uint32_t SOPC_Internal_GetFreshTimerId_WithoutLock(void)
         }
         else
         {
-            if (idx < SOPC_MAX_TIMERS + 1) // 0 idx value is invalid (max idx = MAX + 1)
+            if (idx < SOPC_MAX_TIMERS) // 0 idx value is invalid (max idx = MAX)
             {
                 idx = idx + 1;
             }
