0bbf97b3b37629ef20a642fd61aa30ebbf61a62d
Pierre-Antoine <brameret@systerel.fr>
2018-05-14 13:19:15 +0200

    Ticket #415: Cleaner ifdef for GCC warning ignore defines
    Note: SOPC_GCC_DIAGNOSTIC_IGNORE_STRINGS_OVERLENGTH was not defined when __GNUC__ was not defined.

--- csrc/configuration/sopc_toolkit_constants.h
+++ csrc/configuration/sopc_toolkit_constants.h
@@ -90,18 +90,13 @@ typedef enum SOPC_ReturnStatus {
 #define SOPC_MONOTONIC_CLOCK true
 #endif
 
-#ifdef __GNUC__
-#ifndef __clang__
+#if defined(__GNUC__) && !defined(__clang__)
 #define SOPC_GCC_DIAGNOSTIC_IGNORE_CAST_CONST _Pragma("GCC diagnostic ignored \"-Wcast-qual\"");
 #define SOPC_GCC_DIAGNOSTIC_IGNORE_STRINGS_OVERLENGTH _Pragma("GCC diagnostic ignored \"-Woverlength-strings\"");
 #define SOPC_GCC_DIAGNOSTIC_RESTORE _Pragma("GCC diagnostic pop")
 #else
 #define SOPC_GCC_DIAGNOSTIC_IGNORE_CAST_CONST
 #define SOPC_GCC_DIAGNOSTIC_IGNORE_STRINGS_OVERLENGTH
-#define SOPC_GCC_DIAGNOSTIC_RESTORE
-#endif
-#else
-#define SOPC_GCC_DIAGNOSTIC_IGNORE_CAST_CONST
 #define SOPC_GCC_DIAGNOSTIC_RESTORE
 #endif
 
