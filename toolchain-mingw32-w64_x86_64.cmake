97f4e2e26b4931e64f1ce58b693920070f1e592c
Vincent Lacroix <vincent.lacroix@systerel.fr>
2018-06-18 22:28:21 +0200

    Ticket #470: change licence into source files

--- toolchain-mingw32-w64_x86_64.cmake
+++ toolchain-mingw32-w64_x86_64.cmake
@@ -1,3 +1,21 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+# the name of the target operating system
+
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
 
@@ -10,7 +28,7 @@ SET(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 SET(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32/sys-root/mingw;/usr/x86_64-w64-mingw32)
 
 # adjust the default behaviour of the FIND_XXX() commands:
-# search headers and libraries in the target environment, search 
+# search headers and libraries in the target environment, search
 # programs in the host environment
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
