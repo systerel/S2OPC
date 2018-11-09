7e131199af6302b2bef7bf579c5badef6569721e
Pierre-Antoine <brameret@systerel.fr>
2018-11-09 17:02:31 +0100

    Ticket #415: Fix trailing whitespaces and refactor constants in validation tests

--- validation/client_session_timeout.py
+++ validation/client_session_timeout.py
@@ -35,7 +35,10 @@ from common import sUri
 from tap_logger import TapLogger
 from opcua.crypto import security_policies
 
-session_timeout = 10000
+
+SESSION_TIMEOUT     = 10000
+MIN_SESSION_TIMEOUT = 10000
+MAX_SESSION_TIMEOUT = 600000
 
 class Client2(Client):
 
@@ -51,7 +54,7 @@ class Client2(Client):
         desc.ProductUri = self.product_uri
         desc.ApplicationName = ua.LocalizedText(self.name)
         desc.ApplicationType = ua.ApplicationType.Client
-    
+
         params = ua.CreateSessionParameters()
         nonce = utils.create_nonce(32)  # at least 32 random bytes for server to prove possession of private key (specs part 4, 5.6.2.2)
         params.ClientNonce = nonce
@@ -60,7 +63,7 @@ class Client2(Client):
         params.EndpointUrl = self.server_url.geturl()
         params.SessionName = self.description + " Session" + str(self._session_counter)
         # START MODIFICATION
-        params.RequestedSessionTimeout = session_timeout
+        params.RequestedSessionTimeout = SESSION_TIMEOUT
         # END MODIFICATION
         params.MaxResponseMessageSize = 0  # means no max size
         response = self.uaclient.create_session(params)
@@ -84,12 +87,8 @@ class Client2(Client):
         return response
 
 
-min_session_timeout = 10000
-max_session_timeout = 600000
-
 if __name__=='__main__':
-
-    # tests with one connexion
+    # Tests with one connexion
     print('Connecting to', sUri)
     client = Client2(sUri)
     logger = TapLogger("session_timeout.tap")
@@ -99,20 +98,19 @@ if __name__=='__main__':
     for l_session_timeout in [5000, 360000, 45000000]:
         try:
             logger.begin_section("Requested session timeout {0}".format(l_session_timeout))
-            session_timeout = l_session_timeout
             # secure channel connection
             print(headerString.format("SC connection and session establishment with requested timeout {0}"
                                      .format(l_session_timeout)))
             secure_channels_connect(client, SecurityPolicy)
             print(headerString.format("Check revised session timeout value"))
-            if (l_session_timeout < min_session_timeout
-                and client.session_timeout == min_session_timeout):
+            if (l_session_timeout < MIN_SESSION_TIMEOUT
+                and client.session_timeout == MIN_SESSION_TIMEOUT):
                 logger.add_test("- Revised timeout set to min value {}".format(client.session_timeout), True)
-            elif (l_session_timeout > max_session_timeout
-                  and client.session_timeout == max_session_timeout):
+            elif (l_session_timeout > MAX_SESSION_TIMEOUT
+                  and client.session_timeout == MAX_SESSION_TIMEOUT):
                 logger.add_test("- Revised timeout set to max value {}".format(client.session_timeout), True)
-            elif (l_session_timeout >= min_session_timeout
-                  and l_session_timeout <= max_session_timeout
+            elif (l_session_timeout >= MIN_SESSION_TIMEOUT
+                  and l_session_timeout <= MAX_SESSION_TIMEOUT
                   and client.session_timeout == l_session_timeout):
                 logger.add_test("- Revised timeout set to requested value {}".format(client.session_timeout), True)
             else:
@@ -120,12 +118,11 @@ if __name__=='__main__':
         finally:
             client.disconnect()
             print('Disconnected')
-        
+
     # Test session timeout expiration
-    session_timeout = 10000
     for sp in [SecurityPolicy, security_policies.SecurityPolicyBasic256]:
         logger.begin_section("Requested session timeout {0} (policy {1})"
-                             .format(session_timeout, re.split("#",sp.URI)[-1]))
+                             .format(SESSION_TIMEOUT, re.split("#",sp.URI)[-1]))
         try:
             # secure channel connection
             print(headerString.format("SC connection and session establishment"))
@@ -139,7 +136,7 @@ if __name__=='__main__':
             value = node.get_value()
 
             print(headerString.format("Wait session timeout (10 seconds)"))
-            sleep((session_timeout / 1000) + 1) # add 1 seconde to timeout
+            sleep((SESSION_TIMEOUT / 1000) + 1) # add 1 seconde to timeout
 
             # read tests attempt on session after timeout
             print(headerString.format("Read: error excepted since session timeout"))
@@ -150,7 +147,7 @@ if __name__=='__main__':
 
             else:
                 logger.add_test('- Session timeout - read refused after timeout', False)
-    
+
             # check endpoints (no session needed)
             print(headerString.format("GetEndpoints after session timeout on session Channel"))
             try:
