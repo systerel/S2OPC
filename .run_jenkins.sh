4b989cf14034ce685db24a48c93538be4df2d1c2
monfort <vincent.monfort@systerel.fr>
2018-11-29 10:13:08 +0100

    Ticket #415: jenkins script: remove option not necessary for cross compilation

--- .run_jenkins.sh
+++ .run_jenkins.sh
@@ -43,4 +43,4 @@ pushd acceptances_tests/
 ../.run-uactt-in-docker.sh ./launch_acceptance_tests.sh
 popd
 # Build binaries for Windows target on Linux host
-./.mingwbuild-in-docker.sh CROSS_COMPILE_MINGW=true WARNINGS_AS_ERRORS=0 ./build.sh
+./.mingwbuild-in-docker.sh WARNINGS_AS_ERRORS=0 ./build.sh
