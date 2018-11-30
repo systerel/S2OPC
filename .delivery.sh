9e3d7cb3d160295029cdc7b082718947c11c6c35
monfort <vincent.monfort@systerel.fr>
2018-11-30 09:39:58 +0100

    Ticket #415: delivery script: update latest version in README.md

--- .delivery.sh
+++ .delivery.sh
@@ -86,12 +86,16 @@ git checkout -b $DELIVERY_NAME &> /dev/null || exit 1
 
 echo "Checking out $2-update-tagged-version"
 git checkout -b $2-update-tagged-version &> /dev/null || exit 1
-echo "Update to $1* version in sopc_toolkit_constants.h in $2-update-tagged-version"
+echo "Update to $1 version in sopc_toolkit_constants.h in $2-update-tagged-version"
 sed -i 's/#define SOPC_TOOLKIT_VERSION_MAJOR .*/#define SOPC_TOOLKIT_VERSION_MAJOR '"$major"'/' $VERSION_HEADER || exit 1
 sed -i 's/#define SOPC_TOOLKIT_VERSION_MEDIUM .*/#define SOPC_TOOLKIT_VERSION_MEDIUM '"$medium"'/' $VERSION_HEADER || exit 1
 sed -i 's/#define SOPC_TOOLKIT_VERSION_MINOR .*/#define SOPC_TOOLKIT_VERSION_MINOR '"$minor"'/' $VERSION_HEADER || exit 1
+echo "Update to $1 version in README.md file"
+sed -i "s/S2OPC_Toolkit_[0-9].[0-9].[0-9]/SOPC_Toolkit_$1/" README.md || exit 1
+
 echo "Commit updated current version in $2-update-tagged-version: it shall be pushed as MR on gitlab ASAP"
-git commit $VERSION_HEADER -S -m "Ticket #$2: Update current version of Toolkit" &> /dev/null || exit 1
+git commit README.md $VERSION_HEADER -S -m "Ticket #$2: Update current version of Toolkit" &> /dev/null || exit 1
+
 
 echo "Checking out $DELIVERY_NAME"
 git checkout $DELIVERY_NAME || exit 1
@@ -100,7 +104,9 @@ echo "Update to $1 version in sopc_toolkit_constants.h in $DELIVERY_NAME"
 sed -i 's/#define SOPC_TOOLKIT_VERSION_MAJOR .*/#define SOPC_TOOLKIT_VERSION_MAJOR '"$major"'/' $VERSION_HEADER || exit 1
 sed -i 's/#define SOPC_TOOLKIT_VERSION_MEDIUM .*/#define SOPC_TOOLKIT_VERSION_MEDIUM '"$medium"'/' $VERSION_HEADER || exit 1
 sed -i 's/#define SOPC_TOOLKIT_VERSION_MINOR .*/#define SOPC_TOOLKIT_VERSION_MINOR '"$minor"'/' $VERSION_HEADER || exit 1
-git commit $VERSION_HEADER -S -m "Update tagged $1 version information" &> /dev/null || exit 1
+echo "Update to $1 version in README.md file"
+sed -i "s/S2OPC_Toolkit_[0-9].[0-9].[0-9]/SOPC_Toolkit_$1/" README.md || exit 1
+git commit README.md $VERSION_HEADER -S -m "Update tagged $1 version information" &> /dev/null || exit 1
 
 echo "Generate and commit version file 'VERSION' with '$1' content"
 echo "$1" > VERSION
