b55e60afe9f29e19fb7f494f83cb8ee17ccda88c
Pierre-Antoine <brameret@systerel.fr>
2018-12-13 18:22:11 +0100

    Ticket #519: Make the browse example more resilient

--- tests/pys2opc/examples/1-browse.py
+++ tests/pys2opc/examples/1-browse.py
@@ -34,47 +34,50 @@ if __name__ == '__main__':
     with PyS2OPC.initialize():
         config = PyS2OPC.add_configuration_unsecured(**configuration_parameters_no_subscription)
         PyS2OPC.mark_configured()
-        with PyS2OPC.connect(config, BaseConnectionHandler) as connection:
-            # The tree structure: stores explored nodes
-            dNodes = {}  # {parent_node_id: [sub_node_id for each sub_node]}
-            # The browse names are stored in a different structure
-            dNames = {}  # {node_id: browse_name}
-            # Adds nodes we don't want to explore.
-            dNodes['i=86'] = []  # Types
-            dNodes['i=87'] = []  # Views
-            # The structure to store known-but-yet-to-be-explored nodes
-            sCandidates = {'i=84'}  # The Root node
-            while sCandidates:
-                lToBrowse = list(sCandidates)[:50]  # Make a request of max 50 nodes
-                # Make the request
-                print('.', end='', flush=True)
-                respBrowse = connection.browse_nodes(lToBrowse)
-                # Put the browsed node face to its browse result
-                for a, bwsr in zip(lToBrowse, respBrowse.results):
-                    sCandidates.remove(a)
-                    assert a not in dNodes
-                    dNodes[a] = []
-                    # The browse result contains the list of references
-                    for ref in bwsr.references:
-                        if not ref.isForward:
-                            continue
-                        if ref.referenceTypeId not in ('i=47',  # HasComponent
-                                                       'i=46',  # HasProperty
-                                                       'i=35'):  # Organizes
-                            continue
-                        b = ref.nodeId
-                        dNodes[a].append(b)
-                        if b not in dNames:
-                            dNames[b] = ref.browseName[1]
-                        # Add unexplored states to future candidates
-                        if b not in dNodes:
-                            sCandidates.add(b)
-            # Finds missing names (mostly, the root node)
-            lToName = [node for node in dNodes if node not in dNames]
-            respRead = connection.read_nodes(lToName, [AttributeId.BrowseName]*len(lToName))
-            print('x')
-            for node, dvName in zip(lToName, respRead.results):
-                dNames[node] = dvName.variant[1]
+        # The tree structure: stores explored nodes
+        dNodes = {}  # {parent_node_id: [sub_node_id for each sub_node]}
+        # The browse names are stored in a different structure
+        dNames = {}  # {node_id: browse_name}
+        # Adds nodes we don't want to explore.
+        dNodes['i=86'] = []  # Types
+        dNodes['i=87'] = []  # Views
+        # The structure to store known-but-yet-to-be-explored nodes
+        sCandidates = {'i=84'}  # The Root node
+        try:
+            with PyS2OPC.connect(config, BaseConnectionHandler) as connection:
+                while sCandidates:
+                    lToBrowse = list(sCandidates)[:50]  # Make a request of max 50 nodes
+                    # Make the request
+                    print('.', end='', flush=True)
+                    respBrowse = connection.browse_nodes(lToBrowse)
+                    # Put the browsed node face to its browse result
+                    for a, bwsr in zip(lToBrowse, respBrowse.results):
+                        sCandidates.remove(a)
+                        assert a not in dNodes
+                        dNodes[a] = []
+                        # The browse result contains the list of references
+                        for ref in bwsr.references:
+                            if not ref.isForward:
+                                continue
+                            if ref.referenceTypeId not in ('i=47',  # HasComponent
+                                                           'i=46',  # HasProperty
+                                                           'i=35'):  # Organizes
+                                continue
+                            b = ref.nodeId
+                            dNodes[a].append(b)
+                            if b not in dNames:
+                                dNames[b] = ref.browseName[1]
+                            # Add unexplored states to future candidates
+                            if b not in dNodes:
+                                sCandidates.add(b)
+                # Finds missing names (mostly, the root node)
+                lToName = [node for node in dNodes if node not in dNames]
+                respRead = connection.read_nodes(lToName, [AttributeId.BrowseName]*len(lToName))
+                print('x')
+                for node, dvName in zip(lToName, respRead.results):
+                    dNames[node] = dvName.variant[1]
+        except:
+            print('Connection failed, printing partial Address Space')
 
     def print_recurs(node, iIndent=0, sPrinted=set()):
         global dNodes, dNames
