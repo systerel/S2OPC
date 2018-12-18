1fe3fc4426bb307ccd0c44ee672c664436a69a2d
monfort <vincent.monfort@systerel.fr>
2018-12-18 18:10:28 +0100

    Ticket #547: Tests certificates: fix and improve tests certificates generation script
    - Avoid prompting for information
    - Fix issues to reuse the script (remove index/serial files)
    - Generate certificates compatible with TLS

--- tests/data/cert/generate_certs.sh
+++ tests/data/cert/generate_certs.sh
@@ -17,27 +17,33 @@
 # specific language governing permissions and limitations
 # under the License.
 
+set -e
+rm -f index.txt serial.txt
 touch index.txt
 touch index.txt.attr
-echo FA > serial.txt  # Current serial number
+echo 00 > serial.txt  # Current serial
+CONF_FILE=cassl.cnf
+CA_KEY=cakey.pem
+CA_CERT=cacert.pem
 
 # CA generation: generate key, generate self signed certificate# (does not work with 4096 key length)
-openssl genrsa -aes256 -out cakey.pem 4096
-openssl req -config cassl.cnf -new -x509 -key cakey.pem -out cacert.pem -days 36500
+# /!\ only for test as no pass phrase is embedeed"
+openssl genrsa -out $CA_KEY 4096
+openssl req -config $CONF_FILE -new -x509 -key $CA_KEY -out $CA_CERT -days 36500
 
 # Generate an empty Certificate Revocation List, convert it to DER format for UA stack
-openssl ca -config cassl.cnf -gencrl -crldays 36500 -out cacrl.pem
+openssl ca -config $CONF_FILE -gencrl -crldays 36500 -out cacrl.pem
 openssl crl -in cacrl.pem -outform der -out cacrl.der
 
 # Generate, for both client and server, and both 2048 and 4096 key lengths, a new key pair and sign them, for the next century
-openssl req -config clientssl.cnf -sha256 -nodes -newkey rsa:2048 -keyout client_2k_key.pem -out client_2k.csr
-openssl req -config clientssl.cnf -sha256 -nodes -newkey rsa:4096 -keyout client_4k_key.pem -out client_4k.csr
-openssl req -config serverssl.cnf -sha256 -nodes -newkey rsa:2048 -keyout server_2k_key.pem -out server_2k.csr
-openssl req -config serverssl.cnf -sha256 -nodes -newkey rsa:4096 -keyout server_4k_key.pem -out server_4k.csr
-openssl ca -config cassl.cnf -policy signing_policy -days 36500 -in client_2k.csr -out client_2k_cert.pem
-openssl ca -config cassl.cnf -policy signing_policy -days 36500 -in client_4k.csr -out client_4k_cert.pem
-openssl ca -config cassl.cnf -policy signing_policy -days 36500 -in server_2k.csr -out server_2k_cert.pem
-openssl ca -config cassl.cnf -policy signing_policy -days 36500 -in server_4k.csr -out server_4k_cert.pem
+openssl req -config $CONF_FILE -extensions server_cert -sha256 -nodes -newkey rsa:2048 -keyout client_2k_key.pem -out client_2k.csr
+openssl req -config $CONF_FILE -extensions server_cert -sha256 -nodes -newkey rsa:4096 -keyout client_4k_key.pem -out client_4k.csr
+openssl req -config $CONF_FILE -extensions server_cert -sha256 -nodes -newkey rsa:2048 -keyout server_2k_key.pem -out server_2k.csr
+openssl req -config $CONF_FILE -extensions server_cert -sha256 -nodes -newkey rsa:4096 -keyout server_4k_key.pem -out server_4k.csr
+openssl ca -batch -config $CONF_FILE -policy signing_policy -days 36500 -in client_2k.csr -out client_2k_cert.pem
+openssl ca -batch -config $CONF_FILE -policy signing_policy -days 36500 -in client_4k.csr -out client_4k_cert.pem
+openssl ca -batch -config $CONF_FILE -policy signing_policy -days 36500 -in server_2k.csr -out server_2k_cert.pem
+openssl ca -batch -config $CONF_FILE -policy signing_policy -days 36500 -in server_4k.csr -out server_4k_cert.pem
 
 # Output certificates in DER format for UA stack
 for fradix in ca client_2k_ client_4k_ server_2k_ server_4k_; do
