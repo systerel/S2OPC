97db3c48f8549bd103bf7f48f4ae3b13de2e7fe3
Vincent Lacroix <vincent.lacroix@systerel.fr>
2018-12-19 17:42:08 +0100

    Ticket #547: update jenkins script following repository move.

--- .pre-build-in-docker.sh
+++ .pre-build-in-docker.sh
@@ -38,7 +38,7 @@ cd $TMP_TOOLING_DIR > /dev/null
 # Copy tooling repository with git archive from server repository or local one
 if [[ -z $TOOLING_DIR ]]; then
     echo "Environment variable TOOLING_DIR not set => copy of tooling.git through SSH"
-    ssh pcb git --git-dir /git/c765/tooling.git archive master bin | tar x
+    ssh pcb git --git-dir /git/c838/tooling.git archive master bin | tar x
 else
     echo "Environment variable TOOLING_DIR set to '$TOOLING_DIR' => copy of repository"
     git --git-dir $TOOLING_DIR/.git archive master bin | tar x
