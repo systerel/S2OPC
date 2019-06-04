890190ebda06edbe188a2c7ef0b1bfa715e73c56
Pierre-Antoine <brameret@systerel.fr>
2019-06-04 18:54:10 +0200

    Ticket #599: Add missing license headers

--- csrc/helpers_platform_dep/freertos/p_time.c
+++ csrc/helpers_platform_dep/freertos/p_time.c
@@ -1,4 +1,4 @@
-/*_
+/*
  * Licensed to Systerel under one or more contributor license
  * agreements. See the NOTICE file distributed with this work
  * for additional information regarding copyright ownership.
