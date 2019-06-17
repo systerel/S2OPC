3edc1a68899f1d2d19f1d7da2e6be2130dbe705c
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2019-06-17 17:06:10 +0200

    Ticket #599: work in progress logsrv ethernet_if

---
+++ tests/freertos/test_helpers_platform_dep/logserver/p_ethernet_if.h
@@ -0,0 +1,69 @@
+/*
+ * p_ethernet_if.h
+ *
+ *  Created on: 6 juin 2019
+ *      Author: nottin
+ */
+
+#ifndef HELPERS_PLATFORM_DEP_FREERTOS_P_ETHERNET_IF_H_
+#define HELPERS_PLATFORM_DEP_FREERTOS_P_ETHERNET_IF_H_
+
+#include <board.h>
+#include <inttypes.h> /* stdlib includes */
+#include <limits.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "FreeRTOS.h" /* freeRtos includes */
+#include "queue.h"
+#include "semphr.h"
+#include "task.h"
+#include "timers.h"
+
+#include "p_synchronisation.h" /* synchronisation include */
+#include "p_threads.h"
+#include "p_utils.h"           /* private list include */
+
+#include "lwip/opt.h"
+#include "lwipopts.h"
+#include "lwip/mem.h"
+#include "lwip/memp.h"
+#include "netif/etharp.h"
+#include "lwip/dhcp.h"
+#include "lwip/netif.h"
+#include "lwip/timeouts.h"
+#include "ethernetif.h"
+
+#include "lwip/init.h"
+#include "lwip/netif.h"
+#include "lwip/tcpip.h"
+#include "lwip/sockets.h"
+
+#define IP_ADDRESS_0 192
+#define IP_ADDRESS_1 168
+#define IP_ADDRESS_2 1
+#define IP_ADDRESS_3 102
+
+#define IP_MASK_0 255
+#define IP_MASK_1 255
+#define IP_MASK_2 255
+#define IP_MASK_3 0
+
+#define IP_GW_0 192
+#define IP_GW_1 168
+#define IP_GW_2 1
+#define IP_GW_3 100
+
+#define PHY_MAC_ADDRESS { 0x02, 0x12, 0x13, 0x10, 0x15, 0x11 }
+
+typedef enum E_ETHERNET_IF_RESULT
+{
+    ETHERNET_IF_RESULT_OK,
+    ETHERNET_IF_RESULT_NOK
+}eEthernetIfResult;
+
+eEthernetIfResult P_ETHERNET_IF_Initialize(void);
+eEthernetIfResult P_ETHERNET_IF_IsReady(void);
+
+#endif /* HELPERS_PLATFORM_DEP_FREERTOS_P_ETHERNET_IF_H_ */
