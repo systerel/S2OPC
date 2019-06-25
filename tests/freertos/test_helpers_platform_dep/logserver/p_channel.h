3cf193dae0991d6b8f84f06cc2fead809c32206d
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2019-06-25 11:41:17 +0200

    Ticket #599: Format p_channel

--- tests/freertos/test_helpers_platform_dep/logserver/p_channel.h
+++ tests/freertos/test_helpers_platform_dep/logserver/p_channel.h
@@ -17,7 +17,6 @@
  * under the License.
  */
 
-
 #ifndef P_CHANNEL_H
 #define P_CHANNEL_H
 
@@ -35,64 +34,72 @@
 
 #include "p_utils.h"
 
-//Channel result
+// Channel result
 typedef enum E_CHANNEL_RESULT
 {
-    E_CHANNEL_RESULT_OK,            //No error
-    E_CHANNEL_RESULT_NOK,           //General error
-    E_CHANNEL_RESULT_ERROR_FULL,    //Not enough space
-    E_CHANNEL_RESULT_ERROR_EMPTY,    //Empty
+    E_CHANNEL_RESULT_OK,          // No error
+    E_CHANNEL_RESULT_NOK,         // General error
+    E_CHANNEL_RESULT_ERROR_FULL,  // Not enough space
+    E_CHANNEL_RESULT_ERROR_EMPTY, // Empty
     E_CHANNEL_RESULT_ERROR_TMO,
     E_CHANNEL_RESULT_MORE_DATA
-}eChannelResult;
+} eChannelResult;
 
-//Channel receive mode
+// Channel receive mode
 typedef enum E_CHANNEL_READ_MODE
 {
-    E_CHANNEL_RD_MODE_NORMAL,       //Normal, oldest elt is read then removed
-    E_CHANNEL_RD_MODE_JUST_KEEPING  //Just keeping, read without remove oldest elt.
-}eChannelReadMode;
+    E_CHANNEL_RD_MODE_NORMAL,      // Normal, oldest elt is read then removed
+    E_CHANNEL_RD_MODE_JUST_KEEPING // Just keeping, read without remove oldest elt.
+} eChannelReadMode;
 
-//Channel send mode
+// Channel send mode
 typedef enum E_CHANNEL_WRITE_MODE
 {
-    E_CHANNEL_WR_MODE_NORMAL,       //Normal, elt is write to channel if enough space
-    E_CHANNEL_WR_MODE_OVERWRITE     //Overwrited, oldest elts are removed until enough space to write new elt.
-}eChannelWriteMode;
+    E_CHANNEL_WR_MODE_NORMAL,   // Normal, elt is write to channel if enough space
+    E_CHANNEL_WR_MODE_OVERWRITE // Overwrited, oldest elts are removed until enough space to write new elt.
+} eChannelWriteMode;
 
-//Channel workspace
+// Channel workspace
 typedef struct T_CHANNEL
 {
-    uint16_t iWr ;                  //Write buffer record index
-    uint16_t iRd ;                  //Read buffer record index
-    uint16_t iWrData;               //Write buffer data index
-    uint16_t iRdData;               //Read buffer data index
-    uint16_t maxSizeTotalData;      //Max of cumulative data
-    uint16_t maxSizeTotalElts;      //Max of cumulative record
-    uint16_t maxSizeDataPerElt;     //Max size for each record
-    uint16_t currentNbElts;         //Current nb records
-    uint16_t currentNbDatas;        //Current cumulative data for current nb records
+    uint16_t iWr;               // Write buffer record index
+    uint16_t iRd;               // Read buffer record index
+    uint16_t iWrData;           // Write buffer data index
+    uint16_t iRdData;           // Read buffer data index
+    uint16_t maxSizeTotalData;  // Max of cumulative data
+    uint16_t maxSizeTotalElts;  // Max of cumulative record
+    uint16_t maxSizeDataPerElt; // Max size for each record
+    uint16_t currentNbElts;     // Current nb records
+    uint16_t currentNbDatas;    // Current cumulative data for current nb records
     uint16_t overflowCpt;
-    QueueHandle_t lock ;            //Critical section
-    QueueHandle_t isNotEmpty;       //Signal not empty used by receive timeout
-    uint8_t *channelData;           //Buffer of data
-    uint16_t *channelRecord;        //Buffer of record (of size for each elt).
-}tChannel;
+    QueueHandle_t lock;       // Critical section
+    QueueHandle_t isNotEmpty; // Signal not empty used by receive timeout
+    uint8_t* channelData;     // Buffer of data
+    uint16_t* channelRecord;  // Buffer of record (of size for each elt).
+} tChannel;
 
-void P_CHANNEL_DeInit(tChannel*p);
-eChannelResult P_CHANNEL_Init(tChannel*p, size_t maxTotalDataSize, size_t maxEltSize, size_t nbElts);
-eChannelResult P_CHANNEL_Send(      tChannel*p,                 //Channel workspace
-                                    const uint8_t * pBuffer,    //Data to send
-                                    uint16_t size,              //Size of data to send
-                                    uint16_t *pbNbWritedBytes,
-                                    eChannelWriteMode mode)    ; //Mode, overwrite or normal.
-eChannelResult P_CHANNEL_Flush(tChannel*p);
-eChannelResult P_CHANNEL_Receive(   tChannel*p,                 //Channel workspace
-                                    uint16_t* pOutEltSize,      //Output element size
-                                    uint8_t*pBuffer,            //Buffer.  If null, only element size without pop is read
-                                    uint16_t* pNbReadBytes,
-                                    uint16_t maxBytesToRead,
-                                    TickType_t xTimeToWait,     //Time to wait in ticks
-                                    eChannelReadMode mode)   ;   //Mode RD or KEEP_ONLY. KEEP ONLY read without pop older elemt.
+void P_CHANNEL_DeInit(tChannel* p);
+
+eChannelResult P_CHANNEL_Init(tChannel* p,             // Channel workspace
+                              size_t maxTotalDataSize, // Total datasize
+                              size_t maxEltSize,       // Max atomic element size
+                              size_t nbElts);          // Max elements
+
+eChannelResult P_CHANNEL_Send(tChannel* p,               // Channel workspace
+                              const uint8_t* pBuffer,    // Data to send
+                              uint16_t size,             // Size of data to send
+                              uint16_t* pbNbWritedBytes, // Nb bytes writed
+                              eChannelWriteMode mode);   // Mode, overwrite or normal.
+
+eChannelResult P_CHANNEL_Flush(tChannel* p);
+
+eChannelResult P_CHANNEL_Receive(
+    tChannel* p,             // Channel workspace
+    uint16_t* pOutEltSize,   // Output element size
+    uint8_t* pBuffer,        // Buffer.  If null, only element size without pop is read
+    uint16_t* pNbReadBytes,  // Nb bytes read
+    uint16_t maxBytesToRead, // Max bytes to read
+    TickType_t xTimeToWait,  // Time to wait in ticks
+    eChannelReadMode mode);  // Mode RD or KEEP_ONLY. KEEP ONLY read without pop older elemt.
 
 #endif
