e1fd5d56f83abcbfb6959695d831f07940f15378
Pierre-Antoine <brameret@systerel.fr>
2019-06-28 18:15:58 +0200

    Ticket #599: Memory allocation wrappers for FreeRTOS

---
+++ csrc/helpers_platform_dep/freertos/p_mem_alloc.c
@@ -0,0 +1,76 @@
+/*
+ * Licensed to Systerel under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Systerel licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "FreeRTOS.h"
+
+#include "sopc_mem_alloc.h"
+
+void* SOPC_Malloc(size_t size)
+{
+    return pvPortMalloc(size);
+}
+
+void SOPC_Free(void* ptr)
+{
+    vPortFree(ptr);
+}
+
+void* SOPC_Calloc(size_t nmemb, size_t size)
+{
+    if (nmemb > SIZE_MAX / size)
+    {
+        return NULL;
+    }
+
+    void* p = SOPC_Malloc(nmemb * size);
+    if (NULL == p)
+    {
+        return NULL;
+    }
+
+    return memset(p, 0, nmemb * size);
+}
+
+void* SOPC_Realloc(void* ptr, size_t old_size, size_t new_size)
+{
+    /* Do not realloc/copy if size is reduced.
+     * This keeps the larger buffer reserved but avoids the copy. */
+    if (new_size <= old_size)
+    {
+        return ptr;
+    }
+
+    /* realloc(NULL) shall behave as malloc */
+    void* new = SOPC_Malloc(new_size);
+    if (NULL == new)
+    {
+        return NULL;
+    }
+
+    if (NULL != ptr)
+    {
+        memcpy(new, ptr, old_size);
+    }
+
+    return new;
+}
