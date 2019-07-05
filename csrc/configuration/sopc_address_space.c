fbae4f0931b2381ea1e970e710d6cf97df134b66
monfort <vincent.monfort@systerel.fr>
2019-07-05 16:22:43 +0200

    Ticket #604: address space module: check address space parameter not NULL

--- csrc/configuration/sopc_address_space.c
+++ csrc/configuration/sopc_address_space.c
@@ -60,6 +60,8 @@ void SOPC_AddressSpace_Node_Initialize(SOPC_AddressSpace* space,
                                        SOPC_AddressSpace_Node* node,
                                        OpcUa_NodeClass node_class)
 {
+    assert(space != NULL);
+
     switch (node_class)
     {
         FOR_EACH_ELEMENT_TYPE(ELEMENT_ATTRIBUTE_INITIALIZE_CASE, NULL)
@@ -102,7 +104,7 @@ void SOPC_AddressSpace_Node_Initialize(SOPC_AddressSpace* space,
 #define ELEMENT_ATTRIBUTE_GETTER(ret_type, name)                                                   \
     ret_type* SOPC_AddressSpace_Get_##name(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node) \
     {                                                                                              \
-        (void) space;                                                                              \
+        assert(space != NULL);                                                                     \
         assert(node->node_class > 0);                                                              \
         switch (node->node_class)                                                                  \
         {                                                                                          \
@@ -140,6 +142,8 @@ static SOPC_Variant* SOPC_AddressSpace_Node_Get_Value(SOPC_AddressSpace_Node* no
 
 SOPC_Variant* SOPC_AddressSpace_Get_Value(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
+    assert(space != NULL);
+
     switch (node->node_class)
     {
     case OpcUa_NodeClass_Variable:
@@ -166,8 +170,8 @@ SOPC_Variant* SOPC_AddressSpace_Get_Value(SOPC_AddressSpace* space, SOPC_Address
 
 SOPC_NodeId* SOPC_AddressSpace_Get_DataType(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
-    // Avoid unused parameter warning:
-    (void) space;
+    assert(space != NULL);
+
     switch (node->node_class)
     {
     case OpcUa_NodeClass_Variable:
@@ -182,8 +186,8 @@ SOPC_NodeId* SOPC_AddressSpace_Get_DataType(SOPC_AddressSpace* space, SOPC_Addre
 
 int32_t* SOPC_AddressSpace_Get_ValueRank(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
-    // Avoid unused parameter warning:
-    (void) space;
+    assert(space != NULL);
+
     switch (node->node_class)
     {
     case OpcUa_NodeClass_Variable:
@@ -198,8 +202,8 @@ int32_t* SOPC_AddressSpace_Get_ValueRank(SOPC_AddressSpace* space, SOPC_AddressS
 
 int32_t SOPC_AddressSpace_Get_NoOfArrayDimensions(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
-    // Avoid unused parameter warning:
-    (void) space;
+    assert(space != NULL);
+
     switch (node->node_class)
     {
     case OpcUa_NodeClass_Variable:
@@ -214,8 +218,8 @@ int32_t SOPC_AddressSpace_Get_NoOfArrayDimensions(SOPC_AddressSpace* space, SOPC
 
 uint32_t* SOPC_AddressSpace_Get_ArrayDimensions(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
-    // Avoid unused parameter warning:
-    (void) space;
+    assert(space != NULL);
+
     switch (node->node_class)
     {
     case OpcUa_NodeClass_Variable:
@@ -230,8 +234,8 @@ uint32_t* SOPC_AddressSpace_Get_ArrayDimensions(SOPC_AddressSpace* space, SOPC_A
 
 SOPC_Byte SOPC_AddressSpace_Get_AccessLevel(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
-    // Avoid unused parameter warning:
-    (void) space;
+    assert(space != NULL);
+
     switch (node->node_class)
     {
     case OpcUa_NodeClass_Variable:
@@ -244,8 +248,8 @@ SOPC_Byte SOPC_AddressSpace_Get_AccessLevel(SOPC_AddressSpace* space, SOPC_Addre
 
 SOPC_Boolean* SOPC_AddressSpace_Get_IsAbstract(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
-    // Avoid unused parameter warning:
-    (void) space;
+    assert(space != NULL);
+
     switch (node->node_class)
     {
     case OpcUa_NodeClass_VariableType:
@@ -264,6 +268,8 @@ SOPC_Boolean* SOPC_AddressSpace_Get_IsAbstract(SOPC_AddressSpace* space, SOPC_Ad
 
 SOPC_StatusCode SOPC_AddressSpace_Get_StatusCode(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
+    assert(space != NULL);
+
     if (!space->readOnlyNodes)
     {
         return node->value_status;
@@ -276,6 +282,8 @@ SOPC_StatusCode SOPC_AddressSpace_Get_StatusCode(SOPC_AddressSpace* space, SOPC_
 
 bool SOPC_AddressSpace_Set_StatusCode(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node, SOPC_StatusCode status)
 {
+    assert(space != NULL);
+
     if (!space->readOnlyNodes)
     {
         node->value_status = status;
@@ -286,6 +294,8 @@ bool SOPC_AddressSpace_Set_StatusCode(SOPC_AddressSpace* space, SOPC_AddressSpac
 
 SOPC_Value_Timestamp SOPC_AddressSpace_Get_SourceTs(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
+    assert(space != NULL);
+
     if (!space->readOnlyNodes)
     {
         return node->value_source_ts;
@@ -299,6 +309,8 @@ SOPC_Value_Timestamp SOPC_AddressSpace_Get_SourceTs(SOPC_AddressSpace* space, SO
 
 bool SOPC_AddressSpace_Set_SourceTs(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node, SOPC_Value_Timestamp ts)
 {
+    assert(space != NULL);
+
     if (!space->readOnlyNodes)
     {
         node->value_source_ts = ts;
@@ -321,8 +333,8 @@ static void SOPC_AddressSpace_Node_Clear_Local(SOPC_AddressSpace_Node* node)
 
 void SOPC_AddressSpace_Node_Clear(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
-    // Avoid unused parameter warning:
-    (void) space;
+    assert(space != NULL);
+
     SOPC_AddressSpace_Node_Clear_Local(node);
 }
 
@@ -383,11 +395,15 @@ SOPC_AddressSpace* SOPC_AddressSpace_CreateReadOnlyNodes(uint32_t nb_nodes,
 
 bool SOPC_AddressSpace_AreReadOnlyNodes(const SOPC_AddressSpace* space)
 {
+    assert(space != NULL);
+
     return space->readOnlyNodes;
 }
 
 SOPC_ReturnStatus SOPC_AddressSpace_Append(SOPC_AddressSpace* space, SOPC_AddressSpace_Node* node)
 {
+    assert(space != NULL);
+
     if (space->readOnlyNodes)
     {
         return SOPC_STATUS_INVALID_STATE;
@@ -412,22 +428,27 @@ SOPC_ReturnStatus SOPC_AddressSpace_Append(SOPC_AddressSpace* space, SOPC_Addres
 
 void SOPC_AddressSpace_Delete(SOPC_AddressSpace* space)
 {
-    SOPC_Dict_Delete(space->dict_nodes);
-    space->dict_nodes = NULL;
-    for (uint32_t i = 0; i < space->nb_variables; i++)
+    if (NULL != space)
     {
-        SOPC_Variant_Clear(&space->variables[i]);
+        SOPC_Dict_Delete(space->dict_nodes);
+        space->dict_nodes = NULL;
+        for (uint32_t i = 0; i < space->nb_variables; i++)
+        {
+            SOPC_Variant_Clear(&space->variables[i]);
+        }
+        // Do not free variables array which was provided as input
+        space->nb_nodes = 0;
+        space->const_nodes = NULL;
+        space->nb_variables = 0;
+        space->variables = NULL;
+        SOPC_Free(space);
     }
-    // Do not free variables array which was provided as input
-    space->nb_nodes = 0;
-    space->const_nodes = NULL;
-    space->nb_variables = 0;
-    space->variables = NULL;
-    SOPC_Free(space);
 }
 
 SOPC_AddressSpace_Node* SOPC_AddressSpace_Get_Node(SOPC_AddressSpace* space, const SOPC_NodeId* key, bool* found)
 {
+    assert(space != NULL);
+
     if (!space->readOnlyNodes)
     {
         return SOPC_Dict_Get(space->dict_nodes, key, found);
@@ -451,6 +472,8 @@ SOPC_AddressSpace_Node* SOPC_AddressSpace_Get_Node(SOPC_AddressSpace* space, con
 
 void SOPC_AddressSpace_ForEach(SOPC_AddressSpace* space, SOPC_AddressSpace_ForEach_Fct func, void* user_data)
 {
+    assert(space != NULL);
+
     if (!space->readOnlyNodes)
     {
         SOPC_Dict_ForEach(space->dict_nodes, func, user_data);
