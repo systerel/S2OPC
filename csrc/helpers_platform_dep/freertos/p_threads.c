c93daa2fa16f22b65196189ab69c6d207291bb9a
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2019-07-08 14:56:12 +0200

    Ticket #599: P_Thread_Init shall return INVALID_PARAMETERS on NULL callback

--- csrc/helpers_platform_dep/freertos/p_threads.c
+++ csrc/helpers_platform_dep/freertos/p_threads.c
@@ -171,9 +171,7 @@ SOPC_ReturnStatus P_THREAD_Init(Thread* ptrWks,            // Workspace
     SOPC_ReturnStatus resList = SOPC_STATUS_NOK;
     Thread handleWks = NULL;
 
-    assert(NULL != taskName);
-
-    if (NULL == ptrWks)
+    if (NULL == ptrWks || NULL == fct)
     {
         return SOPC_STATUS_INVALID_PARAMETERS;
     }
