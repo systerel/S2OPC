44b5edfe5509b582e703fdf05f7100718a6ce568
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2019-07-09 10:46:31 +0200

    Ticket #599: Add test U CHECK_THREAD

--- tests/freertos/test_helpers_platform_dep/FreeRTOSTest.h
+++ tests/freertos/test_helpers_platform_dep/FreeRTOSTest.h
@@ -23,5 +23,6 @@
 void FREE_RTOS_TEST_API_S2OPC_THREAD(void* ptr);
 void FREE_RTOS_TEST_S2OPC_SERVER(void* ptr);
 void FREE_RTOS_TEST_S2OPC_TIME(void* ptr);
+void FREE_RTOS_TEST_S2OPC_CHECK_THREAD(void*ptr);
 
 #endif
