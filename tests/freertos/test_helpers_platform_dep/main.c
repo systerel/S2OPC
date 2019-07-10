cfc48ecbe53b2eb944b036109d9dfad9de1850d8
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2019-07-10 10:12:38 +0200

    Ticket #599: Start s2opc test server by default

--- tests/freertos/test_helpers_platform_dep/main.c
+++ tests/freertos/test_helpers_platform_dep/main.c
@@ -93,13 +93,13 @@ int main(void)
     // FREE_RTOS_TEST_API_S2OPC_THREAD(&gHandleConditionVariable);
 
     // Toolkit server application
-    // FREE_RTOS_TEST_S2OPC_SERVER(&gHandleConditionVariable);
+    FREE_RTOS_TEST_S2OPC_SERVER(&gHandleConditionVariable);
 
     // Unit test copy from tests sources. CHECK_TIME
     // FREE_RTOS_TEST_S2OPC_TIME(&gHandleConditionVariable);
 
     // Unit test copy from tests sources. CHECK_THREAD and MUTEX and COND VAR
-    FREE_RTOS_TEST_S2OPC_CHECK_THREAD(&gHandleConditionVariable);
+    // FREE_RTOS_TEST_S2OPC_CHECK_THREAD(&gHandleConditionVariable);
 
     vTaskStartScheduler();
 
