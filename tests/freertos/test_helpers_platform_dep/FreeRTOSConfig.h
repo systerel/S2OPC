1b63dfa6806dbbf4946272ab68709e4e85eb18da
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2019-07-18 17:10:29 +0200

    Ticket #599: FreeRTOS conf 256K ram and priority tasks enabled

--- tests/freertos/test_helpers_platform_dep/FreeRTOSConfig.h
+++ tests/freertos/test_helpers_platform_dep/FreeRTOSConfig.h
@@ -42,8 +42,8 @@
 #define configUSE_TICKLESS_IDLE 0
 #define configCPU_CLOCK_HZ (SystemCoreClock)
 #define configTICK_RATE_HZ ((TickType_t) 1000)
-#define configMAX_PRIORITIES 1
-#define configMINIMAL_STACK_SIZE ((unsigned short) 1024)
+#define configMAX_PRIORITIES 15
+#define configMINIMAL_STACK_SIZE ((unsigned short) 2048)
 #define configMINIMAL_STACK_SIZE_TIMER ((unsigned short) 256)
 #define configMAX_TASK_NAME_LEN 40
 #define configUSE_16_BIT_TICKS 0
@@ -55,7 +55,7 @@
 #define configUSE_ALTERNATIVE_API 0 /* Deprecated! */
 #define configQUEUE_REGISTRY_SIZE 8
 #define configUSE_QUEUE_SETS 0
-#define configUSE_TIME_SLICING 1
+#define configUSE_TIME_SLICING 0
 #define configUSE_NEWLIB_REENTRANT 1
 #define configENABLE_BACKWARD_COMPATIBILITY 0
 #define configNUM_THREAD_LOCAL_STORAGE_POINTERS 5
@@ -64,7 +64,7 @@
 /* Memory allocation related definitions. */
 #define configSUPPORT_STATIC_ALLOCATION 0
 #define configSUPPORT_DYNAMIC_ALLOCATION 1
-#define configTOTAL_HEAP_SIZE ((size_t)(174762))
+#define configTOTAL_HEAP_SIZE ((size_t)(262144))
 #define configAPPLICATION_ALLOCATED_HEAP 0
 
 /* Hook function related definitions. */
