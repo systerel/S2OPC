5b06ddfdd09c3f4f5f20dc120db33f386cdc659a
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2019-08-09 08:29:30 +0200

    Ticket #599: to stash: cleanup wrap_read.c

--- tests/freertos/test_helpers_platform_dep/toolkit_test_client/wrap_read.c
+++ tests/freertos/test_helpers_platform_dep/toolkit_test_client/wrap_read.c
@@ -41,9 +41,6 @@
 /* http://stackoverflow.com/questions/7265583/combine-designated-initializers-and-malloc-in-c99 */
 #define DESIGNATE_NEW(T, ...) memcpy(SOPC_Malloc(sizeof(T)), &(T const){__VA_ARGS__}, sizeof(T))
 
-const uint32_t N_READ_NODES = 7; // Read nodes with Node Id 1000 to 1000 + N_READ_NODES only
-const uint32_t N_READ_VARS = 6;  // Read variables values with Node Id 1001 to 1001 + N_READ_VARS only
-
 /**
  * Creates a request of length N_REQUEST
  */
@@ -64,39 +61,6 @@ OpcUa_ReadRequest* read_new_read_request(void)
                                  .AttributeId = constants__e_aid_Value,
                                  .IndexRange = {.Length = 0},
                                  .DataEncoding = {.Name.Length = 0}};
-
-    //    /* All nodes Ids and classes */
-    //    for (i = 0; i < N_READ_NODES; ++i)
-    //    {
-    //        /* Request for the NodeId (...) */
-    //        lrv[2 * i + 0] = (OpcUa_ReadValueId){
-    //            .encodeableType = &OpcUa_ReadValueId_EncodeableType,
-    //            .NodeId = {.IdentifierType = SOPC_IdentifierType_Numeric, .Data.Numeric = i + 1000, .Namespace = 1},
-    //            .AttributeId = constants__e_aid_NodeId,
-    //            .IndexRange = {.Length = 0},
-    //            .DataEncoding = {.Name.Length = 0}};
-    //        /* Request for the NodeClass */
-    //        lrv[2 * i + 1] = (OpcUa_ReadValueId){
-    //            .encodeableType = &OpcUa_ReadValueId_EncodeableType,
-    //            .NodeId = {.IdentifierType = SOPC_IdentifierType_Numeric, .Data.Numeric = i + 1000, .Namespace = 1},
-    //            .AttributeId = constants__e_aid_NodeClass,
-    //            .IndexRange = {.Length = 0},
-    //            .DataEncoding = {.Name.Length = 0}};
-    //    }
-
-    //    /* Note: variables have the last numeric node ids in the @space used for this test*/
-    //    /* All variables */
-    //    for (i = 0; i < N_READ_VARS; ++i)
-    //    {
-    //        /* Request for the Value */
-    //        lrv[2 * N_READ_NODES + i] = (OpcUa_ReadValueId){.encodeableType = &OpcUa_ReadValueId_EncodeableType,
-    //                                                        .NodeId = {.IdentifierType = SOPC_IdentifierType_Numeric,
-    //                                                                   .Data.Numeric = (N_READ_NODES - 1 - i) + 1000,
-    //                                                                   .Namespace = 1},
-    //                                                        .AttributeId = constants__e_aid_Value,
-    //                                                        .IndexRange = {.Length = 0},
-    //                                                        .DataEncoding = {.Name.Length = 0}};
-    //    }
 
     OpcUa_ReadRequest* pReadReq = DESIGNATE_NEW(OpcUa_ReadRequest, .encodeableType = &OpcUa_ReadRequest_EncodeableType,
                                                 .MaxAge = 0., .TimestampsToReturn = OpcUa_TimestampsToReturn_Neither,
