9d13a78ed712ab88c55e0f81b79dd51ab3ac2c91
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2019-08-09 08:38:04 +0200

    Ticket #599: Add EthernetConfig.h with MAC and IP config

--- tests/freertos/test_helpers_platform_dep/logserver/p_ethernet_if.h
+++ tests/freertos/test_helpers_platform_dep/logserver/p_ethernet_if.h
@@ -54,27 +54,9 @@
 #include "lwip/sockets.h"
 #include "lwip/tcpip.h"
 
+#include "EthernetConfig.h"
+
 // ************Private API**************
-
-#define IP_ADDRESS_0 192
-#define IP_ADDRESS_1 168
-#define IP_ADDRESS_2 1
-#define IP_ADDRESS_3 102
-
-#define IP_MASK_0 255
-#define IP_MASK_1 255
-#define IP_MASK_2 255
-#define IP_MASK_3 0
-
-#define IP_GW_0 192
-#define IP_GW_1 168
-#define IP_GW_2 1
-#define IP_GW_3 100
-
-#define PHY_MAC_ADDRESS                    \
-    {                                      \
-        0x02, 0x12, 0x13, 0x10, 0x15, 0x11 \
-    }
 
 typedef enum E_ETHERNET_IF_RESULT
 {
