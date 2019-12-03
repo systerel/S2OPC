447160edb2b678ba8491ca1cdbed9fe91c54bcc1
Pierre-Antoine <brameret@systerel.fr>
2019-12-03 19:15:27 +0100

    Ticket #000000: Pushed a public docker with RSA_ALT_SUPPORT

--- .build-in-docker.sh
+++ .build-in-docker.sh
@@ -22,8 +22,8 @@
 #
 set -e
 
-DOCKER_IMAGE=sha256:c1958a853af652ee12a74d6eee23e7af746711e10ff1e2a58453cf1e21f85b49 # build:1.15
-DOCKER_IMAGE=817d42f50067  # Temp docker that has mbedtls with RSA_ALT_SUPPORT
+# Temp public docker that has mbedtls with RSA_ALT_SUPPORT
+DOCKER_IMAGE=sha256:5f3e9321f7d999ddc3b4df3c035f1eb1a2baa585ef4aacda80807c79239f1c0c # build:1.15c
 
 if [[ -z $SOPC_DOCKER_NEEDS_SUDO ]]; then
     /etc/scripts/run-in-docker $DOCKER_IMAGE DOCKER_IMAGE=$DOCKER_IMAGE "$@"
