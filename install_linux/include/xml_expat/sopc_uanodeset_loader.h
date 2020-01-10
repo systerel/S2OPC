5de2cde2a6480248d03bda3037b8266308a24a7f
monfort <vincent.monfort@systerel.fr>
2020-01-10 16:27:52 +0100

    Add toolkit binaries for version S2OPC_Toolkit_0.11.0

---
+++ install_linux/include/xml_expat/sopc_uanodeset_loader.h
@@ -0,0 +1,28 @@
+/*
+ * Licensed to Systerel under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Systerel licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#ifndef SOPC_NODESET_LOADER_H_
+#define SOPC_NODESET_LOADER_H_
+
+#include <stdio.h>
+#include "sopc_address_space.h"
+
+SOPC_AddressSpace* SOPC_UANodeSet_Parse(FILE* fd);
+
+#endif /* SOPC_NODESET_LOADER_H_ */
