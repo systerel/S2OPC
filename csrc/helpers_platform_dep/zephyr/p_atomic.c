a3c596a3504821714b6bc5741954cbd4b379f2b4
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2020-01-10 09:44:31 +0100

    Ticket #738: Zephyr p_atomic - modify includes

---
+++ csrc/helpers_platform_dep/zephyr/p_atomic.c
@@ -0,0 +1,58 @@
+/*
+ * Licensed to Systerel under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Systerel licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <errno.h>
+#include <inttypes.h>
+#include <kernel.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sopc_atomic.h"
+
+int32_t SOPC_Atomic_Int_Get(int32_t* atomic)
+{
+    __sync_synchronize();
+    return *atomic;
+}
+
+void SOPC_Atomic_Int_Set(int32_t* atomic, int32_t val)
+{
+    *atomic = val;
+    __sync_synchronize();
+}
+
+int32_t SOPC_Atomic_Int_Add(int32_t* atomic, int32_t val)
+{
+    return __sync_fetch_and_add(atomic, val);
+}
+
+void* SOPC_Atomic_Ptr_Get(void** atomic)
+{
+    __sync_synchronize();
+    return *atomic;
+}
+
+void SOPC_Atomic_Ptr_Set(void** atomic, void* val)
+{
+    *atomic = val;
+    __sync_synchronize();
+}
