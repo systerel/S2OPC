08656a4f648c47a3f60c3ce2a75024f3a872fd4a
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2020-01-22 16:36:32 +0100

    Ticket #738: Zephyr p_time - cleanup includes

--- csrc/helpers_platform_dep/zephyr/p_time.c
+++ csrc/helpers_platform_dep/zephyr/p_time.c
@@ -25,6 +25,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/printk.h>
 
 #ifndef __INT32_MAX__
 #include <toolchain/xcc_missing_defs.h>
@@ -34,7 +35,9 @@
 #define NULL ((void*) 0)
 #endif
 
-#include "sopc_enums.h" /* s2opc includes */
+/* s2opc includes */
+
+#include "sopc_enums.h"
 #include "sopc_time.h"
 
 /* Private time api */
