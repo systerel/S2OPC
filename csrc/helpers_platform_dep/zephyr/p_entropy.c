4ede08f7ae0c9bdbbfa66d323286582c9ace439c
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2020-01-22 08:56:39 +0100

    Ticket #738: Zephyr p_entropy - add platform dep get entropy (use directly driver api - portable between each zephyr version

---
+++ csrc/helpers_platform_dep/zephyr/p_entropy.c
@@ -0,0 +1,109 @@
+/*
+ * Licensed to Systerel under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Systerel licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <device.h>
+#include <drivers/entropy.h>
+#include <errno.h>
+#include <init.h>
+#include <inttypes.h>
+#include <kernel.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/printk.h>
+
+#ifndef __INT32_MAX__
+#include <toolchain/xcc_missing_defs.h>
+#endif
+
+#include <fcntl.h>
+#include <kernel.h>
+#include <net/net_ip.h>
+#include <net/socket.h>
+#ifndef NULL
+#define NULL ((void*) 0)
+#endif
+
+#if defined(CONFIG_MBEDTLS)
+#if !defined(CONFIG_MBEDTLS_CFG_FILE)
+#include "mbedtls/config.h"
+#else
+#include CONFIG_MBEDTLS_CFG_FILE
+#endif /* CONFIG_MBEDTLS_CFG_FILE */
+#endif
+
+#ifndef CONFIG_ENTROPY_NAME
+#define CONFIG_ENTROPY_NAME ((const char*) ("TRNG"))
+#endif
+
+#define P_ENTROPY_DEBUG (0)
+
+int32_t mbedtls_hardware_poll(void* data, unsigned char* output, int32_t len, int32_t* olen)
+{
+#if P_ENTROPY_DEBUG == 1
+    printk("\r\n mbedtls_hardware_poll - %d - \r\n", len);
+#endif
+
+    /* static to obtain it once in a first call */
+    static struct device* dev;
+    int err;
+
+    if ((output == NULL) || (olen == 0))
+    {
+        return -1;
+    }
+
+    if (dev == NULL)
+    {
+#if P_ENTROPY_DEBUG == 1
+        printk("\r\nFirst call to obtain entropy device %s\r\n", CONFIG_ENTROPY_NAME);
+#endif
+        dev = device_get_binding(CONFIG_ENTROPY_NAME);
+        if (dev == NULL)
+        {
+#if P_ENTROPY_DEBUG == 1
+            printk("\r\nFailed to obtain entropy device\r\n");
+#endif
+            return -1;
+        }
+    }
+
+    err = entropy_get_entropy(dev, output, len);
+    if (err != 0)
+    {
+#if P_ENTROPY_DEBUG == 1
+        printk("\r\nFailed to obtain entropy, err %d\r\n", err);
+#endif
+        return -1;
+    }
+
+#if P_ENTROPY_DEBUG == 1
+    printk("\r\n Entropy = ");
+    for (int32_t i = 0; i < len; i++)
+    {
+        printk("%02X ", output[i]);
+    }
+    printk("\r\n");
+#endif
+
+    *olen = len;
+    return 0;
+}
