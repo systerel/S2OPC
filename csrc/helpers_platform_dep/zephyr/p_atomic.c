0dbf796c8ccce8bc4eed186a344b53177963f29a
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2020-02-13 13:50:05 +0100

    Ticket #738: Zephyr p_atomic - CODE REVIEW - Copy of Linux file

--- csrc/helpers_platform_dep/zephyr/p_atomic.c
+++ csrc/helpers_platform_dep/zephyr/p_atomic.c
@@ -21,14 +21,23 @@
 
 int32_t SOPC_Atomic_Int_Get(int32_t* atomic)
 {
+#if !defined(__clang__) && (__GNUC__ > 4)
+    // This version works with TSan, the other one creates false positives...
+    return (int32_t) __atomic_load_4(atomic, __ATOMIC_SEQ_CST);
+#else
     __sync_synchronize();
     return *atomic;
+#endif
 }
 
 void SOPC_Atomic_Int_Set(int32_t* atomic, int32_t val)
 {
+#if !defined(__clang__) && (__GNUC__ > 4)
+    __atomic_store_4(atomic, (unsigned int) val, __ATOMIC_SEQ_CST);
+#else
     *atomic = val;
     __sync_synchronize();
+#endif
 }
 
 int32_t SOPC_Atomic_Int_Add(int32_t* atomic, int32_t val)
@@ -38,12 +47,36 @@ int32_t SOPC_Atomic_Int_Add(int32_t* atomic, int32_t val)
 
 void* SOPC_Atomic_Ptr_Get(void** atomic)
 {
+#if !defined(__clang__) && (__GNUC__ > 4)
+
+#if SOPC_PTR_SIZE == 4
+    return (void*) __atomic_load_4(atomic, __ATOMIC_SEQ_CST);
+#elif SOPC_PTR_SIZE == 8
+    return (void*) __atomic_load_8(atomic, __ATOMIC_SEQ_CST);
+#else
+#error "Unsupported pointer size"
+#endif // SOPC_PTR_SIZE
+
+#else
     __sync_synchronize();
     return *atomic;
+#endif // !defined(clang)
 }
 
 void SOPC_Atomic_Ptr_Set(void** atomic, void* val)
 {
+#if !defined(__clang__) && (__GNUC__ > 4)
+
+#if SOPC_PTR_SIZE == 4
+    __atomic_store_4(atomic, (uintptr_t) val, __ATOMIC_SEQ_CST);
+#elif SOPC_PTR_SIZE == 8
+    __atomic_store_8(atomic, (uintptr_t) val, __ATOMIC_SEQ_CST);
+#else
+#error "Unsupported pointer size"
+#endif // SOPC_PTR_SIZE
+
+#else
     *atomic = val;
     __sync_synchronize();
+#endif // !defined(clang)
 }
