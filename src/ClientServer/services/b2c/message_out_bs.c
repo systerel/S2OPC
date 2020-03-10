941e35665cb2beca0679a0aa345b0724565e4ec7
monfort <vincent.monfort@systerel.fr>
2020-03-10 11:33:20 +0100

    Ticket #713: session services minor fix: change session placeholders NodeIds due to conflict
    NodeIds used for session placeholders are already used in base info address space.
    Change NodeIds to avoid conflicts.

--- src/ClientServer/services/b2c/message_out_bs.c
+++ src/ClientServer/services/b2c/message_out_bs.c
@@ -492,7 +492,7 @@ void message_out_bs__write_create_session_msg_session_token(
     assert(SOPC_STATUS_OK == status);
     createSessionResp->SessionId.IdentifierType = SOPC_IdentifierType_Numeric;
     createSessionResp->SessionId.Data.Numeric = message_out_bs__session;
-    createSessionResp->SessionId.Data.Numeric += 10000;
+    createSessionResp->SessionId.Data.Numeric += 100000;
 }
 
 void message_out_bs__write_create_session_msg_session_revised_timeout(const constants__t_msg_i message_out_bs__req_msg,
