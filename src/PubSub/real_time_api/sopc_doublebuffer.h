d085fbbea5ead8648aeba67b6f9c5fef1dcedda8
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2020-03-20 15:07:00 +0100

    Ticket #742: RT API DBO - CODE REVIEW - Remove inline comments from header

--- src/PubSub/real_time_api/sopc_doublebuffer.h
+++ src/PubSub/real_time_api/sopc_doublebuffer.h
@@ -38,13 +38,13 @@ typedef struct SOPC_DoubleBuffer SOPC_DoubleBuffer;
 /// @param[in]    nbReaders           Nb of readers will access to DBO object via SOPC_DBO_Read API
 /// @param[in]    atomic_elt_size     Size of an atomic element
 /// @return   DBO object. Shall be destroyed via SOPC_DoubleBuffer_Destroy.
-SOPC_DoubleBuffer* SOPC_DoubleBuffer_Create(uint32_t nbReaders,        // Nb readers
-                                            uint32_t atomic_elt_size); // Size of atomic element
+SOPC_DoubleBuffer* SOPC_DoubleBuffer_Create(uint32_t nbReaders,        //
+                                            uint32_t atomic_elt_size); //
 
 /// @brief   Destroy a DBO object
 /// @warning Not thread safe!
 /// @param [inout]  p  DBO object to destroy. Pointer is setted to NULL.
-void SOPC_DoubleBuffer_Destroy(SOPC_DoubleBuffer** p); // DBO object to destroy
+void SOPC_DoubleBuffer_Destroy(SOPC_DoubleBuffer** p); //
 
 // Following functions (SOPC_DoubleBuffer_XxWriteBufferXx) shall be used from one same thread execution context.
 // because not thread safe. SOPC_DoubleBuffer is designed for 1 writer / Multiple readers
@@ -55,9 +55,9 @@ void SOPC_DoubleBuffer_Destroy(SOPC_DoubleBuffer** p); // DBO object to destroy
 /// @param [out] pIdBuffer Buffer identifier which will be used with write buffer functions.
 /// @param [out] pMaxSize Max allowed size
 /// @return SOPC_STATUS_NOK in case of failure (returned idBuffer set to UINT32_MAX), else SOPC_STATUS_OK
-SOPC_ReturnStatus SOPC_DoubleBuffer_GetWriteBuffer(SOPC_DoubleBuffer* p, // DBO object
-                                                   uint32_t* pIdBuffer,  // Address to write idBuffer
-                                                   uint32_t* pMaxSize);  // Max allowed size
+SOPC_ReturnStatus SOPC_DoubleBuffer_GetWriteBuffer(SOPC_DoubleBuffer* p, //
+                                                   uint32_t* pIdBuffer,  //
+                                                   uint32_t* pMaxSize);  //
 
 /// @brief Write data to a buffer.
 /// @warning Not thread safe for concurrent WRITERS!
@@ -72,15 +72,14 @@ SOPC_ReturnStatus SOPC_DoubleBuffer_GetWriteBuffer(SOPC_DoubleBuffer* p, // DBO 
 /// @param [in] ignorePrevious Ignore previous data: don't copy previous last record information before offset
 /// @param [in] ignoreAfter Ignore previous data after offset + size
 /// @return Returns SOPC_STATUS_OK in case of success.
-SOPC_ReturnStatus SOPC_DoubleBuffer_WriteBuffer(
-    SOPC_DoubleBuffer* p,   // DBO object
-    uint32_t idBuffer,      // Id of buffer
-    uint32_t offset,        // Offset of writing start. Total size = current size + offset + size of data to write
-    uint8_t* data,          // Data to write
-    uint32_t size,          // Size of data to write
-    uint32_t* pWrittenSize, // Size written
-    bool ignorePrevious,    // Ignore previous data : don't copy previous record information before offset
-    bool ignoreAfter);      // Ignore previous data after offset + size
+SOPC_ReturnStatus SOPC_DoubleBuffer_WriteBuffer(SOPC_DoubleBuffer* p,   //
+                                                uint32_t idBuffer,      //
+                                                uint32_t offset,        //
+                                                uint8_t* data,          //
+                                                uint32_t size,          //
+                                                uint32_t* pWrittenSize, //
+                                                bool ignorePrevious,    //
+                                                bool ignoreAfter);      //
 
 /// @brief Expose directly double buffer data field and size field.
 /// @warning Not thread safe for concurrent WRITERS!
@@ -91,12 +90,11 @@ SOPC_ReturnStatus SOPC_DoubleBuffer_WriteBuffer(
 /// @param [out] ppSizeField Address where is returned pointer on buffer size field
 /// @param [in] ignorePrevious If true, last updated value of DBO are NOT copied to reserved buffer.
 /// @return SOPC_STATUS_OK in case of success.
-SOPC_ReturnStatus SOPC_DoubleBuffer_WriteBufferGetPtr(
-    SOPC_DoubleBuffer* p,   // DBO object
-    uint32_t idBuffer,      // Buffer identifier
-    uint8_t** ppDataField,  // Pointer on data field
-    uint32_t** ppSizeField, // Pointer on size field
-    bool ignorePrevious);   // Copy last updated value of DBO on output ptr
+SOPC_ReturnStatus SOPC_DoubleBuffer_WriteBufferGetPtr(SOPC_DoubleBuffer* p,   //
+                                                      uint32_t idBuffer,      //
+                                                      uint8_t** ppDataField,  //
+                                                      uint32_t** ppSizeField, //
+                                                      bool ignorePrevious);   //
 
 /// @brief Set nb of significant bytes to 0 of a double buffer
 /// @warning Not thread safe for concurrent WRITERS!
@@ -104,8 +102,8 @@ SOPC_ReturnStatus SOPC_DoubleBuffer_WriteBufferGetPtr(
 /// @param [in] p DBO object
 /// @param [in] idBuffer Identifier of buffer returned by SOPC_DoubleBuffer_GetWriteBuffer
 /// @return SOPC_STATUS_OK in case of success.
-SOPC_ReturnStatus SOPC_DoubleBuffer_WriteBufferErase(SOPC_DoubleBuffer* p, // DBO object
-                                                     uint32_t idBuffer);   // Id of buffer returned by GetWriteBuffer
+SOPC_ReturnStatus SOPC_DoubleBuffer_WriteBufferErase(SOPC_DoubleBuffer* p, //
+                                                     uint32_t idBuffer);   //
 
 /// @brief Release a reserved buffer after a write operation. Modification by SOPC_DoubleBuffer_WriteXXX are taken into
 /// account.
@@ -113,8 +111,8 @@ SOPC_ReturnStatus SOPC_DoubleBuffer_WriteBufferErase(SOPC_DoubleBuffer* p, // DB
 /// @param [in] p DBO buffer object reference
 /// @param [inout] pIdBuffer Id of buffer returned by SOPC_DoubleBuffer_GetWriteBuffer. Set to UINT32_MAX.
 /// @return SOPC_STATUS_OK in case of success, else SOPC_STATUS_NOK.
-SOPC_ReturnStatus SOPC_DoubleBuffer_ReleaseWriteBuffer(SOPC_DoubleBuffer* p, // DBO buffer object reference
-                                                       uint32_t* pIdBuffer); // Id of buffer return by GetWriteBuffer
+SOPC_ReturnStatus SOPC_DoubleBuffer_ReleaseWriteBuffer(SOPC_DoubleBuffer* p, //
+                                                       uint32_t* pIdBuffer); //
 
 /// @brief Get most up to date buffer to read. This buffer is marked as reading status.
 /// @warning Shall always be called before ReadBuffer/ReadBufferPtr and ReleaseReadBuffer
