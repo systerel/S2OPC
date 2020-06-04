3f8ebbb7f5f99ba2c423bf9c7a007da2a7a4fd56
Pierre-Antoine <brameret@systerel.fr>
2020-06-04 19:30:36 +0200

    Ticket #765: PyS2OPC Fix unpack which does not always return bytes

--- src/ClientServer/frontend/pys2opc/pys2opc/types.py
+++ src/ClientServer/frontend/pys2opc/pys2opc/types.py
@@ -309,7 +309,7 @@ def bytestring_to_bytes(bstring):
     """SOPC_ByteString or SOPC_ByteString* to python bytes()"""
     if bstring == ffi.NULL or bstring.Data == ffi.NULL or bstring.Length <= 0:
         return b''
-    return ffi.unpack(bstring.Data, bstring.Length)
+    return bytes(ffi.unpack(bstring.Data, bstring.Length))
 def bytes_to_bytestring(b, no_gc=False):
     """
     Python bytes to SOPC_ByteString*.
