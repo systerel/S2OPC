f21994853b0f2df8db7cc57a4c1cc422e81070a9
Aurelien NOTTIN <aurelien.nottin@systerel.fr>
2020-07-03 19:42:37 +0200

    Ticket #738: Security none for pub conf static (to fix pb load key from file)

--- samples/PubSub_ClientServer/zephyr/pubsub_test_server/src/pubsub_config_static.c
+++ samples/PubSub_ClientServer/zephyr/pubsub_test_server/src/pubsub_config_static.c
@@ -181,8 +181,7 @@ SOPC_PubSubConfiguration* SOPC_PubSubConfig_GetStatic()
     SOPC_DataSetWriter* writer = NULL;
     if (alloc)
     {
-        writer =
-            SOPC_PubSubConfig_SetPubMessageAt(connection, 0, 14, 1, PUBLISH_PERIOD, SOPC_SecurityMode_SignAndEncrypt);
+        writer = SOPC_PubSubConfig_SetPubMessageAt(connection, 0, 14, 1, PUBLISH_PERIOD,SOPC_SecurityMode_None);
         alloc = NULL != writer;
     }
 
@@ -240,8 +239,7 @@ SOPC_PubSubConfiguration* SOPC_PubSubConfig_GetStatic()
 
     if (alloc)
     {
-        reader = SOPC_PubSubConfig_SetSubMessageAt(connection, 0, 42, 14, 1, SUBSCRIBE_PERIOD,
-                                                   SOPC_SecurityMode_SignAndEncrypt);
+        reader = SOPC_PubSubConfig_SetSubMessageAt(connection, 0, 42, 14, 1, SUBSCRIBE_PERIOD,SOPC_SecurityMode_None);
         alloc = NULL != reader;
     }
 
@@ -259,7 +257,7 @@ SOPC_PubSubConfiguration* SOPC_PubSubConfig_GetStatic()
 
     if (alloc)
     {
-        reader = SOPC_PubSubConfig_SetSubMessageAt(connection, 1, 42, 15, 1, SUBSCRIBE_PERIOD, SOPC_SecurityMode_None);
+        reader = SOPC_PubSubConfig_SetSubMessageAt(connection, 1, 42, 15, 1, SUBSCRIBE_PERIOD,SOPC_SecurityMode_None);
         alloc = NULL != reader;
     }
 
