86d4fd97f487a8b72fd2ae25b796bc6dcbccbab4
Bertille Miellin <bertille.miellin@systerel.fr>
2021-02-03 20:31:47 +0100

    Ticket #000000: Add server reading and UDP frames sending

--- samples/PubSub_ClientServer/pubsub_server/udp_json_gtw.c
+++ samples/PubSub_ClientServer/pubsub_server/udp_json_gtw.c
@@ -56,6 +56,12 @@
 #define DATA3_TYPE SOPC_String_Id
 
 #define NODEID_PREFIX "ns=1;s="
+#define REQUEST_LENGTH 500
+#define REQUEST_HEADER_1 "POST HTTP/1.1\nHost UDP:"
+#define REQUEST_HEADER_2 "\n{\n\"code\":\"request\",\n\"cid\":"
+#define REQUEST_HEADER_3 ",\n\"adr\":\"\",\n\"data\":{ "
+#define REQUEST_END "}\n"
+#define CID 5
 
 /* ---------------------------------------- */
 /* TYPES DEFINITION */
@@ -91,17 +97,17 @@ static char* localhost_ip = "127.0.0.1";
 /* Reception global variables */
 static int32_t UDP_recepRunning = 0;
 static int recepSockfd = 0;
-static struct sockaddr_in servaddr;
+static struct sockaddr_in recepSock;
 static pthread_t recepThreadId = 0;
 
 /* Sending global variables */
 static int32_t UDP_sendRunning = 0;
 static int sendSockfd = 0;
-static struct sockaddr_in cltaddr;
+static struct sockaddr_in sendSock;
 static pthread_t sendThreadId = 0;
 
 /* Data global variables */
-static DataType Data[NBDATA];
+static DataType rcvdData[NBDATA];
 
 /* ---------------------------------------- */
 /* STATIC FUNCTIONS */
@@ -129,13 +135,13 @@ static SOPC_ReturnStatus UDP_ReceptionConfigure (void)
 
     if (SOPC_STATUS_OK == status)
     {
-        /* Configure server information */
-        servaddr.sin_family = AF_INET;
-        servaddr.sin_addr.s_addr = inet_addr(localhost_ip);
-        servaddr.sin_port = htons(UDP_PORT_IN);
+        /* Configure reception information */
+        recepSock.sin_family = AF_INET;
+        recepSock.sin_addr.s_addr = inet_addr(localhost_ip);
+        recepSock.sin_port = htons(UDP_PORT_IN);
 
         /* Bind reception socket to the server address */
-        if (bind(recepSockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 )
+        if (bind(recepSockfd, (const struct sockaddr *)&recepSock, sizeof(recepSock)) < 0 )
         {
             printf("# Error: reception socket binding failed.\n");
             status = SOPC_STATUS_NOK;
@@ -159,7 +165,10 @@ static SOPC_ReturnStatus UDP_SendingConfigure (void)
 {
     SOPC_ReturnStatus status = SOPC_STATUS_OK;
 
-    // TODO
+    /* Configure sending information */
+    sendSock.sin_family = AF_INET;
+    sendSock.sin_addr.s_addr = inet_addr(localhost_ip);
+    sendSock.sin_port = htons(UDP_PORT_OUT);
 
     return status;
 }
@@ -203,27 +212,35 @@ static void UDP_CreateWriteValue(OpcUa_WriteValue* writeValue, uint8_t dataIdx)
     switch (Cfg[dataIdx].type)
     {
         case SOPC_String_Id :
-            val->Value.String.Length = (int32_t) strlen(Data[dataIdx].Value.text);
+            val->Value.String.Length = (int32_t) strlen(rcvdData[dataIdx].Value.text);
             val->Value.String.DoNotClear = true;
-            val->Value.String.Data = (SOPC_Byte*) Data[dataIdx].Value.text;
+            val->Value.String.Data = (SOPC_Byte*) rcvdData[dataIdx].Value.text;
             break;
 
         case SOPC_Int32_Id :
-            val->Value.Int32 = Data[dataIdx].Value.int32;
+            val->Value.Int32 = rcvdData[dataIdx].Value.int32;
             break;
 
         case SOPC_UInt32_Id :
-            val->Value.Uint32 = Data[dataIdx].Value.uint32;
+            val->Value.Uint32 = rcvdData[dataIdx].Value.uint32;
             break;
 
         case SOPC_Int16_Id :
-            val->Value.Int16 =  Data[dataIdx].Value.int16;
+            val->Value.Int16 =  rcvdData[dataIdx].Value.int16;
             break;
 
         default :
             printf("# Info: Unexpected type in UDP configuration: %d\n", Cfg[dataIdx].type);
             break;
     }
+}
+
+static void UDP_CreateNodeId(char* dataName, char* nodeId_Str)
+{
+    memset(nodeId_Str, '\0', 2*MAXNBCHAR);
+    strcpy(nodeId_Str, NODEID_PREFIX);
+    strcat(nodeId_Str, dataName);
+    printf("# Info: Node Id created: %s\n", nodeId_Str);
 }
 
 
@@ -238,10 +255,7 @@ static bool UDP_UpdateServer(void)
     for (uint8_t i = 0; i < NBDATA; i++)
     {
         /* Generate Node Id*/
-        memset(nodeId_Str, '\0', 2*MAXNBCHAR);
-        strcpy(nodeId_Str, NODEID_PREFIX);
-        strcat(nodeId_Str, Data[i].name);
-        printf("# Info: Update Node Id  %s\n", nodeId_Str);
+        UDP_CreateNodeId(rcvdData[i].name, nodeId_Str);
         nodeId = SOPC_NodeId_FromCString(nodeId_Str, (int32_t) strlen(nodeId_Str));
 
         /* Create OPC UA Write value */
@@ -286,27 +300,27 @@ static void UDP_DecodeData(char* buffer)
         switch (Cfg[i].type)
         {
             case SOPC_String_Id :
-                sscanf(dataArr[i], "%s :%s" , Data[i].name,  Data[i].Value.text);
-                printf("# Info: Data name: %s\n", Data[i].name);
-                printf("# Info: Data value: %s\n", Data[i].Value.text);
+                sscanf(dataArr[i], "%s :%s" , rcvdData[i].name,  rcvdData[i].Value.text);
+                printf("# Info: Data name: %s\n", rcvdData[i].name);
+                printf("# Info: Data value: %s\n", rcvdData[i].Value.text);
                 break;
 
             case SOPC_Int32_Id :
-                sscanf(dataArr[i], "%s :%" SCNi32, Data[i].name, &Data[i].Value.int32);
-                printf("# Info: Data name: %s\n", Data[i].name);
-                printf("# Info: Data value: %d\n", Data[i].Value.int32);
+                sscanf(dataArr[i], "%s :%" SCNi32, rcvdData[i].name, &rcvdData[i].Value.int32);
+                printf("# Info: Data name: %s\n", rcvdData[i].name);
+                printf("# Info: Data value: %d\n", rcvdData[i].Value.int32);
                 break;
 
             case SOPC_UInt32_Id :
-                sscanf(dataArr[i], "%s :%" SCNu32, Data[i].name, &Data[i].Value.uint32);
-                printf("# Info: Data name: %s\n", Data[i].name);
-                printf("# Info: Data value: %d\n", Data[i].Value.uint32);
+                sscanf(dataArr[i], "%s :%" SCNu32, rcvdData[i].name, &rcvdData[i].Value.uint32);
+                printf("# Info: Data name: %s\n", rcvdData[i].name);
+                printf("# Info: Data value: %d\n", rcvdData[i].Value.uint32);
                 break;
 
             case SOPC_Int16_Id :
-                sscanf(dataArr[i], "%s :%" SCNi16, Data[i].name, &Data[i].Value.int16);
-                printf("# Info: Data name: %s\n", Data[i].name);
-                printf("# Info: Data value: %d\n", Data[i].Value.int16);
+                sscanf(dataArr[i], "%s :%" SCNi16, rcvdData[i].name, &rcvdData[i].Value.int16);
+                printf("# Info: Data name: %s\n", rcvdData[i].name);
+                printf("# Info: Data value: %d\n", rcvdData[i].Value.int16);
                 break;
 
             default :
@@ -401,16 +415,144 @@ static void* UDP_ManageReception (void* arg)
 }
 
 
+static bool UDP_ReadServer(SOPC_DataValue** readValues)
+{
+    OpcUa_ReadValueId* readValuesId;
+    char nodeId_Str[2*MAXNBCHAR];
+    SOPC_NodeId* nodeId;
+    SOPC_StatusCode status;
+    bool readStatus = true;
+
+    /* Allocate memory */
+    readValuesId = SOPC_Malloc(NBDATA * sizeof(OpcUa_ReadValueId));
+
+    /* Initialize readValuesId for each data */
+    for (int i = 0; i < NBDATA; i++)
+    {
+        OpcUa_ReadValueId_Initialize(&readValuesId[i]);
+        readValuesId[i].AttributeId = SOPC_AttributeId_Value;
+        UDP_CreateNodeId(Cfg[i].name, nodeId_Str);
+        nodeId = SOPC_NodeId_FromCString(nodeId_Str, (int32_t) strlen(nodeId_Str));
+        status = SOPC_NodeId_Copy(&readValuesId[i].NodeId, nodeId);
+        if (SOPC_STATUS_OK != status)
+        {
+            printf("# Info: Node Id creation failure for %s.\n", Cfg[i].name);
+            readStatus = false;
+        }
+
+        SOPC_NodeId_Clear(nodeId);
+        SOPC_Free(nodeId);
+    }
+
+    if (readStatus)
+    {
+        /* Read values on Server */
+        /* readValuesId is freed by Server_GetSourceVariables */
+        *readValues = Server_GetSourceVariables(readValuesId, NBDATA);
+    }
+
+    return readStatus;
+}
+
+
+static void UDP_CreateRequest(char* request, SOPC_DataValue* readValues)
+{
+    char buffer[MAXNBCHAR];
+
+    /* Reset Request content */
+    memset(request, '\0', REQUEST_LENGTH);
+
+    /* Build request header */
+    strcpy(request, REQUEST_HEADER_1);
+    sprintf(buffer, "%d;", UDP_PORT_IN);
+    strncat(request, buffer, REQUEST_LENGTH-strlen(request));
+    strncat(request, localhost_ip, REQUEST_LENGTH-strlen(request));
+    sprintf(buffer, ";%d", UDP_PORT_OUT);
+    strncat(request, buffer, REQUEST_LENGTH-strlen(request));
+    strncat(request, REQUEST_HEADER_2, REQUEST_LENGTH-strlen(request));
+    sprintf(buffer, "%d", CID);
+    strncat(request, buffer, REQUEST_LENGTH-strlen(request));
+    strncat(request, REQUEST_HEADER_3, REQUEST_LENGTH-strlen(request));
+
+    /* Add request data */
+    for (int i = 0; i < NBDATA; i++)
+    {
+        strncat(request, Cfg[i].name, REQUEST_LENGTH-strlen(request));
+        strncat(request, " :", REQUEST_LENGTH-strlen(request));
+
+        switch (readValues[i].Value.BuiltInTypeId)
+        {
+            case SOPC_String_Id :
+                strncat(request, (char*)readValues[i].Value.Value.String.Data, REQUEST_LENGTH-strlen(request));
+                break;
+
+            case SOPC_Int32_Id :
+                sprintf(buffer, "%d", readValues[i].Value.Value.Int32);
+                strncat(request, buffer, REQUEST_LENGTH-strlen(request));
+                break;
+
+            case SOPC_UInt32_Id :
+                sprintf(buffer, "%d", readValues[i].Value.Value.Uint32);
+                strncat(request, buffer, REQUEST_LENGTH-strlen(request));
+                break;
+
+            case SOPC_Int16_Id :
+                sprintf(buffer, "%d", readValues[i].Value.Value.Int16);
+                strncat(request, buffer, REQUEST_LENGTH-strlen(request));
+                break;
+
+            default :
+                printf("# Info: Received unexpected type : %d\n", readValues[i].Value.BuiltInTypeId);
+                break;
+        }
+
+        if (NBDATA-1 != i)
+        {
+            strncat(request, ", ", REQUEST_LENGTH-strlen(request));
+        }
+    }
+
+    /* Finalize request (both open curly bracket to be closed) */
+    strncat(request, REQUEST_END, REQUEST_LENGTH-strlen(request));
+    strncat(request, REQUEST_END, REQUEST_LENGTH-strlen(request));
+
+    printf("# Info: Request to be sent is:\n%s\n", request);
+}
+
+
 static void* UDP_ManageSending (void* arg)
 {
     /*unused parameter */
     (void) arg;
 
+    SOPC_DataValue* readValues = NULL;
+    char request[REQUEST_LENGTH];
+
     printf("# Info: UDP Sending thread running.\n");
 
     while (SOPC_Atomic_Int_Get(&UDP_sendRunning))
     {
-        // TODO
+        /* Retrieve data values */
+        if (UDP_ReadServer(&readValues))
+        {
+            /* Create request with received data values */
+            UDP_CreateRequest(request, readValues);
+
+            /* Free memory */
+            SOPC_Free(readValues);
+
+            /* Send UDP datagram */
+            sendto(sendSockfd, request, strlen(request),
+                   MSG_CONFIRM, (const struct sockaddr *) &sendSock,
+                   sizeof(sendSock));
+        }
+        else
+        {
+            printf("# Info: Server read has failed, no request sent.\n");
+        }
+
+        /* Wait sending delay => 1 second */
+        SOPC_Sleep(SLEEP_TIMEOUT*10);
     }
 
     /* Exit current thread */
