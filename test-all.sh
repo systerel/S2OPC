5ab5f363890c942da9279bb4928537878e526b4b
Aurelien GILLES <aurelien.gilles@systerel.fr>
2021-02-18 18:54:13 +0100

    Ticket #696: [Tests] add pubsub SKS validation script to CI

--- test-all.sh
+++ test-all.sh
@@ -66,6 +66,7 @@ pubsub_modules_test.tap
 xml_parser_test.tap'
 
 PUBSUB_CLIENTSERVER_TAP_FILES='pubsub_server_test.tap'
+PUBSUB_CLIENTSERVER_TAP_FILES=$PUBSUB_CLIENTSERVER_TAP_FILES$'\npubsub_sks_test.tap'
 
 
 PYS2OPC_TAP_FILES=$'\nvalidation_pys2opc.tap'
@@ -122,7 +123,10 @@ if [ -z $S2OPC_CLIENTSERVER_ONLY ]; then
    cd "${PUBSUB_TEST_DIR}"
    EXPECTED_TAP_FILES=$EXPECTED_TAP_FILES$'\n'$PUBSUB_TAP_FILES
    if [ -z $S2OPC_PUBSUB_ONLY ]; then
-       EXPECTED_TAP_FILES=$EXPECTED_TAP_FILES$'\n'$PUBSUB_CLIENTSERVER_TAP_FILES
+       if [ -z $WITH_NANO_EXTENDED ] || [ "$WITH_NANO_EXTENDED" == "1" ]; then
+           # si nano extended pas défini ou à 1 on fait le tests
+           EXPECTED_TAP_FILES=$EXPECTED_TAP_FILES$'\n'$PUBSUB_CLIENTSERVER_TAP_FILES
+       fi
    fi
    ctest -T test --no-compress-output --test-output-size-passed 65536 --test-output-size-failed 65536
    CTEST_RET2=$?
