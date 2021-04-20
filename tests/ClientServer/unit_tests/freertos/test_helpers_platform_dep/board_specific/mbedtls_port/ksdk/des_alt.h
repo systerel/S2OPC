eb3ca959913016c83df272b7d5dd01fe7c3e1b5c
Adrien COURNAND <adrien@systerel.fr>
2021-04-20 17:39:48 +0200

    Ticket #852: Adding expected header

--- tests/ClientServer/unit_tests/freertos/test_helpers_platform_dep/board_specific/mbedtls_port/ksdk/des_alt.h
+++ tests/ClientServer/unit_tests/freertos/test_helpers_platform_dep/board_specific/mbedtls_port/ksdk/des_alt.h
@@ -1,25 +1,22 @@
-/**
- * \file des_alt.h
+/*
+ * Licensed to Systerel under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Systerel licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain
+ * a copy of the License at
  *
- * \brief DES block cipher
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- *  Copyright (C) 2006-2015, ARM Limited, All Rights Reserved
- *  SPDX-License-Identifier: Apache-2.0
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may
- *  not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- *
- *  This file is part of mbed TLS (https://tls.mbed.org)
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 #ifndef MBEDTLS_DES_ALT_H
 #define MBEDTLS_DES_ALT_H
 
--- tests/ClientServer/unit_tests/freertos/test_helpers_platform_dep/board_specific/mbedtls_port/ksdk/ksdk_mbedtls.h
+++ tests/ClientServer/unit_tests/freertos/test_helpers_platform_dep/board_specific/mbedtls_port/ksdk/ksdk_mbedtls.h
@@ -1,9 +1,20 @@
 /*
- * Copyright (c) 2017, NXP Semiconductors, Inc.
- * All rights reserved.
+ * Licensed to Systerel under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Systerel licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain
+ * a copy of the License at
  *
+ *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * SPDX-License-Identifier: BSD-3-Clause
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 #ifndef KSDK_MBEDTLS_H
