e59869078a2ee74956a769fa8dbd4a1c9064280a
Adrien COURNAND <adrien@systerel.fr>
2021-06-14 16:51:44 +0200

    Ticket #0: removing non needed sudo

--- samples/PubSub_ClientServer/zephyr/pubsub_test_server/renode/scripts/prepare.sh
+++ samples/PubSub_ClientServer/zephyr/pubsub_test_server/renode/scripts/prepare.sh
@@ -1,4 +1,4 @@
 renode --disable-xwt --console -e 'emulation CreateTap "tap0" "tap" true; quit'
-sudo ip addr add 192.0.2.2/24 dev tap0
-sudo ip link set tap0 up
-sudo ip route add 232.0.0.0/8 dev tap0
+ip addr add 192.0.2.2/24 dev tap0
+ip link set tap0 up
+ip route add 232.0.0.0/8 dev tap0
