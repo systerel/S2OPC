92d03b4f425230e541ebf74bc8138502b182bccd
Pierre-Antoine <brameret@systerel.fr>
2021-06-16 15:46:26 +0200

    Ticket #849: Peer-review, a first effort to document SOPC_RealTime

--- src/Common/helpers_platform_dep/linux/p_time.h
+++ src/Common/helpers_platform_dep/linux/p_time.h
@@ -17,6 +17,18 @@
  * under the License.
  */
 
+/** \file
+ *
+ * PubSub publications require a time that has finer resolution than C99 time_t.
+ * The OPC UA type *Duration* is a double representing ms delays between publications.
+ * It's resolution goes finer than the nanosecond resolution.
+ *
+ * The SOPC_RealTime type should be defined to be as precise as possible to support sub-milliseconds publications.
+ *
+ * Under Linux, the implementation should use CLOCK_MONOTONIC, as we want to send regular notifications,
+ * even if the system time gets back or forth (e.g. because of ntp).
+ */
+
 #ifndef SOPC_P_TIME_H_
 #define SOPC_P_TIME_H_
 
@@ -24,20 +36,33 @@
 #include <stdint.h>
 #include <time.h>
 
-/* TODO: generalize the precise time interface */
-/* CLOCK_MONOTONIC under linux, as we want to send regular notifications, even if the time gets back or forth */
-typedef struct timespec SOPC_RealTime; /* Or SensitiveTime or SpecialTime or PreciseTime */
+/** Type used to measure milliseconds and sub-millisecond times */
+typedef struct timespec SOPC_RealTime;
 
-/* ret = now if copy is NULL */
+/** Create a new timestamp
+ *
+ * \return the current time if copy is NULL
+ */
 SOPC_RealTime* SOPC_RealTime_Create(const SOPC_RealTime* copy);
-bool SOPC_RealTime_GetTime(SOPC_RealTime* t);
 void SOPC_RealTime_Delete(SOPC_RealTime** t);
-/* t += double in ms */
+
+/** Store the current time in t.
+ *
+ * \return false if failed.
+ */
+bool SOPC_RealTime_GetTime(SOPC_RealTime* t);
+
+/** Performs t += duration (milliseconds) with highest available resolution on the system. */
 void SOPC_RealTime_AddDuration(SOPC_RealTime* t, double duration_ms);
-/* t <= now */
+
+/** Returns now >= t */
 bool SOPC_RealTime_IsExpired(const SOPC_RealTime* t, const SOPC_RealTime* now);
-/* Precise sleep until there, does not check that date is in the future
- * The calling thread must have appropriate scheduling policy and priority for precise timing */
+
+/** \brief Precise sleep until specified date.
+ *
+ * \note Does not check that date is in the future.
+ * \note The calling thread must have appropriate scheduling policy and priority for precise timing.
+ */
 bool SOPC_RealTime_SleepUntil(const SOPC_RealTime* date);
 
 #endif /* SOPC_P_TIME_H_ */
