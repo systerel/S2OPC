de4b71aa11d6e4a67cca627fa6887c0be09db303
Adrien COURNAND <adrien@systerel.fr>
2021-07-20 10:40:16 +0200

    Ticket #861: all the scripts in the sources

--- samples/PubSub_ClientServer/zephyr/pubsub_test_server/renode/scripts/pub.build.sh
+++ samples/PubSub_ClientServer/zephyr/pubsub_test_server/renode/scripts/pub.build.sh
@@ -1,7 +1,7 @@
 #Preparing for building
 rm -Rf pub
-cp ~/pubconfig.xml ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/data/config/config_pubsub_server.xml
-cp ~/pubprj.conf ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/zephyr/pubsub_test_server/prj.conf
+cp ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/data/config/pubconfig.xml ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/data/config/config_pubsub_server.xml
+cp ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/zephyr/pubsub_test_server/pubprj.conf ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/zephyr/pubsub_test_server/prj.conf
 
 #Generating static configuration file
 python3 modules/lib/s2opc/scripts/generate-s2opc_pubsub-static-config.py modules/lib/s2opc/samples/PubSub_ClientServer/data/config/config_pubsub_server.xml modules/lib/s2opc/samples/PubSub_ClientServer/zephyr/pubsub_test_server/src/pubsub_config_static.c
--- samples/PubSub_ClientServer/zephyr/pubsub_test_server/renode/scripts/sub.build.sh
+++ samples/PubSub_ClientServer/zephyr/pubsub_test_server/renode/scripts/sub.build.sh
@@ -1,7 +1,7 @@
 #Preparing for building
 rm -Rf sub
-cp ~/subconfig.xml ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/data/config/config_pubsub_server.xml
-cp ~/subprj.conf ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/zephyr/pubsub_test_server/prj.conf
+cp ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/data/config/subconfig.xml ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/data/config/config_pubsub_server.xml
+cp ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/zephyr/pubsub_test_server/subprj.conf ~/zephyrproject/modules/lib/s2opc/samples/PubSub_ClientServer/zephyr/pubsub_test_server/prj.conf
 
 #Generating static configuration file
 python3 modules/lib/s2opc/scripts/generate-s2opc_pubsub-static-config.py modules/lib/s2opc/samples/PubSub_ClientServer/data/config/config_pubsub_server.xml modules/lib/s2opc/samples/PubSub_ClientServer/zephyr/pubsub_test_server/src/pubsub_config_static.c
