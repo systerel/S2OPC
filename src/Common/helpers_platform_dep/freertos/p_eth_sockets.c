6746a91073c1657cba9eb2e8c5fb073d843d36b2
monfort <vincent.monfort@systerel.fr>
2021-12-15 17:20:43 +0100

    Ticket #895: PubSub Ethernet: add empty ethernet socket implem for FreeRTOS

---
+++ src/Common/helpers_platform_dep/freertos/p_eth_sockets.c
@@ -0,0 +1,104 @@
+/*
+ * Licensed to Systerel under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Systerel licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include "sopc_eth_sockets.h"
+
+#include "p_sockets.h"
+
+SOPC_ReturnStatus SOPC_ETH_Socket_CreateSendAddressInfo(const char* interfaceName,
+                                                        const char* destMACaddr,
+                                                        SOPC_ETH_Socket_SendAddressInfo** sendAddInfo)
+{
+    // TODO
+    (void) interfaceName;
+    (void) destMACaddr;
+    (void) sendAddInfo;
+    return SOPC_STATUS_NOT_SUPPORTED;
+}
+
+SOPC_ReturnStatus SOPC_ETH_Socket_CreateReceiveAddressInfo(const char* interfaceName,
+                                                           bool recvMulticast,
+                                                           const char* destMACaddr,
+                                                           const char* sourceMACaddr,
+                                                           SOPC_ETH_Socket_ReceiveAddressInfo** recvAddInfo)
+{
+    // TODO
+    (void) interfaceName;
+    (void) recvMulticast;
+    (void) destMACaddr;
+    (void) sourceMACaddr;
+    (void) recvAddInfo;
+    return SOPC_STATUS_NOT_SUPPORTED;
+}
+
+SOPC_ReturnStatus SOPC_ETH_Socket_CreateToReceive(SOPC_ETH_Socket_ReceiveAddressInfo* receiveAddrInfo,
+                                                  bool setNonBlocking,
+                                                  Socket* sock)
+{
+    // TODO
+    (void) receiveAddrInfo;
+    (void) setNonBlocking;
+    (void) sock;
+    return SOPC_STATUS_NOT_SUPPORTED;
+}
+
+SOPC_ReturnStatus SOPC_ETH_Socket_CreateToSend(SOPC_ETH_Socket_SendAddressInfo* sendAddrInfo,
+                                               bool setNonBlocking,
+                                               Socket* sock)
+{
+    // TODO
+    (void) sendAddrInfo;
+    (void) setNonBlocking;
+    (void) sock;
+    return SOPC_STATUS_NOT_SUPPORTED;
+}
+
+SOPC_ReturnStatus SOPC_ETH_Socket_SendTo(Socket sock,
+                                         const SOPC_ETH_Socket_SendAddressInfo* sendAddrInfo,
+                                         uint16_t etherType,
+                                         SOPC_Buffer* buffer)
+{
+    // TODO
+    (void) sock;
+    (void) sendAddrInfo;
+    (void) etherType;
+    (void) buffer;
+    return SOPC_STATUS_NOT_SUPPORTED;
+}
+
+SOPC_ReturnStatus SOPC_ETH_Socket_ReceiveFrom(Socket sock,
+                                              const SOPC_ETH_Socket_ReceiveAddressInfo* receiveAddrInfo,
+                                              bool checkEtherType,
+                                              uint16_t etherType,
+                                              SOPC_Buffer* buffer)
+{
+    // TODO
+    (void) sock;
+    (void) receiveAddrInfo;
+    (void) checkEtherType;
+    (void) etherType;
+    (void) buffer;
+    return SOPC_STATUS_NOT_SUPPORTED;
+}
+
+void SOPC_ETH_Socket_Close(Socket* sock)
+{
+    // TODO
+    (void) sock;
+}
