systerel/S2OPC

commit 041a5c369a7739f740fefa33a1290863739782e1

Ticket #845: Minor updates to increase readability, and correct GCC 9.3 static analysis

src/Common/helpers/sopc_log_manager.c
@@ -350,6 +350,7 @@ SOPC_Log_Instance* SOPC_Log_CreateFileInstance(const char* logDirPath,
     SOPC_Log_File* file = NULL;
     char* filePath = NULL;
     int res = 0;
+    FILE* hFile = NULL;
 
     // Check parameters valid
     if (logDirPath != NULL && logFileName != NULL && strlen(logFileName) > 0 &&
@@ -362,7 +363,7 @@ SOPC_Log_Instance* SOPC_Log_CreateFileInstance(const char* logDirPath,
     {
         file = SOPC_Malloc(sizeof(SOPC_Log_File));
         // Define file path and try to open it
-        if (file != NULL)
+        if (NULL != file)
         {
             file->pFile = NULL;
             file->nbFiles = 0;
@@ -377,9 +378,9 @@ SOPC_Log_Instance* SOPC_Log_CreateFileInstance(const char* logDirPath,
                 res = sprintf(filePath, "%s%s_%s_%05u.log", logDirPath, SOPC_CSTRING_UNIQUE_LOG_PREFIX, logFileName,
                               file->nbFiles);
                 assert(res > 0);
-                file->pFile = SOPC_Log_InstanceFileOpen(filePath);
+                hFile = SOPC_Log_InstanceFileOpen(filePath);
             }
-            if (NULL == file->pFile)
+            if (NULL == hFile)
             {
                 if (filePath != NULL)
                 {
@@ -390,17 +391,51 @@ SOPC_Log_Instance* SOPC_Log_CreateFileInstance(const char* logDirPath,
             }
             else
             {
-                setvbuf(file->pFile, NULL, _IOLBF, BUFSIZ);
+                setvbuf(hFile, NULL, _IOLBF, BUFSIZ);
             }
         }
-        if (NULL != file && NULL != file->pFile)
+        if (NULL != file)
         {
             file->filePath = filePath;
             file->maxBytes = maxBytes - RESERVED_BYTES_PRINT_FILE_CHANGE; // Keep characters to display file change
             file->maxFiles = maxFiles;
             file->nbBytes = 0;
             file->nbRefs = 1;
-            result->file = file;
+            file->pFile = hFile;
+
+            bool started = false;
+            SOPC_ReturnStatus mutex_res = Mutex_Initialization(&file->fileMutex);
+            if (mutex_res == SOPC_STATUS_OK)
+            {
+                result->file = file;
+                // Fill fields
+                SOPC_Log_AlignCategory(category, result);
+                result->consoleFlag = false;
+                result->level = SOPC_LOG_LEVEL_ERROR;
+                result->started = false;
+                // Ensure logCallback and callbackBuffer are initialized to 0 in case of FILE log.
+                result->logCallback = NULL;
+                result->callbackBuffer = 0;
+                // Starts the log instance
+                started = SOPC_Log_Start(result);
+            }
+
+            if (!started)
+            {
+                fclose(hFile);
+                if (mutex_res == SOPC_STATUS_OK)
+                {
+                    Mutex_Clear(&result->file->fileMutex);
+                }
+
+                SOPC_Free(result->file->filePath);
+                SOPC_Free(result->callbackBuffer);
+                result->callbackBuffer = NULL;
+
+                SOPC_Free(result->file);
+                SOPC_Free(result);
+                result = NULL;
+            }
         }
         else
         {
@@ -409,25 +444,6 @@ SOPC_Log_Instance* SOPC_Log_CreateFileInstance(const char* logDirPath,
         }
     }
 
-    if (result != NULL)
-    {
-        Mutex_Initialization(&result->file->fileMutex);
-        // Fill fields
-        SOPC_Log_AlignCategory(category, result);
-        result->consoleFlag = false;
-        result->level = SOPC_LOG_LEVEL_ERROR;
-        result->started = false;
-        // Ensure logCallback and callbackBuffer are initialized to 0 in case of FILE log.
-        result->logCallback = NULL;
-        result->callbackBuffer = 0;
-        // Starts the log instance
-        bool started = SOPC_Log_Start(result);
-
-        if (!started)
-        {
-            SOPC_Log_ClearInstance(&result);
-        }
-    }
     return result;
 }
 

samples/ClientServer/client_wrapper/client_subscription.c
@@ -306,6 +306,7 @@ static bool parse_options(cmd_line_options_t* o, int argc, char* const* argv)
     case val:                                        \
         free(o->field);                              \
         o->field = calloc(strlen(optarg) + 1, 1);    \
+        assert(NULL != o->field);                    \
         strcpy(o->field, optarg);                    \
         break;
 
@@ -362,11 +363,13 @@ static bool parse_options(cmd_line_options_t* o, int argc, char* const* argv)
     if (NULL == o->endpoint_url)
     {
         o->endpoint_url = malloc(strlen(DEFAULT_ENDPOINT_URL) + 1);
+        assert(NULL != o->endpoint_url);
         strcpy(o->endpoint_url, DEFAULT_ENDPOINT_URL);
     }
     if (NULL == o->policyId)
     {
         o->policyId = malloc(strlen(POLICY_ID) + 1);
+        assert(NULL != o->policyId);
         strcpy(o->policyId, POLICY_ID);
     }
     CONVERT_STR_OPT(publish_period, int64_t, PUBLISH_PERIOD_MS)
@@ -391,6 +394,7 @@ static bool parse_options(cmd_line_options_t* o, int argc, char* const* argv)
     for (int i = 0; i < o->node_ids_size; ++i)
     {
         o->node_ids[i] = malloc(strlen(argv[optind + i]) + 1);
+        assert(NULL != o->node_ids[i]);
         strcpy(o->node_ids[i], argv[optind + i]);
     }
 

samples/ClientServer/client_wrapper/examples/browse.c
@@ -99,14 +99,15 @@ int main(int argc, char* const argv[])
             printf("status: %d, nbOfResults: %d\n", browseResult.statusCode, browseResult.nbOfReferences);
             for (int32_t i = 0; i < browseResult.nbOfReferences; i++)
             {
+                const SOPC_ClientHelper_BrowseResultReference* ref = &browseResult.references[i];
                 printf("Item #%d\n", i);
-                printf("- nodeId: %s\n", browseResult.references[i].nodeId);
-                printf("- displayName: %s\n", browseResult.references[i].displayName);
+                printf("- nodeId: %s\n", ref->nodeId);
+                printf("- displayName: %s\n", ref->displayName);
 
-                free(browseResult.references[i].nodeId);
-                free(browseResult.references[i].displayName);
-                free(browseResult.references[i].browseName);
-                free(browseResult.references[i].referenceTypeId);
+                free(ref->nodeId);
+                free(ref->displayName);
+                free(ref->browseName);
+                free(ref->referenceTypeId);
             }
             free(browseResult.references);
         }

samples/ClientServer/client_wrapper/examples/get_endpoints.c
@@ -70,19 +70,21 @@ static void free_endpoints(SOPC_ClientHelper_GetEndpointsResult* result)
         {
             for (int32_t i = 0; i < result->nbOfEndpoints; i++)
             {
-                free(result->endpoints[i].endpointUrl);
-                free(result->endpoints[i].security_policyUri);
-                free(result->endpoints[i].transportProfileUri);
-                if (NULL != result->endpoints[i].userIdentityTokens)
+                const SOPC_ClientHelper_EndpointDescription* ep = &result->endpoints[i];
+                free(ep->endpointUrl);
+                free(ep->security_policyUri);
+                free(ep->transportProfileUri);
+                if (NULL != ep->userIdentityTokens)
                 {
-                    for (int32_t j = 0; j < result->endpoints[i].nbOfUserIdentityTokens; j++)
+                    for (int32_t j = 0; j < ep->nbOfUserIdentityTokens; j++)
                     {
-                        free(result->endpoints[i].userIdentityTokens[j].policyId);
-                        free(result->endpoints[i].userIdentityTokens[j].issuedTokenType);
-                        free(result->endpoints[i].userIdentityTokens[j].issuerEndpointUrl);
-                        free(result->endpoints[i].userIdentityTokens[j].securityPolicyUri);
+                        const SOPC_ClientHelper_UserIdentityToken* token = &ep->userIdentityTokens[j];
+                        free(token->policyId);
+                        free(token->issuedTokenType);
+                        free(token->issuerEndpointUrl);
+                        free(token->securityPolicyUri);
                     }
-                    free(result->endpoints[i].userIdentityTokens);
+                    free(ep->userIdentityTokens);
                 }
             }
             free(result->endpoints);

src/ClientServer/frontend/client_wrapper/state_machine.c
@@ -1167,6 +1167,7 @@ static bool StaMac_IsEventTargeted(SOPC_StaMac_Machine* pSM,
 
     bool bProcess = true;
     SOPC_SLinkedListIterator pListIter = NULL;
+    SOPC_StaMac_ReqCtx* reqCtx = NULL;
 
     if (NULL == pSM)
     {
@@ -1187,26 +1188,26 @@ static bool StaMac_IsEventTargeted(SOPC_StaMac_Machine* pSM,
     case SE_SND_REQUEST_FAILED:
         bProcess = false;
         pListIter = SOPC_SLinkedList_GetIterator(pSM->pListReqCtx);
-        while (!bProcess && NULL != pListIter)
+        reqCtx = (SOPC_StaMac_ReqCtx*) appCtx;
+        while (!bProcess && NULL != pListIter && NULL != reqCtx)
         {
-            if ((uintptr_t) SOPC_SLinkedList_Next(&pListIter) == appCtx)
+            if ((SOPC_StaMac_ReqCtx*) SOPC_SLinkedList_Next(&pListIter) == reqCtx)
             {
                 bProcess = true;
                 /* A response with a known pReqCtx shall free it, and return the appCtx to the caller */
                 if (NULL != pAppCtx)
                 {
-                    *pAppCtx = ((SOPC_StaMac_ReqCtx*) appCtx)->appCtx;
+                    *pAppCtx = reqCtx->appCtx;
                 }
                 if (NULL != pRequestScope)
                 {
-                    *pRequestScope = ((SOPC_StaMac_ReqCtx*) appCtx)->requestScope;
+                    *pRequestScope = reqCtx->requestScope;
                 }
-                if (NULL == SOPC_SLinkedList_RemoveFromId(pSM->pListReqCtx, ((SOPC_StaMac_ReqCtx*) appCtx)->uid))
+                if (NULL == SOPC_SLinkedList_RemoveFromId(pSM->pListReqCtx, reqCtx->uid))
                 {
                     Helpers_Log(SOPC_LOG_LEVEL_WARNING, "failed to pop the request from the pListReqCtx.");
                 }
-                SOPC_Free((void*) appCtx);
-                appCtx = 0;
+                SOPC_Free(reqCtx);
             }
         }
         break;

src/Common/crypto/mbedtls/key_manager_lib.c
@@ -810,23 +810,27 @@ SOPC_ReturnStatus SOPC_KeyManager_CertificateList_RemoveUnmatchedCRL(SOPC_Certif
             mbedtls_x509_crt_free(crt);
 
             /* Set new next certificate (if possible) */
-            if (NULL == prev && NULL == next)
+            if (NULL == prev)
             {
-                /*
-                 * The list would be empty, but we cannot do it here.
-                 * We have no choice but failing with current design.
-                 */
-                crt = NULL; // make iteration stop
-                status = SOPC_STATUS_NOK;
-            }
-            else if (NULL == prev && NULL != next)
-            {
-                /* Head of the chain is a special case */
-                pCert->crt = *next;
-                /* We have to free the new next certificate (copied as first one) */
-                SOPC_Free(next);
+                if (NULL == next)
 
-                /* Do not iterate: current certificate has changed */
+                {
+                    /*
+                     * The list would be empty, but we cannot do it here.
+                     * We have no choice but failing with current design.
+                     */
+                    crt = NULL; // make iteration stop
+                    status = SOPC_STATUS_NOK;
+                }
+                else
+                {
+                    /* Head of the chain is a special case */
+                    pCert->crt = *next;
+                    /* We have to free the new next certificate (copied as first one) */
+                    SOPC_Free(next);
+
+                    /* Do not iterate: current certificate has changed */
+                }
             }
             else
             {

commit 934e9b2e3429c17fc20438d81978c1e389be2322

Ticket #852: Minor fix for coverity leak in LOG_MANAGER
Note: leak introduced in commit "041a5c369"

src/Common/helpers/sopc_log_manager.c
@@ -432,7 +432,7 @@ SOPC_Log_Instance* SOPC_Log_CreateFileInstance(const char* logDirPath,
                 SOPC_Free(result->callbackBuffer);
                 result->callbackBuffer = NULL;
 
-                SOPC_Free(result->file);
+                SOPC_Free(file);
                 SOPC_Free(result);
                 result = NULL;
             }