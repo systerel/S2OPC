71bf5f38d096fde874a27d6d68ee9cc221ef85db
sauvegrain <sauvegrain@systerel.fr>
2022-03-16 11:06:31 +0100

    Ticket #0000: Adding contracts in session_request_handle_bs

--- src/ClientServer/services/b2c/session_request_handle_bs.c
+++ src/ClientServer/services/b2c/session_request_handle_bs.c
@@ -35,6 +35,52 @@ static constants__t_session_i client_requests[SOPC_MAX_PENDING_REQUESTS + 1];
 /* Store number of pending requests remaining for session */
 static uint32_t session_pending_requests_nb[SOPC_MAX_SESSIONS + 1];
 
+/*@ predicate Replaces{K,L}(constants__t_session_i* a, int n, constants__t_session_i val1, constants__t_session_i val2)
+  = \forall integer i; 1 <= i < n ==> \let ai = \at(a[i], K); \let bi = \at(a[i], L); (ai == val1 ==> bi == val2) &&
+  (ai != val1 ==> bi == ai);
+ */
+
+/*@ predicate Unchanged{K, L}(constants__t_session_i* a, int m, int n) =
+    \forall integer k; m <= k < n ==> \at(a[k], K) == \at(a[k], L);
+ */
+
+/*@ axiomatic CountSessionHandle
+  @ {
+  @     logic integer count_handle(constants__t_session_i* tab, integer start, integer n, constants__t_session_i val);
+  @
+  @     axiom count_zero:
+  @         \forall constants__t_session_i* tab, integer start, integer n, constants__t_session_i val;
+  n <= start ==> count_handle(tab, start, n, val) == 0;
+  @
+  @     axiom count_hit:
+  @         \forall constants__t_session_i* tab, integer start, integer n, constants__t_session_i val;
+  n >= start && tab[n] == val ==> count_handle(tab, start, n, val) + 1 == count_handle(tab, start, n+1, val);
+  @
+  @     axiom count_miss:
+  @         \forall constants__t_session_i* tab, integer start, integer n, constants__t_session_i val;
+  n >= start && tab[n] != val ==> count_handle(tab, start, n, val) == count_handle(tab, start, n+1, val);
+  @ }
+ */
+
+/*@ axiomatic HandleProperties
+  @ {
+  @     axiom no_handles:
+  @         \forall constants__t_session_i val; count_handle((constants__t_session_i*) client_requests, 1,
+  SOPC_MAX_PENDING_REQUESTS + 1, val) == 0 <==> session_pending_requests_nb[val] == 0;
+  @
+  @     axiom no_more_handles{L1, L2} :
+  @         \forall constants__t_session_i* tab, constants__t_session_i val, integer i;
+  \at(session_pending_requests_nb[val], L2) == 0 ==> Replaces{L1, L2}(tab, (int) i, val, (constants__t_session_i)
+  constants__c_session_indet);
+  @
+  @ }
+ */
+
+// Global invariant not supported yet, replaced with previous axiom
+/*@ global invariant NumberOfHandle: \forall constants__t_session_i val;
+  count_handle((constants__t_session_i*)client_requests, 1, SOPC_MAX_PENDING_REQUESTS + 1, val) <==>
+  session_pending_requests_nb[val]; */
+
 /*------------------------
    INITIALISATION Clause
   ------------------------*/
@@ -48,6 +94,23 @@ void session_request_handle_bs__INITIALISATION(void)
 /*--------------------
    OPERATIONS Clause
   --------------------*/
+/*@ requires \valid(client_requests+(session_request_handle_bs__req_handle));
+  @ requires \valid(session_pending_requests_nb + (session_request_handle_bs__session));
+  @ requires 0 <= session_request_handle_bs__session < SOPC_MAX_SESSIONS + 1;
+  @ requires 0 <= session_request_handle_bs__req_handle < SOPC_MAX_PENDING_REQUESTS + 1;
+  @ requires 0 <= session_pending_requests_nb[session_request_handle_bs__session] < SOPC_MAX_PENDING_REQUESTS;
+  @ // assert requirements, which are supposedly proven by B model
+  @ requires session_request_handle_bs__session != constants__c_session_indet;
+  @ requires session_request_handle_bs__req_handle != constants__c_client_request_handle_indet;
+  @ requires client_requests[session_request_handle_bs__req_handle] == constants__c_session_indet;
+  @
+  @ assigns client_requests[session_request_handle_bs__req_handle];
+  @ assigns session_pending_requests_nb[session_request_handle_bs__session];
+  @
+  @ ensures client_requests[session_request_handle_bs__req_handle] == session_request_handle_bs__session;
+  @ ensures session_pending_requests_nb[session_request_handle_bs__session] ==
+  (1+\old(session_pending_requests_nb[session_request_handle_bs__session]));
+ */
 void session_request_handle_bs__client_add_session_request_handle(
     const constants__t_session_i session_request_handle_bs__session,
     const constants__t_client_request_handle_i session_request_handle_bs__req_handle)
@@ -56,10 +119,30 @@ void session_request_handle_bs__client_add_session_request_handle(
     assert(session_request_handle_bs__req_handle != constants__c_client_request_handle_indet);
     // Request handle freshness is guaranteed by request_handle_bs,
     // in degraded cases an old session number could be overwritten here
+    assert(client_requests[session_request_handle_bs__req_handle] == constants__c_session_indet);
     client_requests[session_request_handle_bs__req_handle] = session_request_handle_bs__session;
     session_pending_requests_nb[session_request_handle_bs__session]++;
 }
 
+/*@ requires \valid(client_requests+(session_request_handle_bs__req_handle));
+  @ requires \valid(session_request_handle_bs__session);
+  @ requires 0 <= client_requests[session_request_handle_bs__req_handle] < SOPC_MAX_SESSIONS + 1;
+  @ requires 0 <= session_request_handle_bs__req_handle < SOPC_MAX_PENDING_REQUESTS + 1;
+  @ requires 0 < session_pending_requests_nb[client_requests[session_request_handle_bs__req_handle]] <=
+  SOPC_MAX_PENDING_REQUESTS;
+  @ // assert requirements, which are supposedly proven by B model
+  @ requires session_request_handle_bs__req_handle != constants__c_client_request_handle_indet;
+  @ requires session_pending_requests_nb[client_requests[session_request_handle_bs__req_handle]] > 0;
+  @
+  @ assigns *session_request_handle_bs__session;
+  @ assigns client_requests[session_request_handle_bs__req_handle];
+  @ assigns session_pending_requests_nb[client_requests[session_request_handle_bs__req_handle]];
+  @
+  @ ensures *session_request_handle_bs__session == \old(client_requests[session_request_handle_bs__req_handle]);
+  @ ensures client_requests[session_request_handle_bs__req_handle] == constants__c_session_indet;
+  @ ensures session_pending_requests_nb[*session_request_handle_bs__session] ==
+  \at(session_pending_requests_nb[\at(*session_request_handle_bs__session, Post)], Pre) - 1;
+ */
 void session_request_handle_bs__client_get_session_and_remove_request_handle(
     const constants__t_client_request_handle_i session_request_handle_bs__req_handle,
     constants__t_session_i* const session_request_handle_bs__session)
@@ -80,10 +163,47 @@ void session_request_handle_bs__client_get_session_and_remove_request_handle(
     }
 }
 
+/*@ requires \valid(session_pending_requests_nb+(session_request_handle_bs__session));
+  @ requires \valid(client_requests+(0 .. SOPC_MAX_PENDING_REQUESTS));
+  @ requires session_request_handle_bs__session != 0;
+  @
+  @ assigns client_requests[1 .. SOPC_MAX_PENDING_REQUESTS];
+  @ assigns session_pending_requests_nb[session_request_handle_bs__session];
+  @
+  @ ensures Replaces{Pre, Here}((constants__t_session_i*) client_requests, (int) (SOPC_MAX_PENDING_REQUESTS + 1),
+  session_request_handle_bs__session, (constants__t_session_i) constants__c_session_indet);
+  @ ensures \forall integer x; 1 <= x < (SOPC_MAX_PENDING_REQUESTS + 1) ==> client_requests[x] !=
+  session_request_handle_bs__session;
+  @ ensures \forall integer x; \old(client_requests[x]) == session_request_handle_bs__session ==>
+  client_requests[x] == constants__c_session_indet;
+  @ ensures session_pending_requests_nb[session_request_handle_bs__session] == 0;
+
+ */
+
 void session_request_handle_bs__client_remove_all_request_handles(
     const constants__t_session_i session_request_handle_bs__session)
 {
     assert(session_request_handle_bs__session != constants__c_session_indet);
+/*@ loop invariant 1 <= idx <= SOPC_MAX_PENDING_REQUESTS + 1;
+      @ loop invariant session_pending_requests_nb[session_request_handle_bs__session] >= 0;
+      @ loop invariant replaced: Replaces{Pre, Here}((constants__t_session_i*) client_requests, (int) idx,
+      session_request_handle_bs__session, (constants__t_session_i) constants__c_session_indet);
+      @ loop invariant unchanged: Unchanged{Pre, Here}((constants__t_session_i*) client_requests, (int) idx,
+      (int) (SOPC_MAX_PENDING_REQUESTS + 1));
+      @ loop invariant
+      \at(count_handle((constants__t_session_i*)client_requests, 1, idx, session_request_handle_bs__session), LoopEntry)
+      == \at(session_pending_requests_nb[session_request_handle_bs__session], LoopEntry) -
+      \at(session_pending_requests_nb[session_request_handle_bs__session], LoopCurrent);
+      @ loop invariant session_pending_requests_nb[session_request_handle_bs__session] == 0 ==>
+      count_handle((constants__t_session_i*)client_requests, (int)idx, (int)SOPC_MAX_PENDING_REQUESTS + 1,
+      session_request_handle_bs__session) == 0;
+      @
+      @ loop assigns idx;
+      @ loop assigns client_requests[1 .. SOPC_MAX_PENDING_REQUESTS];
+      @ loop assigns session_pending_requests_nb[session_request_handle_bs__session];
+      @ loop variant ((SOPC_MAX_PENDING_REQUESTS + 1) - idx) *
+      session_pending_requests_nb[session_request_handle_bs__session];
+     */
     for (uint32_t idx = 1;
          idx <= SOPC_MAX_PENDING_REQUESTS && session_pending_requests_nb[session_request_handle_bs__session] > 0; idx++)
     {
