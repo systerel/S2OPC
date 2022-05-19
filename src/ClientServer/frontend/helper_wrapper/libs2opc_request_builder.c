3e1b68b6bb6a1a72a87b66e9fe1e6b5ddf70fba1
Shahbaaz Ramjane <shahbaaz.ramjane@systerel.fr>
2022-05-19 10:40:35 +0200

    Ticket #830: refactor SetReadValue function :
    - remove the common section code
    
    - use the InitializeReadvalPointer function and check if there is no error
    
    - move SOPC_ReturnStatus variable and affect it the result of SOPC_NodeId function

--- src/ClientServer/frontend/helper_wrapper/libs2opc_request_builder.c
+++ src/ClientServer/frontend/helper_wrapper/libs2opc_request_builder.c
@@ -200,15 +200,12 @@ SOPC_ReturnStatus SOPC_ReadRequest_SetReadValue(OpcUa_ReadRequest* readRequest,
                                                 SOPC_AttributeId attribute,
                                                 SOPC_String* indexRange)
 {
-    if (!CHECK_ELEMENT_EXISTS(readRequest, NoOfNodesToRead, index) ||
-        SOPC_AttributeId_Invalid == SOPC_TypeHelperInternal_CheckAttributeId(attribute))
+    OpcUa_ReadValueId* readVal = ReadRequest_InitializeReadvalPointer(readRequest, index, attribute);
+    if (readVal == NULL)
     {
         return SOPC_STATUS_INVALID_PARAMETERS;
     }
-    SOPC_ReturnStatus status = SOPC_STATUS_OK;
-    OpcUa_ReadValueId* readVal = &readRequest->NodesToRead[index];
-    readVal->AttributeId = attribute;
-    status = SOPC_NodeId_Copy(&readVal->NodeId, nodeId);
+    SOPC_ReturnStatus status = SOPC_NodeId_Copy(&readVal->NodeId, nodeId);
     if (SOPC_STATUS_OK == status && NULL != indexRange)
     {
         status = SOPC_String_Copy(&readVal->IndexRange, indexRange);
