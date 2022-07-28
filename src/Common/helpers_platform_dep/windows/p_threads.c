a83f4aedd940aa925b57976c0449445168605d7c
Vincent Lacroix <vincent.lacroix@systerel.fr>
2022-07-28 11:45:06 +0200

    Ticket #852: Revert update to new MinGW Docker image.

--- src/Common/helpers_platform_dep/windows/p_threads.c
+++ src/Common/helpers_platform_dep/windows/p_threads.c
@@ -187,9 +187,7 @@ SOPC_ReturnStatus SOPC_Thread_Create(Thread* thread, void* (*startFct)(void*), v
         /* We need to check if the API is available at execution time */
         HMODULE kernel32 = LoadLibraryW(L"kernel32");
         assert(kernel32 != NULL);
-        /* This cast is needed to avoid an error linked to `-Wcast-function-type` flag with MinGW */
-        pSetThreadDescription funcAddress =
-            (pSetThreadDescription)(void (*)(void)) GetProcAddress(kernel32, "SetThreadDescription");
+        pSetThreadDescription funcAddress = (pSetThreadDescription) GetProcAddress(kernel32, "SetThreadDescription");
 
         if (NULL != funcAddress)
         {
