75cf0e0e9de82d92f9c4dabbf7f44ba0ad16fa0a
Robin Barrucand <robin.barrucand@systerel.fr>
2022-11-04 15:19:05 +0100

    Ticket #1027: review: update python tool to generate hash from an iteractive password

--- scripts/generate-password.py
+++ scripts/generate-password.py
@@ -20,115 +20,108 @@
 
 import argparse
 import os
+import getpass
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-from binascii import hexlify, b2a_base64
+from binascii import hexlify, unhexlify, b2a_base64, a2b_base64
 
+SHA256_DIGEST_SIZE_BYTES = 32
+SHA256_BLOCK_SIZE_BYTES = 64
 PBKDF2 = "PBKDF2"
-LIST_OF_SUPPORTED_ALGO = {PBKDF2: "PBKDF2 with HMAC-SHA-256"}
+SUPPORTED_ALGO = {PBKDF2: "PBKDF2 with HMAC-SHA-256"}
 
-class PBKDF2_HMAC_SHA256:
-    def __init__(self, pwd, salt, iter, outLen=32, base64=False):
-        self.pwd = pwd
-        self.salt = salt
-        self.iter = iter
-        self.outLen = outLen
-        self.out = None
-        self.base64 = base64
-        self._set_args()
+def print_supported_algo():
+    print("\nSupported algorithms:")
+    for algo, description in SUPPORTED_ALGO.items():
+        print("-{} ({})".format(algo, description))
+    print("\n")
 
-    def _set_args(self):
-        self.pwd = str.encode(self.pwd)          # password as bytes object
-        if not self.salt:
-            self.salt = os.urandom(16)
-        else:
-            self.salt = str.encode(self.salt) # salt as bytes
-    
-    def _check_args(self):
-        if len(self.pwd) > 64:
-            print("\ngenerate-password: password must not exceed 64 bytes because if the HMAC key is longer \
-                    than the blocks in the hash function (SH256 => 512 bits blocks size) then the password \
-                    is hashed beforehand, which can reduce the entropy of the derived output.\n")
-        if self.outLen < 0:
-            print("\ngenerate-password: hashlen must be positive value\n")
-            return False
-        if (self.outLen % 32) != 0:
-            print("\ngenerate-password: haslen must be a multiple of the digest size (32 bytes).\n")
-            return False
-        return True
-
-    
-    def _print(self):
-        if not self.out:
-            print("\ngenerate-password: Unexpected error\n")
-            return
-        if not self.base64:
-            print("\n\n*************************\n")
-            print("PBKDF2 with HMAC-SHA-256\n")
-            print("-iter[in]: {}".format(self.iter))
-            print("-salt[in]: {} ({} bytes)".format(hexlify(self.salt), len(self.salt)))
-            print("-password[in]: {} ({} bytes)".format(hexlify(self.pwd), len(self.pwd)))
-            print("-hash[out]: {} ({} bytes)".format(hexlify(self.out), len(self.out)))
-            print("\n*************************\n\n")
-        else:
-            print("\n\n*************************\n")
-            print("PBKDF2 with HMAC-SHA-256\n")
-            print("-iter[in]: {}".format(self.iter))
-            print("-salt[in]: {} ({} bytes)".format(b2a_base64(self.salt, newline=False), len(self.salt)))
-            print("-password[in]: {} ({} bytes)".format(b2a_base64(self.pwd, newline=False), len(self.pwd)))
-            print("-hash[out]: {} ({} bytes)".format(b2a_base64(self.out, newline=False), len(self.out)))
-            print("\n*************************\n\n")
-
-    def run(self):
-        if self._check_args():
-            kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=self.outLen, salt=self.salt, iterations=self.iter)
-            self.out = kdf.derive(self.pwd)
-            self._print()
-    
-
-class CryptoUser:
-    def __init__(self, args):
-        self.args = args
-        self._algo_class = None
-
-    def _print_supported_algo(self):
-        print("\nSupported algorithms:")
-        for algo, description in LIST_OF_SUPPORTED_ALGO.items():
-            print("-{} ({})".format(algo, description))
-        print("\n")
-
-    def check_arguments(self):
-        if self.args.list:
-            self._print_supported_algo()
-            return False
-        if self.args.algo not in LIST_OF_SUPPORTED_ALGO:
-            print("\ngenerate-password: Unrecognized algo '{}'".format(self.args.algo))
-            print("generate-password: Use --help for summary\n")
-            return False
-        return True
-    
-    def run_algo(self):
-        if self.args.algo == PBKDF2:
-            self._algo_class = PBKDF2_HMAC_SHA256(self.args.usr_pwd, self.args.salt, self.args.iter, self.args.hashlen, self.args.base64)
-            self._algo_class.run()
-
-
+def pbkdf2_print_result(pwd, salt, iter, result, base64_format=False):
+    print("\n\n*************************\n")
+    print("PBKDF2 with HMAC-SHA-256\n")
+    print("-iter[in]: {}".format(iter))
+    if base64_format:
+        print("-salt[in]: {} ({} bytes)".format(b2a_base64(salt, newline=False), len(salt)))
+        print("-hash[out]: {} ({} bytes)".format(b2a_base64(result, newline=False), len(result)))
+        print("\n*************************\n\n")
+    else:
+        print("-salt[in]: {} ({} bytes)".format(hexlify(salt), len(salt)))
+        print("-hash[out]: {} ({} bytes)".format(hexlify(result), len(result)))
+        print("\n*************************\n\n")
 
 def main():
 
-    argparser = argparse.ArgumentParser(description='Generate password from a secret')
-    argparser.add_argument('usr_pwd', type=str, help='Input user password (ascii encoding)')
-    argparser.add_argument('--salt', type=str, help='Desired user salt (ascii encoding, default 128 bits random value)')
+    argparser = argparse.ArgumentParser(description='This tool Generate a hash from a password, the output hash is useful to prepare the XML user \
+                                                    configuration file for server. Password is asking interactively.')
+    argparser.add_argument('--salt', type=str, help='Desired user salt (hexadecimal encoding, default 128 bits random value)')
     argparser.add_argument('--iter', type=int, default=1000000, help='Desired iteration count (default 1000000)')
-    argparser.add_argument('--hashlen', type=int, default=32, help='Desired output len  (octets, default 32)')
-    argparser.add_argument('--algo', type=str, default="PBKDF2", help='Desired algorithm (dafault PBKDF2)')
+    argparser.add_argument('--hashlen', type=int, default=SHA256_DIGEST_SIZE_BYTES, help='Desired output len  (default 32 bytes)')
+    argparser.add_argument('--algo', type=str, default=PBKDF2, help='Desired algorithm (dafault PBKDF2)')
     argparser.add_argument("-l", "--list", help="Display the list of supported algorithms with their descriptions", action='store_true')
-    argparser.add_argument("-b64", "--base64", help='Display output and salt with base64 encoding (dafault hex encoding)', action='store_true')
+    argparser.add_argument("-b", "--base64", help='Input salt in base64 encoding format as well as the result display (dafault hexadecimal encoding)', action='store_true')
     args = argparser.parse_args()
 
-    crypto_user = CryptoUser(args)
-    if crypto_user.check_arguments():
-        crypto_user.run_algo()
+    # Check if it is necessary to print the supported algorithm.
+    if args.list:
+        print_supported_algo()
+        return
+
+    # Check it the user algorithm is supported by the tool
+    if args.algo not in SUPPORTED_ALGO:
+        print("\ngenerate-password: Unrecognized algo '{}'".format(args.algo))
+        print("generate-password: Use --help for summary\n")
+        return
+    
+    # Get the right algorithm
+    if args.algo == PBKDF2:
+        # Check hashLen argument
+        if (args.hashlen % SHA256_DIGEST_SIZE_BYTES) != 0:
+            print("\ngenerate-password: hashlen must be a multiple of the digest size ({} bytes).\n".format(SHA256_DIGEST_SIZE_BYTES))
+            return
+        # Retrieve the password/salt and encode them as byte objects.
+        try:
+            # get password form interactive console
+            pwd_tmp = getpass.getpass()
+            pwd = getpass.getpass(prompt='Confirm your password:')
+        except:
+            print('\ngenerate-password: RUN_INTERACTIVELY environement variable is missing\n')
+            return
+        if pwd_tmp != pwd:
+            print('\ngenerate-password: ERROR: not the same password\n')
+            return
+        # from ascii string to byte object
+        pwd = str.encode(pwd)
+        if len(pwd) > SHA256_BLOCK_SIZE_BYTES:
+            print("\ngenerate-password: password should not exceed 64 bytes because if the HMAC key is longer \
+                    than the blocks in the hash function (SH256 => 512 bits blocks size) then the password \
+                    is hashed beforehand, which can reduce the entropy of the derived output.\n")
+
+        if not args.salt:
+            # If user doesn't defined the salt then generate it randomly
+            salt = os.urandom(16)
+        else:
+            if args.base64:
+                # User salt in base64 format
+                try:
+                    # from ascii base64 string to byte object
+                    salt = a2b_base64(args.salt)
+                except:
+                    print('\ngenerate-password: Non-base64 digit found for the salt\n')
+                    return
+            else:
+                # User salt in hexadecimal format
+                try:
+                    # from ascii hex to byte object
+                    salt = unhexlify(args.salt)
+                except:
+                    print('\ngenerate-password: Non-hexadecimal digit found for the salt\n')
+                    return
+
+        # Run PBKDF2
+        kdf = PBKDF2HMAC(algorithm=hashes.SHA256(), length=args.hashlen, salt=salt, iterations=args.iter)
+        result = kdf.derive(pwd)
+        # Print result 
+        pbkdf2_print_result(pwd, salt, args.iter, result, args.base64)
 
 if __name__ == '__main__':
     main()
