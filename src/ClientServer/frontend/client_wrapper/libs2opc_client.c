69ef96bf48ae04adc2002b2d4e2352176e8fc585
monfort <vincent.monfort@systerel.fr>
2022-12-23 17:56:36 +0100

    Ticket #1033: LibSub version: remove mention to subscribe library since it is S2OPC version

--- src/ClientServer/frontend/client_wrapper/libs2opc_client.c
+++ src/ClientServer/frontend/client_wrapper/libs2opc_client.c
@@ -45,7 +45,7 @@
 
 SOPC_LibSub_CstString SOPC_LibSub_GetVersion(void)
 {
-    return "S2OPC Toolkit subscribe library v" SOPC_TOOLKIT_VERSION;
+    return "S2OPC Toolkit library v" SOPC_TOOLKIT_VERSION;
 }
 
 SOPC_ReturnStatus SOPC_LibSub_Initialize(const SOPC_LibSub_StaticCfg* pCfg)
