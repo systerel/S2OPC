bd8db95f009e04a9aee365dcabd366bde0b11423
monfort <vincent.monfort@systerel.fr>
2022-12-23 18:51:40 +0100

    Ticket #993: generation B to C: add bconf to copied directories for local script

--- .pre-build-get-tooling.sh
+++ .pre-build-get-tooling.sh
@@ -35,7 +35,7 @@ cd "$TMP_TOOLING_DIR" > /dev/null
 # Copy tooling repository with git archive from server repository or local one
 if [[ -z "$TOOLING_DIR" ]]; then
     echo "Environment variable TOOLING_DIR not set => copy of tooling.git through SSH"
-    ssh pcb git --git-dir /git/c838/tooling.git archive master bin | tar x
+    ssh pcb git --git-dir /git/c838/tooling.git archive master bin bconf | tar x
 else
     echo "Environment variable TOOLING_DIR set to '$TOOLING_DIR' => copy of repository"
     git --git-dir "$TOOLING_DIR"/.git archive master bin bconf | tar x
