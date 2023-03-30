50bf2893b71e560c3c6bdf5ced6f8186055dc356
Nathan Lebeau <nathan.lebeau-stg@systerel.fr>
2023-03-30 14:59:26 +0200

    Ticket #1150: CI: Add cyclone docker images (rpi-build, build, check)

--- .docker-images.sh
+++ .docker-images.sh
@@ -20,10 +20,10 @@
 # Define the docker images used in S2OPC
 
 # Public images (registry.gitlab.com/systerel/s2opc)
-BUILD_IMAGE=sha256:12e94f0631ca8a3c9121d142a467351a89d120eaf6d1819e728a9d41d06e218b # build:1.34
+BUILD_IMAGE=sha256:40cd080b81dbfff47a595342c982153192c353964e0e3f708c41540085167f8d # build_cyclone:1.1
 MINGW_IMAGE=sha256:51364ca5cfa2d75219e616967f4969112c35758704f24738d0083e05f326e061 # mingw_build:1.13
-RPI_IMAGE=sha256:464cfa4e22af6d1933a17b14db182632dfd28febc3495adbd286aa1e5d8265e4 # rpi_build:1.5
-CHECK_IMAGE=sha256:55f5a3f10a3e6dd4b1c2d076df98c818eaeb749d623ef57208f6293061a7b8a4 # check 1.16
+RPI_IMAGE=sha256:8ec55cda0f0a951cffee641f7507cea0a4ed27373d9c0f30827cf7fe24aa468b # rpi-build_cyclone:1.1
+CHECK_IMAGE=sha256:f4cbb1c28938954cd56be8731088bcdf1ed41987ccfca6cf45002ed897e4014d # check_cyclone:1.1
 TEST_IMAGE=sha256:8aa26a1fc2f67f96dff5789e55373bd39e60dabca63acbfd9670707ebc171ee1 # test:2.14
 ZEPHYR_IMAGE=sha256:f7c2b2759954d9b876adfc933978b2974cba027f591878a876b27ebee1c0d54d # zephyr_build:v3.2.0-b
 
