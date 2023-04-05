c6a22bf5ff5e8c0aa0e94205f8f9779998aba481
Nicolas Beauger <nicolas.beauger@systerel.fr>
2023-04-05 16:03:20 +0200

    Ticket #1147: rename nodeset-address-space-utils with underscores
    Rename into nodeset_address_space_utils.
    
    The dashes '-' prevent from importing the module from another module,
    especially in tests.

--- samples/ClientServer/data/address_space/generate_demo_nodesets.sh
+++ samples/ClientServer/data/address_space/generate_demo_nodesets.sh
@@ -20,35 +20,35 @@
 set -euox pipefail
 
 # Generate demo NodeSet for Micro profile + node management
-../../../../scripts/nodeset-address-space-utils.py --output s2opc_node_mgt.xml.tmp s2opc_base_nodeset_origin.xml s2opc_demo_data_origin.xml s2opc_demo_data_perfs.xml
+../../../../scripts/nodeset_address_space_utils.py --output s2opc_node_mgt.xml.tmp s2opc_base_nodeset_origin.xml s2opc_demo_data_origin.xml s2opc_demo_data_perfs.xml
 cat licence.xml > s2opc_node_mgt.xml
 sed '1d' s2opc_node_mgt.xml.tmp >> s2opc_node_mgt.xml
 echo '' >> s2opc_node_mgt.xml
 rm s2opc_node_mgt.xml.tmp
 
 # Generate demo NodeSet for Micro profile
-../../../../scripts/nodeset-address-space-utils.py --output s2opc.xml.tmp --remove-max-node-management s2opc_base_nodeset_origin.xml s2opc_demo_data_origin.xml s2opc_demo_data_perfs.xml
+../../../../scripts/nodeset_address_space_utils.py --output s2opc.xml.tmp --remove-max-node-management s2opc_base_nodeset_origin.xml s2opc_demo_data_origin.xml s2opc_demo_data_perfs.xml
 cat licence.xml > s2opc.xml
 sed '1d' s2opc.xml.tmp >> s2opc.xml
 echo '' >> s2opc.xml
 rm s2opc.xml.tmp
 
 # Generate demo NodeSet for Micro profile + SKS
-../../../../scripts/nodeset-address-space-utils.py --output s2opc_sks.xml.tmp --remove-max-node-management s2opc_base_nodeset_origin.xml s2opc_base_sks_origin.xml s2opc_demo_data_origin.xml s2opc_demo_data_perfs.xml
+../../../../scripts/nodeset_address_space_utils.py --output s2opc_sks.xml.tmp --remove-max-node-management s2opc_base_nodeset_origin.xml s2opc_base_sks_origin.xml s2opc_demo_data_origin.xml s2opc_demo_data_perfs.xml
 cat licence.xml > s2opc_sks.xml
 sed '1d' s2opc_sks.xml.tmp >> s2opc_sks.xml
 echo '' >> s2opc_sks.xml
 rm s2opc_sks.xml.tmp
 
 # Generate demo NodeSet for Nano profile
-../../../../scripts/nodeset-address-space-utils.py --output s2opc_nano.xml.tmp --remove-max-monitored-items --remove-methods --remove-max-node-management s2opc_base_nodeset_origin.xml s2opc_demo_data_origin.xml s2opc_demo_data_perfs.xml
+../../../../scripts/nodeset_address_space_utils.py --output s2opc_nano.xml.tmp --remove-max-monitored-items --remove-methods --remove-max-node-management s2opc_base_nodeset_origin.xml s2opc_demo_data_origin.xml s2opc_demo_data_perfs.xml
 cat licence.xml > s2opc_nano.xml
 sed '1d' s2opc_nano.xml.tmp >> s2opc_nano.xml
 echo '' >> s2opc_nano.xml
 rm s2opc_nano.xml.tmp
 
 # Generate demo NodeSet with limited base info data
-../../../../scripts/nodeset-address-space-utils.py --output s2opc_no_base_info.xml.tmp --remove-node-ids-greater-than 3000 --remove-methods s2opc_base_nodeset_origin.xml s2opc_demo_data_origin.xml
+../../../../scripts/nodeset_address_space_utils.py --output s2opc_no_base_info.xml.tmp --remove-node-ids-greater-than 3000 --remove-methods s2opc_base_nodeset_origin.xml s2opc_demo_data_origin.xml
 cat licence.xml > s2opc_no_base_info.xml
 sed '1d' s2opc_no_base_info.xml.tmp >> s2opc_no_base_info.xml
 echo '' >> s2opc_no_base_info.xml
