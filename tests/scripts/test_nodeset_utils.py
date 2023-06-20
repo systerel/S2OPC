a126ca1703dd2aaab35312f421cd7d824cf85c3e
Nicolas Beauger <nicolas.beauger@systerel.fr>
2023-06-20 10:39:13 +0200

    Ticket #1147: add tests with split NS and duplicate node IDs

--- tests/scripts/test_nodeset_utils.py
+++ tests/scripts/test_nodeset_utils.py
@@ -42,30 +42,47 @@ def make_temp_dir():
     return tmp_dir
 
 
+ERROR = 'ERROR: '
+
+
 class MergeTests(unittest.TestCase):
+
+    def setUp(self)->None:
+        self.tmp_dir = make_temp_dir()
+
+    def tearDown(self):
+        if CLEANUP_TEMP:
+            shutil.rmtree(self.tmp_dir)
 
     def assert_content_equal(self, expected, actual):
         with open(expected, encoding='utf-8') as exp_file, open(actual, encoding='utf-8') as act_file:
             exp = exp_file.readlines()
             act = act_file.readlines()
-            if exp != act:
-                d = difflib.Differ()
-                self.fail(''.join(d.compare(exp, act)))
+            delta = difflib.unified_diff(exp, act, 'expected', 'actual')
+            d = ''.join(delta)
+            if len(d) > 0:
+                self.fail(d)
+
+    def _launch_merge(self, result_name, options, *src_paths):
+        parser = make_argparser()
+        actual = pj(self.tmp_dir, result_name)
+        res_src_paths = [pj(RESOURCE_DIR, src) for src in src_paths]
+        args = parser.parse_args([*res_src_paths, '-o', actual, *options])
+        run_merge(args)
+        return actual
 
     def run_test(self, result_name, options, *src_paths):
-        parser = make_argparser()
-        # TODO: with tempfile.TemporaryDirectory() as tmp_dir:
-        tmp_dir = make_temp_dir()
+        expected = pj(RESOURCE_DIR, 'expected', result_name)
+        actual = self._launch_merge(result_name, options, *src_paths)
+        self.assert_content_equal(expected, actual)
+
+    def run_error(self, expected_message, options, *src_paths):
         try:
-            expected = pj(RESOURCE_DIR, 'expected', result_name)
-            actual = pj(tmp_dir, result_name)
-            res_src_paths = [pj(RESOURCE_DIR, src) for src in src_paths]
-            args = parser.parse_args([*res_src_paths, '-o', actual, *options])
-            run_merge(args)
-            self.assert_content_equal(expected, actual)
-        finally:
-            if CLEANUP_TEMP:
-                shutil.rmtree(tmp_dir)
+            self._launch_merge(ERROR, options, *src_paths)
+        except Exception as e:
+            self.assertEqual(expected_message, str(e))
+            return
+        self.fail("An error was expected, but the merge succeeded")
 
     def test_ns0_alone(self):
         self.run_test('test_ns0_alone.xml', [],
@@ -82,7 +99,7 @@ class MergeTests(unittest.TestCase):
     def test_merge_pressure_with_ref_to_temperature(self):
         self.run_test('test_merge_pressure_with_ref_to_temperature.xml', [],
                       'ns0.xml', 'TestTemperatureNS.NodeSet2.xml', 'TestPressureNS_with_TemperatureNS.NodeSet2.xml')
-    
+
     def test_merge_with_ns_srv_arrays(self):
         self.run_test('test_merge_with_ns_srv_arrays.xml', [],
                       'ns0.xml', 'TestTemperatureNS_with_ns_srv_arrays.NodeSet2.xml', 'TestPressureNS_with_ns_srv_arrays.NodeSet2.xml')
@@ -91,6 +108,29 @@ class MergeTests(unittest.TestCase):
         # pressure is given before temperature
         self.run_test('test_merge_with_ns_srv_arrays2.xml', [],
                       'ns0.xml', 'TestPressureNS_with_ns_srv_arrays.NodeSet2.xml', 'TestTemperatureNS_with_ns_srv_arrays.NodeSet2.xml')
+
+    def test_merge_same_namespace_in_different_files(self):
+        # file1: temperature as NS1
+        # file2: temperature as NS1
+        self.run_test('test_merge_ns0_temperature.xml', [],
+                      'ns0.xml', 'TestTemperatureNS_split1.NodeSet2.xml', 'TestTemperatureNS_split2.NodeSet2.xml')
+
+    def test_merge_same_namespace_in_different_files_with_other_ns(self):
+        # file1: temperature as NS1, pressure as NS2
+        # file2: pressure as NS1
+        self.run_test('test_merge_ns0_temperature_pressure.xml', [],
+                      'ns0.xml', 'TestTemperatureNS_PressureNS_split1.NodeSet2.xml', 'TestPressureNS_split2.NodeSet2.xml')
+
+    def test_merge_error_same_node_id_in_one_file(self):
+        # file1: temperature as NS1 with duplicate node ID
+        self.run_error("There are duplicate Node IDs: ['ns=1;i=15001', 'ns=1;i=15006']", [],
+                      'ns0.xml', 'TestTemperatureNS_duplicate_nodeID.NodeSet2.xml')
+
+    def test_merge_error_same_node_id_in_two_files(self):
+        # file1: temperature as NS1, pressure as NS2
+        # file2: pressure as NS2 with duplicate node ID for pressure NS
+        self.run_error("There are duplicate Node IDs: ['ns=2;i=15014']", [],
+                      'ns0.xml', 'TestTemperatureNS_PressureNS_split1.NodeSet2.xml', 'TestPressureNS_split2_duplicate_nodeID.NodeSet2.xml')
 
     def test_remove_subtree(self):
         self.run_test('test_remove_subtree.xml', ['--remove-subtree', 'ns=1;i=15009'],
