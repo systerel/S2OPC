2d49def925c7786b941e7565c153c12ca398d30f
monfort <vincent.monfort@systerel.fr>
2023-10-02 12:49:09 +0200

    Ticket #1209: Demo root CAs: rework script of root CAs

--- samples/ClientServer/data/cert/generate_certs.sh
+++ samples/ClientServer/data/cert/generate_certs.sh
@@ -19,20 +19,12 @@
 
 
 set -e
-rm -f index.txt serial.txt
-touch index.txt
-touch index.txt.attr
-echo 00 > serial.txt  # Current serial
+
 CONF_FILE=cassl.cnf
 CONF_CLI=cli_req.cnf
 CONF_SRV=srv_req.cnf
 CA_KEY=cakey.pem
 CA_CERT=cacert.pem
-
-CONF_CA_USR=usr_cassl.cnf
-CONF_USR=usr_req.cnf
-CA_KEY_USR=user_cakey.pem
-CA_CERT_USR=user_cacert.pem
 
 DURATION=730
 
@@ -61,6 +53,9 @@ openssl ca -batch -config $CONF_FILE -policy signing_policy -extensions client_s
 openssl ca -batch -config $CONF_FILE -policy signing_policy -extensions server_signing_req -days $DURATION -in server_2k.csr -out server_2k_cert.pem
 openssl ca -batch -config $CONF_FILE -policy signing_policy -extensions server_signing_req -days $DURATION -in server_4k.csr -out server_4k_cert.pem
 
+# Remove the CSRs
+rm ./*.csr
+
 # Generate, for the client and the server, the encrypted private keys (these commands require the password).
 echo "****** Server private keys encryption ******"
 openssl rsa -in server_2k_key.pem -aes-256-cbc -out encrypted_server_2k_key.pem
@@ -72,47 +67,27 @@ openssl rsa -in client_4k_key.pem -aes-256-cbc -out encrypted_client_4k_key.pem
 # Remove the unencrypted keys
 rm client*_key.pem && rm server*_key.pem
 
+# Output application certificates in DER format
+for fradix in ca client_2k_ client_4k_ server_2k_ server_4k_; do
+    openssl x509 -in ${fradix}cert.pem -out ${fradix}cert.der -outform der
+done
+
 # Output hexlified certificate to include in check_crypto_certificates.c
-echo "Server signed public key:"
+echo "Server signed 2k public key:"
 hexdump -ve '/1 "%02x"' server_2k_cert.der
 echo
-echo -e "\nServer's thumbprint (SHA-1):"
+echo -e "\nServer's 2k certificate thumbprint (SHA-1):"
 openssl x509 -noout -fingerprint -in server_2k_cert.pem
 echo
-echo -e "\nCertificate Authority signed public key:"
+echo "Client signed 2k public key:"
+hexdump -ve '/1 "%02x"' client_2k_cert.der
+echo
+echo -e "\nClient's 2k certificate thumbprint (SHA-1):"
+openssl x509 -noout -fingerprint -in client_2k_cert.pem
+echo
+echo -e "\nApplication certificate Authority signed public key:"
 hexdump -ve '/1 "%02x"' cacert.der
 echo
 echo -e "\nCRL of the CA:"
 hexdump -ve '/1 "%02x"' cacrl.der
 echo
-
-# CA generation for users X509IdentityToken: generate key, generate self signed certificate
-# /!\ only for test as no pass phrase is embedeed
-openssl genrsa -out $CA_KEY_USR -aes-256-cbc 4096
-openssl req -config $CONF_CA_USR -new -x509 -key $CA_KEY_USR -out $CA_CERT_USR -days $DURATION
-
-# Generate an empty Certificate Revocation List, convert it to DER format
-openssl ca -config $CONF_CA_USR -gencrl -crldays $DURATION -out user_cacrl.pem
-openssl crl -in user_cacrl.pem -outform der -out user_cacrl.der
-
-# Generate user X509IdentityToken 2048 key lengths, a new key pair
-openssl req -config $CONF_USR -reqexts user_cert -sha256 -nodes -newkey rsa:2048 -keyout user_2k_key.pem -out user_2k.csr
-openssl req -config $CONF_USR -reqexts user_cert -sha256 -nodes -newkey rsa:4096 -keyout user_4k_key.pem -out user_4k.csr
-
-# And sign them, for the next century
-openssl ca -batch -config $CONF_CA_USR -policy signing_policy -extensions user_signing_req -days $DURATION -in user_2k.csr -out user_2k_cert.pem
-openssl ca -batch -config $CONF_CA_USR -policy signing_policy -extensions user_signing_req -days $DURATION -in user_4k.csr -out user_4k_cert.pem
-
-# Generate user encrypted private keys (these commands require the password).
-echo "****** User private keys encryption ******"
-openssl rsa -in user_2k_key.pem -aes-256-cbc -out encrypted_user_2k_key.pem
-openssl rsa -in user_4k_key.pem -aes-256-cbc -out encrypted_user_4k_key.pem
-
-# Remove the unencrypted keys
-rm user*_key.pem
-
-
-# Output application and user certificates in DER format
-for fradix in ca user_ca client_2k_ client_4k_ server_2k_ server_4k_ user_2k_ user_4k_; do
-    openssl x509 -in ${fradix}cert.pem -out ${fradix}cert.der -outform der
-done
