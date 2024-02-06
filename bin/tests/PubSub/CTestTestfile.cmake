7025186728d4bad36d59748446e2479ae77ba721
monfort <vincent.monfort@systerel.fr>
2024-02-06 11:02:17 +0100

    Ticket #1209: Add CTest test file for version S2OPC_Toolkit_1.5.0

---
+++ bin/tests/PubSub/CTestTestfile.cmake
@@ -0,0 +1,38 @@
+# CMake generated Testfile for 
+# Source directory: S2OPC_ROOT_DIR/tests/PubSub
+# Build directory: S2OPC_ROOT_DIR/tests/PubSub
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit::xml_parser_test "S2OPC_ROOT_DIR/bin/xml_parser_test")
+set_tests_properties(unit::xml_parser_test PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=xml_parser_test.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(unit::pubsub_modules_test "S2OPC_ROOT_DIR/bin/pubsub_modules_test")
+set_tests_properties(unit::pubsub_modules_test PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=pubsub_modules_test.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(unit::mqtt_pubsub_test "S2OPC_ROOT_DIR/bin/mqtt_pubsub_test")
+set_tests_properties(unit::mqtt_pubsub_test PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=mqtt_pubsub_test.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(validation::ll_pub_sub_test "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--publisher-cmd" "S2OPC_ROOT_DIR/bin/udp_pub_test" "S2OPC_ROOT_DIR/bin/udp_sub_test")
+set_tests_properties(validation::ll_pub_sub_test PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=ll_pub_sub_test.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(validation::ll_pub_sub_conf_test "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--publisher-cmd" "S2OPC_ROOT_DIR/bin/udp_pub_conf_test" "S2OPC_ROOT_DIR/bin/udp_sub_test")
+set_tests_properties(validation::ll_pub_sub_conf_test PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=ll_pub_sub_conf_test.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(validation::ll_pub_sub_xml_test "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--publisher-cmd" "S2OPC_ROOT_DIR/bin/udp_pub_xml_test" "S2OPC_ROOT_DIR/bin/sub_scheduler_conf_test")
+set_tests_properties(validation::ll_pub_sub_xml_test PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=ll_pub_sub_xml_test.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(validation::pubsub_scheduler_udp "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--publisher-cmd" "S2OPC_ROOT_DIR/bin/pub_scheduler_test" "S2OPC_ROOT_DIR/bin/sub_scheduler_conf_test")
+set_tests_properties(validation::pubsub_scheduler_udp PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=pubsub_scheduler_udp.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(validation::pubsub_sched_ethernet_ROOT_required "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--no-wait-pub-message" "--publisher-cmd" "S2OPC_ROOT_DIR/bin/pub_scheduler_test S2OPC_ROOT_DIR/bin/config_pub_scheduler_eth.xml" "S2OPC_ROOT_DIR/bin/sub_scheduler_conf_test" "S2OPC_ROOT_DIR/bin/config_sub_eth.xml")
+set_tests_properties(validation::pubsub_sched_ethernet_ROOT_required PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=pubsub_sched_ethernet.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(validation::pubsub_sched_ethernet_with_recv_itf "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--no-wait-pub-message" "--publisher-cmd" "S2OPC_ROOT_DIR/bin/pub_scheduler_test S2OPC_ROOT_DIR/bin/config_pub_scheduler_eth.xml" "S2OPC_ROOT_DIR/bin/sub_scheduler_conf_test" "S2OPC_ROOT_DIR/bin/config_sub_eth2.xml")
+set_tests_properties(validation::pubsub_sched_ethernet_with_recv_itf PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=pubsub_sched_ethernet_with_recv_itf.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(validation::pubsub_sched_ethernet_pub_config_err "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "S2OPC_ROOT_DIR/bin/pub_scheduler_test" "S2OPC_ROOT_DIR/bin/config_pub_scheduler_eth_err.xml")
+set_tests_properties(validation::pubsub_sched_ethernet_pub_config_err PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=pubsub_sched_ethernet_pub_config_err.tap;CK_EXP_RET_CODE=255" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(validation::pubsub_sched_ethernet_sub_config_err "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--no-wait-pub-message" "--publisher-cmd" "S2OPC_ROOT_DIR/bin/pub_scheduler_test S2OPC_ROOT_DIR/bin/config_pub_scheduler_eth.xml" "S2OPC_ROOT_DIR/bin/sub_scheduler_conf_test" "S2OPC_ROOT_DIR/bin/config_sub_eth_err.xml")
+set_tests_properties(validation::pubsub_sched_ethernet_sub_config_err PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=pubsub_sched_ethernet_sub_config_err.tap;CK_EXP_RET_CODE=255" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(validation::ll_multi_pub_sub_test "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--publisher-cmd" "S2OPC_ROOT_DIR/bin/udp_multi_pub_test" "S2OPC_ROOT_DIR/bin/udp_multi_sub_test")
+set_tests_properties(validation::ll_multi_pub_sub_test PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=ll_multi_pub_sub_test.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(validation::pubsub_server_test.py "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--sks-cmd" "S2OPC_ROOT_DIR/bin/test_server_sks" "--publisher-cmd" "S2OPC_ROOT_DIR/bin/pubsub_server" "python3" "S2OPC_ROOT_DIR/tests/PubSub/validation_tools/pubsub_server_test.py" "--tap" "pubsub_server_test.tap")
+set_tests_properties(validation::pubsub_server_test.py PROPERTIES  WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(interop::open62541_pub "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--publisher-cmd" "S2OPC_ROOT_DIR/tests/PubSub/bin_interop/open62541_pub" "S2OPC_ROOT_DIR/bin/interop_sub_test")
+set_tests_properties(interop::open62541_pub PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=interop_pub_test.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(interop::open62541_sub "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--publisher-cmd" "S2OPC_ROOT_DIR/bin/pub_scheduler_test S2OPC_ROOT_DIR/bin/config_pub_interop.xml" "S2OPC_ROOT_DIR/tests/PubSub/bin_interop/open62541_sub")
+set_tests_properties(interop::open62541_sub PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=interop_sub_test.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
+add_test(interop::open62541_pub_encrypted "python3" "S2OPC_ROOT_DIR/tests/scripts/tap-wrap.py" "python3" "S2OPC_ROOT_DIR/tests/PubSub/scripts/with-publisher.py" "--publisher-cmd" "S2OPC_ROOT_DIR/tests/PubSub/bin_interop/open62541_pub_encrypted" "S2OPC_ROOT_DIR/bin/interop_sub_test" "encrypted")
+set_tests_properties(interop::open62541_pub_encrypted PROPERTIES  ENVIRONMENT "CK_TAP_LOG_FILE_NAME=interop_pub_encrypted_interop.tap" WORKING_DIRECTORY "S2OPC_ROOT_DIR/bin")
