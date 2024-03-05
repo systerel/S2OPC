b7140c32da8753a9122311e40a291eab1dd3088b
Eloïse Rotondo <eloise.rotondo@systerel.fr>
2024-03-05 11:09:40 +0100

    Ticket #1057: Creation of base64 unit tests

---
+++ tests/ClientServer/unit_tests/helpers/check_base64.c
@@ -0,0 +1,147 @@
+/*
+ * Licensed to Systerel under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work
+ * for additional information regarding copyright ownership.
+ * Systerel licenses this file to you under the Apache
+ * License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <assert.h>
+#include <check.h>
+#include <stdio.h>
+
+#include "check_helpers.h"
+#include "opcua_statuscodes.h"
+#include "sopc_helper_encode.h"
+#include "sopc_mem_alloc.h"
+
+START_TEST(test_decode_with_number)
+{
+    SOPC_ReturnStatus status = 0;
+    const char* input = "VGhpc0lzQUJhc2U2NFRlc3Q=";
+    unsigned char* buffer = NULL;
+    size_t outLen = 0;
+    status = SOPC_HelperDecode_Base64(input, &buffer, &outLen);
+    ck_assert_int_eq(SOPC_STATUS_OK, status);
+    ck_assert_int_eq(17, (int) outLen);
+    int result = memcmp(buffer, "ThisIsABase64Test", strlen("ThisIsABase64Test"));
+    ck_assert_int_eq(0, result);
+    SOPC_Free(buffer);
+}
+END_TEST
+
+START_TEST(test_decode_without_padding)
+{
+    SOPC_ReturnStatus status = 0;
+    const char* input = "Tm9QYWRkaW5n";
+    unsigned char* buffer = NULL;
+    size_t outLen = 0;
+    status = SOPC_HelperDecode_Base64(input, &buffer, &outLen);
+    ck_assert_int_eq(SOPC_STATUS_OK, status);
+    ck_assert_int_eq(9, (int) outLen);
+    int result = memcmp(buffer, "NoPadding", strlen("NoPadding"));
+    ck_assert_int_eq(0, result);
+    SOPC_Free(buffer);
+}
+END_TEST
+
+START_TEST(test_decode_with_one_pad_char)
+{
+    SOPC_ReturnStatus status = 0;
+    const char* input = "VGhpc0lzQVRlc3Q=";
+    unsigned char* buffer = NULL;
+    size_t outLen = 0;
+    status = SOPC_HelperDecode_Base64(input, &buffer, &outLen);
+    ck_assert_int_eq(SOPC_STATUS_OK, status);
+    ck_assert_int_eq(11, (int) outLen);
+    int result = memcmp(buffer, "ThisIsATest", outLen);
+    ck_assert_int_eq(0, result);
+    SOPC_Free(buffer);
+}
+END_TEST
+
+START_TEST(test_decode_with_two_pad_char)
+{
+    SOPC_ReturnStatus status = 0;
+    const char* input = "VHdvUGFkZGluZw==";
+    unsigned char* buffer = NULL;
+    size_t outLen = 0;
+    status = SOPC_HelperDecode_Base64(input, &buffer, &outLen);
+    ck_assert_int_eq(SOPC_STATUS_OK, status);
+    ck_assert_int_eq(10, (int) outLen);
+    int result = memcmp(buffer, "TwoPadding", strlen("TwoPadding"));
+    ck_assert_int_eq(0, result);
+    SOPC_Free(buffer);
+}
+END_TEST
+
+START_TEST(test_decode_with_three_pad_char)
+{
+    SOPC_ReturnStatus status = 0;
+    const char* input = "abcde===";
+    unsigned char* buffer = NULL;
+    size_t outLen = 0;
+    status = SOPC_HelperDecode_Base64(input, &buffer, &outLen);
+    ck_assert_int_eq(SOPC_STATUS_NOK, status);
+    SOPC_Free(buffer);
+}
+END_TEST
+
+START_TEST(test_decode_char_after_pad_char)
+{
+    SOPC_ReturnStatus status = 0;
+    const char* input = "abc=de";
+    unsigned char* buffer = NULL;
+    size_t outLen = 0;
+    status = SOPC_HelperDecode_Base64(input, &buffer, &outLen);
+    ck_assert_int_eq(SOPC_STATUS_NOK, status);
+    SOPC_Free(buffer);
+}
+END_TEST
+
+START_TEST(test_decode_char_after_pad_char_with_end_pad)
+{
+    SOPC_ReturnStatus status = 0;
+    const char* input = "abc=de=";
+    unsigned char* buffer = NULL;
+    size_t outLen = 0;
+    status = SOPC_HelperDecode_Base64(input, &buffer, &outLen);
+    ck_assert_int_eq(SOPC_STATUS_NOK, status);
+    SOPC_Free(buffer);
+}
+END_TEST
+
+Suite* tests_make_suite_base64(void)
+{
+    Suite* s;
+    TCase *tc_correct_format, *tc_wrong_format;
+
+    s = suite_create("Base64");
+
+    tc_correct_format = tcase_create("Correct Format");
+    tcase_add_test(tc_correct_format, test_decode_without_padding);
+    tcase_add_test(tc_correct_format, test_decode_with_one_pad_char);
+    tcase_add_test(tc_correct_format, test_decode_with_two_pad_char);
+    tcase_add_test(tc_correct_format, test_decode_with_number);
+    suite_add_tcase(s, tc_correct_format);
+
+    tc_wrong_format = tcase_create("Wrong Format");
+    tcase_add_test(tc_wrong_format, test_decode_with_three_pad_char);
+    tcase_add_test(tc_wrong_format, test_decode_char_after_pad_char);
+    tcase_add_test(tc_wrong_format, test_decode_char_after_pad_char_with_end_pad);
+
+    suite_add_tcase(s, tc_wrong_format);
+
+    return s;
+}
