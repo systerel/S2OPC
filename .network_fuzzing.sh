25eb491ff2413544c585833bb46a452ad5e29175
Eloïse Rotondo <eloise.rotondo@systerel.fr>
2025-01-03 11:24:34 +0100

    Ticket #1548: Update network fuzzer CI

--- .network_fuzzing.sh
+++ .network_fuzzing.sh
@@ -21,37 +21,15 @@ set -o errexit
 set -o nounset                                                                                          
 set -o pipefail 
 
-#Increase the number of sessions.
-sed -i -e 's/\(#define SOPC_MAX_SOCKETS \)\(150\)/\11500/' -e 's/\(#define SOPC_MAX_SOCKETS_CONNECTIONS \)\(50\)/\11500/' src/ClientServer/configuration/sopc_toolkit_config_constants.h
-sed -i -e 's/\(#define SOPC_MAX_SECURE_CONNECTIONS \)\(21\)/\11001/' -e 's/\(#define SOPC_MAX_SESSIONS \)\(20\)/\11000/' src/ClientServer/configuration/sopc_toolkit_config_constants.h
-
-sed -i 's/\(#define SOPC_MAX_TIMERS \)\(UINT8_MAX\)/\1UINT16_MAX/' src/Common/configuration/sopc_common_constants.h
-
-#Rebuild S2OPC
-./build.sh && cd build && cmake --build . --target install || exit $?
-
-#Change encrypt server key for server key without password.
-cd bin/server_private
-echo "${TEST_PASSWORD_PRIVATE_KEY}" > secret.txt
-openssl rsa -in encrypted_server_4k_key.pem -passin file:secret.txt -out server_4k_key.pem
-cd ..
-sed -i -e 's/encrypted_server_4k_key.pem/server_4k_key.pem/' -e 's/\(encrypted=\)\("true"\)/\1"false"/' S2OPC_Server_Demo_Config.xml 
-
-#Change the type of server of the demo server.
-sed -i '\,</ApplicationDescription>,i <ApplicationType type="DiscoveryServer"/>' S2OPC_Server_Demo_Config.xml
-
-#Download opcua_network_fuzzer requirements
-python3 -m pip install -r /work/opcua_network_fuzzer-main/requirements.txt
-
 # Fuzz all possible requests 
-export TEST_SERVER_XML_CONFIG=./S2OPC_Server_Demo_Config.xml TEST_SERVER_XML_ADDRESS_SPACE=./S2OPC_Demo_NodeSet.xml TEST_USERS_XML_CONFIG=./S2OPC_Users_Demo_Config.xml
+export TEST_SERVER_XML_CONFIG=../../tests/ClientServer/data/config/S2OPC_Server_Demo_Config_Fuzzer.xml TEST_SERVER_XML_ADDRESS_SPACE=./S2OPC_Demo_NodeSet.xml TEST_USERS_XML_CONFIG=./S2OPC_Users_Demo_Config.xml
 request_list=(read_request browse_request create_subscription_request browse_next_request add_nodes_request)
-cd ../..
+
 for request in "${request_list[@]}"
 do 
     echo FUZZING "$request"
     # Timeout is set to 4 hours for every request
-    if python3 tests/ClientServer/scripts/with-opc-server.py --server-wd build/bin --server-cmd ./toolkit_demo_server --wait-timeout 14400 "python3" /work/opcua_network_fuzzer-main/opcua_fuzzer.py --target_host_ip localhost --target_host_port 4841 --target_app_name s2opc --r "$request" ; then
+    if python3 tests/ClientServer/scripts/with-opc-server.py --server-wd build_fuzz/bin --server-cmd ./toolkit_demo_server --wait-timeout 14400 "python3" /work/opcua_network_fuzzer-main/opcua_fuzzer.py --target_host_ip localhost --target_host_port 4841 --target_app_name s2opc --r "$request" ; then
         exit_status=$?
         echo fuzzing of "$request" ended with SUCCESS! 
         echo Exit status : $exit_status
