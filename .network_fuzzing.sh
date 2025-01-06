9fcffc3dff8d8b6a4b59c74cefe443ecb0649ac0
Eloïse Rotondo <eloise.rotondo@systerel.fr>
2025-01-06 11:07:18 +0100

    Ticket #1548: Modifications after peer review

--- .network_fuzzing.sh
+++ .network_fuzzing.sh
@@ -21,6 +21,13 @@ set -o errexit
 set -o nounset                                                                                          
 set -o pipefail 
 
+# Set Path to server binary
+DIRECTORY_PATH=$1
+[ -z  ${DIRECTORY_PATH} ] && echo "First argument must be the path to the directory where the server should run in" && exit 1
+[ ! -f ${DIRECTORY_PATH}"/toolkit_demo_server" ] && echo " ${DIRECTORY_PATH}/toolkit_demo_server file doesn't exist. Please give a path to a valid opc ua server binary" && exit 2
+
+FUZZER_WD=/work/opcua_network_fuzzer-main
+
 # Fuzz all possible requests 
 export TEST_SERVER_XML_CONFIG=../../tests/ClientServer/data/config/S2OPC_Server_Demo_Config_Fuzzer.xml TEST_SERVER_XML_ADDRESS_SPACE=./S2OPC_Demo_NodeSet.xml TEST_USERS_XML_CONFIG=./S2OPC_Users_Demo_Config.xml
 request_list=(read_request browse_request create_subscription_request browse_next_request add_nodes_request)
@@ -28,8 +35,8 @@ request_list=(read_request browse_request create_subscription_request browse_nex
 for request in "${request_list[@]}"
 do 
     echo FUZZING "$request"
-    # Timeout is set to 4 hours for every request
-    if python3 tests/ClientServer/scripts/with-opc-server.py --server-wd build_fuzz/bin --server-cmd ./toolkit_demo_server --wait-timeout 14400 "python3" /work/opcua_network_fuzzer-main/opcua_fuzzer.py --target_host_ip localhost --target_host_port 4841 --target_app_name s2opc --r "$request" ; then
+    # Launch the fuzzing process in opcua_fuzzer.py file. Timeout is set to 4 hours for every request
+    if python3 tests/ClientServer/scripts/with-opc-server.py --server-wd ${DIRECTORY_PATH} --server-cmd ./toolkit_demo_server --wait-timeout 14400 "python3" ${FUZZER_WD}/opcua_fuzzer.py --target_host_ip localhost --target_host_port 4841 --target_app_name s2opc --r "$request" ; then
         exit_status=$?
         echo fuzzing of "$request" ended with SUCCESS! 
         echo Exit status : $exit_status
