ac63be57b848b1d939e275c5df60f5112d4564ed
Lebeau <nathan.lebeau@systerel.fr>
2025-01-22 17:53:10 +0100

    Ticket #1548: Regenerate selfsigned cert with 10 years expiration and modify generation script

--- samples/ClientServer/data/cert/selfsigned/generate_selfsigned_cert.sh
+++ samples/ClientServer/data/cert/selfsigned/generate_selfsigned_cert.sh
@@ -17,6 +17,11 @@
 # specific language governing permissions and limitations
 # under the License.
 
+# This script is used to generate CA root application certificate,
+# with or without generation of new keys. Usage: 
+# - Generate CA root application certificate with generation of new key (default behaviour): "./generate_selfsigned_certs.sh"
+# - Generate CA root application certificate with existing keys: "./generate_selfsigned_certs.sh --no-key-generation"
+
 set -e
 
 CONF_FILE=ca_selfsigned_pathLen0ssl.cnf
@@ -25,10 +30,22 @@ CA_CERT=ca_selfsigned_pathLen0.pem
 
 DURATION=730
 
-# CA generation: generate key, generate self signed certificate
-# /!\ CA key encrypted with AES-256-CBC, these commands require the password.
-openssl genrsa -traditional -out $CA_KEY -aes-256-cbc 4096
-openssl req -config $CONF_FILE -new -x509 -key $CA_KEY -out $CA_CERT -days $DURATION
+# If no argument provided, default behaviour: CA root application certificate with new key.
+if [ $# == 0 ]; then
+    # CA generation: generate key, generate self signed certificate
+    # /!\ CA key encrypted with AES-256-CBC, these commands require the password.
+    openssl genrsa -traditional -out $CA_KEY -aes-256-cbc 4096
+    openssl req -config $CONF_FILE -new -x509 -key $CA_KEY -out $CA_CERT -days $DURATION
+
+# If argument --no-key-generation provided: renew CA root application certificate only (with existing key)
+elif [ $# == 1 ] && [ $1 == "--no-key-generation" ]; then
+    openssl req -new -config $CONF_FILE -new -x509 -key $CA_KEY -out $CA_CERT -days $DURATION
+
+# If invalid argument(s) provided: print usage and return
+else
+    echo "Please provide valid argument: --no-key-generation or no argument at all"
+    exit 1
+fi
 
 # Convert the certificate to DER format
 openssl x509 -in $CA_CERT -out ca_selfsigned_pathLen0.der -outform DER
