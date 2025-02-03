systerel/S2OPC

commit 4afed9eb69303e55e74cead5fbf0a0e9a5329cb4

Ticket #1561: Events: provide a way to defined an Event instance NodeId
Reserve the empty browse path to store the event/node instance NodeId (necessary for ConditionId in part 9)

src/ClientServer/address_space/sopc_event_manager.c
@@ -36,8 +36,10 @@
 #include "sopc_time_reference.h"
 #include "sopc_types.h"
 
-#define QN_PATH_SEPARATOR_CHAR '~'
-#define QN_PATH_SEPARATOR_STR "~"
+/* A reserved entry for empty path is defined to store the event NodeId
+ * which shall be the event/node instance
+ * NodeId if it exists in address space (see ConditionId in part 9) */
+#define QN_EMPTY_PATH_NODEID "OwnNodeId"
 
 typedef struct SOPC_Event_Variable
 {
@@ -54,7 +56,8 @@ struct _SOPC_Event
 /* Reserved index for variants for any event */
 typedef enum SOPC_BaseEventVariantIndexes
 {
-    SOPC_BaseEventVariantIdx_EventId = 0,
+    SOPC_BaseEventVariantIdx_NodeId = 0,
+    SOPC_BaseEventVariantIdx_EventId,
     SOPC_BaseEventVariantIdx_EventType,
     SOPC_BaseEventVariantIdx_SourceNode,
     SOPC_BaseEventVariantIdx_SourceName,
@@ -68,11 +71,14 @@ typedef enum SOPC_BaseEventVariantIndexes
 
 /* Statically defined variants for any event */
 static const char* sopc_baseEventVariantsPaths[SOPC_BaseEventVariantIdx_ReservedLength] = {
-    "0:EventId",     "0:EventType", "0:SourceNode", "0:SourceName", "0:Time",
-    "0:ReceiveTime", "0:LocalTime", "0:Message",    "0:Severity"};
+    QN_EMPTY_PATH_NODEID, "0:EventId",   "0:EventType", "0:SourceNode", "0:SourceName", "0:Time",
+    "0:ReceiveTime",      "0:LocalTime", "0:Message",   "0:Severity"};
 
 // Note: cannot be const because MS build does not support &OpcUa_*_EncodeableType reference in definition for library
 static SOPC_Event_Variable sopc_baseEventVariants[SOPC_BaseEventVariantIdx_ReservedLength] = {
+    {{true, SOPC_NodeId_Id, SOPC_VariantArrayType_SingleValue, {.NodeId = (SOPC_NodeId[]){{0}}}},
+     SOPC_NS0_NUMERIC_NODEID(OpcUaId_NodeId),
+     -1}, // "OwnNodeId": placeholder to store the actual event/node instance NodeId (see ConditionId in part 9)
     {{true, SOPC_ByteString_Id, SOPC_VariantArrayType_SingleValue, {.Bstring = {0}}},
      SOPC_NS0_NUMERIC_NODEID(OpcUaId_ByteString),
      -1}, // EventId
@@ -113,6 +119,27 @@ static SOPC_Event_Variable sopc_baseEventVariants[SOPC_BaseEventVariantIdx_Reser
 static SOPC_NodeId baseEventNodeId = SOPC_NS0_NUMERIC_NODEID(OpcUaId_BaseEventType);
 
 static uint64_t eventIdCounter = 0;
+
+static SOPC_ReturnStatus SOPC_InternalEventManagerUtil_QnPathToCString(uint16_t nbQnPath,
+                                                                       const SOPC_QualifiedName* qualifiedNamePathArray,
+                                                                       char** qnPathStr)
+{
+    if (NULL == qnPathStr || (NULL == qualifiedNamePathArray && 0 != nbQnPath))
+    {
+        return SOPC_STATUS_INVALID_PARAMETERS;
+    }
+    // Specific management of internal empty path which always represents event/node instance NodeId
+    if (0 == nbQnPath)
+    {
+        *qnPathStr = SOPC_strdup(QN_EMPTY_PATH_NODEID);
+        if (NULL != *qnPathStr)
+        {
+            return SOPC_STATUS_OUT_OF_MEMORY;
+        }
+        return SOPC_STATUS_OK;
+    }
+    return SOPC_EventManagerUtil_QnPathToCString(nbQnPath, qualifiedNamePathArray, qnPathStr);
+}
 
 static void SOPC_EventVariable_Delete(SOPC_Event_Variable** ppEventVar)
 {
@@ -513,7 +540,8 @@ const SOPC_Variant* SOPC_Event_GetVariableAndType(const SOPC_Event* pEvent,
     }
     const SOPC_Variant* result = NULL;
     char* qnPathStr = NULL;
-    SOPC_ReturnStatus status = SOPC_EventManagerUtil_QnPathToCString(nbQnPath, qualifiedNamePathArray, &qnPathStr);
+    SOPC_ReturnStatus status =
+        SOPC_InternalEventManagerUtil_QnPathToCString(nbQnPath, qualifiedNamePathArray, &qnPathStr);
     if (SOPC_STATUS_OK == status)
     {
         result = SOPC_Event_GetVariableAndTypeFromStrPath(pEvent, qnPathStr, outDataType, outValueRank);
@@ -524,7 +552,51 @@ const SOPC_Variant* SOPC_Event_GetVariableAndType(const SOPC_Event* pEvent,
 
 const SOPC_Variant* SOPC_Event_GetVariableFromStrPath(const SOPC_Event* pEvent, const char* qnPath)
 {
-    return SOPC_Event_GetVariableAndTypeFromStrPath(pEvent, qnPath, NULL, NULL);
+    if (NULL == qnPath)
+    {
+        return NULL;
+    }
+    const char* qnPathWithEmpty = NULL;
+    if ('\0' == *qnPath)
+    {
+        qnPathWithEmpty = QN_EMPTY_PATH_NODEID;
+    }
+    else
+    {
+        qnPathWithEmpty = qnPath;
+    }
+    return SOPC_Event_GetVariableAndTypeFromStrPath(pEvent, qnPathWithEmpty, NULL, NULL);
+}
+
+static SOPC_NodeId* SOPC_Event_GetNodeId(SOPC_Event* pEvent)
+{
+    SOPC_ASSERT(NULL != pEvent);
+    bool found = false;
+    SOPC_Event_Variable* nodeIdVarInfo = (SOPC_Event_Variable*) SOPC_Dict_Get(
+        pEvent->qnPathToEventVar, (uintptr_t) sopc_baseEventVariantsPaths[SOPC_BaseEventVariantIdx_NodeId], &found);
+
+    if (found && SOPC_NodeId_Id == nodeIdVarInfo->data.BuiltInTypeId &&
+        SOPC_VariantArrayType_SingleValue == nodeIdVarInfo->data.ArrayType)
+    {
+        return nodeIdVarInfo->data.Value.NodeId;
+    }
+    return NULL;
+}
+
+SOPC_ReturnStatus SOPC_Event_SetNodeId(SOPC_Event* pEvent, const SOPC_NodeId* pNodeId)
+{
+    if (NULL == pEvent || NULL == pEvent->qnPathToEventVar || NULL == pNodeId)
+    {
+        return SOPC_STATUS_INVALID_PARAMETERS;
+    }
+    SOPC_NodeId* nId = SOPC_Event_GetNodeId(pEvent);
+    SOPC_ReturnStatus status = NULL != nId ? SOPC_STATUS_OK : SOPC_STATUS_INVALID_STATE;
+    if (SOPC_STATUS_OK == status)
+    {
+        SOPC_NodeId_Clear(nId);
+        status = SOPC_NodeId_Copy(nId, pNodeId);
+    }
+    return status;
 }
 
 typedef struct SOPC_Dict_ForEachEventVar_LocalData
@@ -537,7 +609,12 @@ static void SOPC_Dict_ForEachEventVar_Fct(const uintptr_t key, uintptr_t value, 
 {
     SOPC_Dict_ForEachEventVar_LocalData* localData = (SOPC_Dict_ForEachEventVar_LocalData*) user_data;
     SOPC_Event_Variable* eventVar = (SOPC_Event_Variable*) value;
-    localData->func((const char*) key, &eventVar->data, &eventVar->dataType, eventVar->valueRank, localData->user_data);
+    // Avoid specific case of internal empty path which is the event/node instance NodeId
+    if (0 != SOPC_strcmp_ignore_case(QN_EMPTY_PATH_NODEID, (const char*) key))
+    {
+        localData->func((const char*) key, &eventVar->data, &eventVar->dataType, eventVar->valueRank,
+                        localData->user_data);
+    }
 }
 
 void SOPC_Event_ForEachVar(SOPC_Event* event, SOPC_Event_ForEachVar_Fct* func, uintptr_t user_data)
@@ -550,7 +627,7 @@ SOPC_ReturnStatus SOPC_EventManagerUtil_QnPathToCString(uint16_t nbQnPath,
                                                         const SOPC_QualifiedName* qualifiedNamePathArray,
                                                         char** qnPathStr)
 {
-    if (NULL == qnPathStr || NULL == qualifiedNamePathArray || 0 == nbQnPath)
+    if (NULL == qnPathStr || (NULL == qualifiedNamePathArray && 0 != nbQnPath))
     {
         return SOPC_STATUS_INVALID_PARAMETERS;
     }
@@ -560,6 +637,12 @@ SOPC_ReturnStatus SOPC_EventManagerUtil_QnPathToCString(uint16_t nbQnPath,
         return SOPC_STATUS_OUT_OF_MEMORY;
     }
     char* newStr = NULL;
+    // Manage empty path case
+    if (0 == nbQnPath)
+    {
+        newStr = prevStr;
+        prevStr = NULL;
+    }
     SOPC_ReturnStatus status = SOPC_STATUS_OK;
     for (uint16_t i = 0; i < nbQnPath && SOPC_STATUS_OK == status; i++)
     {
@@ -734,7 +817,8 @@ SOPC_ReturnStatus SOPC_Event_SetVariable(SOPC_Event* pEvent,
                                          const SOPC_Variant* var)
 {
     char* strQnPath = NULL;
-    SOPC_ReturnStatus status = SOPC_EventManagerUtil_QnPathToCString(nbQnPath, qualifiedNamePathArray, &strQnPath);
+    SOPC_ReturnStatus status =
+        SOPC_InternalEventManagerUtil_QnPathToCString(nbQnPath, qualifiedNamePathArray, &strQnPath);
     if (SOPC_STATUS_OK == status)
     {
         status = SOPC_Event_SetVariableFromStrPath(pEvent, strQnPath, var);
@@ -1284,9 +1368,13 @@ SOPC_ReturnStatus SOPC_EventManager_HasEventTypeAndBrowsePath(const SOPC_Server_
                                                               int32_t nbQNamePath,
                                                               const SOPC_QualifiedName* qNamePath)
 {
-    if (nbQNamePath <= 0 || nbQNamePath > UINT16_MAX)
+    if (nbQNamePath > UINT16_MAX)
     {
         return SOPC_STATUS_INVALID_PARAMETERS;
+    }
+    else if (nbQNamePath < 0)
+    {
+        nbQNamePath = 0;
     }
     SOPC_Event* eventInstDecl = getEventType(eventTypes, eventTypeId);
     if (NULL == eventInstDecl)
@@ -1294,7 +1382,8 @@ SOPC_ReturnStatus SOPC_EventManager_HasEventTypeAndBrowsePath(const SOPC_Server_
         return SOPC_STATUS_NOK;
     }
     char* qnPathStr = NULL;
-    SOPC_ReturnStatus status = SOPC_EventManagerUtil_QnPathToCString((uint16_t) nbQNamePath, qNamePath, &qnPathStr);
+    SOPC_ReturnStatus status =
+        SOPC_InternalEventManagerUtil_QnPathToCString((uint16_t) nbQNamePath, qNamePath, &qnPathStr);
     if (SOPC_STATUS_OK == status)
     {
         bool found = false;

samples/ClientServer/demo_server/toolkit_demo_server_methods.c
@@ -37,6 +37,11 @@ static const SOPC_NodeId TestObject = {.IdentifierType = SOPC_IdentifierType_Str
                                        .Namespace = 1,
                                        .Data.String = {sizeof("TestObject") - 1, 1, (SOPC_Byte*) "TestObject"}};
 
+static const SOPC_NodeId EventInstNodeId = {
+    .IdentifierType = SOPC_IdentifierType_String,
+    .Namespace = 1,
+    .Data.String = {sizeof("EventInstance_NodeId_Example") - 1, 1, (SOPC_Byte*) "EventInstance_NodeId_Example"}};
+
 static const SOPC_NodeId TestObject_HelloNextArg = {
     .IdentifierType = SOPC_IdentifierType_String,
     .Namespace = 1,
@@ -605,7 +610,12 @@ SOPC_StatusCode SOPC_Method_Func_GenEvent(const SOPC_CallContext* callContextPtr
         return OpcUa_BadInvalidArgument;
     }
 
-    SOPC_Event_ForEachVar(eventInst, forEachEventVar, 0);
+    status = SOPC_Event_SetNodeId(eventInst, &EventInstNodeId);
+
+    if (SOPC_STATUS_OK == status)
+    {
+        SOPC_Event_ForEachVar(eventInst, forEachEventVar, 0);
+    }
 
     SOPC_LocalizedText lt;
     SOPC_LocalizedText_Initialize(&lt);

src/ClientServer/address_space/sopc_event.h
@@ -268,6 +268,22 @@ const SOPC_Variant* SOPC_Event_GetVariableAndTypeFromStrPath(const SOPC_Event* p
                                                              int32_t* outValueRank);
 
 /**
+ * \brief Sets the event/node instance NodeId.
+ *
+ *        This function associates a NodeId to the event instance
+ *        that shall be the NodeId of the corresponding node if it exists in address space.
+ *
+ *        It is required to define it for ConditionType event instances to could be returned as "ConditionId"
+ *        using subscription EventFilter with specific parameters as defined in part 9.
+ *
+ * \param pEvent  pointer to the event for which variable value shall be set
+ * \param nodeId  the NodeId to associated to the given event instance
+ *
+ * \return SOPC_STATUS_OK in case of success, SOPC_STATUS_INVALID parameter or SOPC_STATUS_OUT_OF_MEMORY otherwise.
+ */
+SOPC_ReturnStatus SOPC_Event_SetNodeId(SOPC_Event* pEvent, const SOPC_NodeId* nodeId);
+
+/**
  * \brief Type of callback functions for \ref SOPC_Event.
  * The value of \p user_data is set when calling \ref SOPC_Event_ForEachVar.
  */

src/ClientServer/address_space/sopc_event_manager.h
@@ -29,6 +29,19 @@
 #include "sopc_address_space.h"
 #include "sopc_event.h"
 
+#ifndef QN_PATH_SEPARATOR_CHAR
+/**
+ * \brief The default QualifiedName path separator used for events fields identification
+ */
+#define QN_PATH_SEPARATOR_CHAR '~'
+#define QN_PATH_SEPARATOR_STR "~"
+#else
+#ifndef QN_PATH_SEPARATOR_STR
+#error \
+    "Both QN_PATH_SEPARATOR_CHAR and QN_PATH_SEPARATOR_STR shall be defined with same chracter if default value not used"
+#endif
+#endif
+
 /**
  * \brief OPC UA server events types configuration
  */

src/ClientServer/services/b2c/monitored_item_event_filter_treatment_bs.c
@@ -219,12 +219,18 @@ void monitored_item_event_filter_treatment_bs__check_select_clause_and_fill_ctx(
     SOPC_ASSERT(clauseArrayIdx < eventFilter->NoOfSelectClauses);
 
     OpcUa_SimpleAttributeOperand* selectClause = &eventFilter->SelectClauses[clauseArrayIdx];
+    // Accepts Value attribute
     if (selectClause->AttributeId != SOPC_AttributeId_Value)
     {
-        *monitored_item_event_filter_treatment_bs__clauseRawSc = OpcUa_BadAttributeIdInvalid;
-        *monitored_item_event_filter_treatment_bs__selectStatusCode =
-            constants_statuscodes_bs__e_sc_bad_monitored_item_filter_unsupported;
-        return;
+        // Note: make an exception to manage the mechanism to obtain the event (node) NodeId (see ConditionId in part9).
+        // When the attributeId is NodeId with an empty path, we will return the NodeId defined in Event if defined.
+        if (selectClause->AttributeId != SOPC_AttributeId_NodeId || selectClause->NoOfBrowsePath > 0)
+        {
+            *monitored_item_event_filter_treatment_bs__clauseRawSc = OpcUa_BadAttributeIdInvalid;
+            *monitored_item_event_filter_treatment_bs__selectStatusCode =
+                constants_statuscodes_bs__e_sc_bad_monitored_item_filter_unsupported;
+            return;
+        }
     }
 
     SOPC_ReturnStatus status = SOPC_STATUS_NOK;

src/ClientServer/services/b2c/util_event.c
@@ -123,6 +123,13 @@ void util_event__set_event_field_list_elt(char** preferredLocalesIds,
     char* qnPath = pFilterCtx->Filter.Event.qnPathStrSelectClauses[selectClauseIdx];
     SOPC_NumericRange* indexRange = pFilterCtx->Filter.Event.indexRangeSelectClauses[selectClauseIdx];
     const SOPC_Variant* eventVar = SOPC_Event_GetVariableFromStrPath(pEvent, qnPath);
+    // Restricts empty browse path for NodeId attribute
+    // (other browse paths are for Value attribute only which is already checked on creation)
+    if (NULL != eventVar && '\0' == *qnPath &&
+        SOPC_AttributeId_NodeId != pFilterCtx->Filter.Event.eventFilter->SelectClauses[selectClauseIdx].AttributeId)
+    {
+        eventVar = NULL;
+    }
 
     if (NULL != eventVar)
     {

tests/ClientServer/unit_tests/helpers/check_tools.c
@@ -4944,15 +4944,15 @@ START_TEST(test_parse_event_qn_path)
         const char sep;
         const char* input;
         const int32_t nbElts;
-    } test_data[] = {{'~', "0:EnabledState~0:Id", 2},
-                     {'~', "0:EnabledState~Id", 2},
-                     {'~', "0:EnabledState\\~~0:Id", 2},
+    } test_data[] = {{QN_PATH_SEPARATOR_CHAR, "0:EnabledState" QN_PATH_SEPARATOR_STR "0:Id", 2},
+                     {QN_PATH_SEPARATOR_CHAR, "0:EnabledState" QN_PATH_SEPARATOR_STR "Id", 2},
+                     {QN_PATH_SEPARATOR_CHAR, "0:EnabledState\\~~0:Id", 2},
                      {'?', "0:EnabledState?0:Id", 2},
                      {'\0', NULL, 0}};
 
     static const char* results[4][2] = {{"0:EnabledState", "0:Id"},
                                         {"0:EnabledState", "0:Id"},
-                                        {"0:EnabledState~", "0:Id"},
+                                        {"0:EnabledState" QN_PATH_SEPARATOR_STR, "0:Id"},
                                         {"0:EnabledState", "0:Id"}};
 
     int32_t nbElts = 0;
@@ -4984,7 +4984,9 @@ START_TEST(test_parse_event_qn_path)
     SOPC_ReturnStatus status = SOPC_EventManagerUtil_cStringPathToQnPath('\0', test_data[0].input, &nbElts, &qnPaths);
     ck_assert_uint_eq(SOPC_STATUS_INVALID_PARAMETERS, status);
     ck_assert_ptr_null(qnPaths);
-    status = SOPC_EventManagerUtil_cStringPathToQnPath('\0', "~0:EnabledState~0:Id~", &nbElts, &qnPaths);
+    status = SOPC_EventManagerUtil_cStringPathToQnPath(
+        '\0', QN_PATH_SEPARATOR_STR "0:EnabledState" QN_PATH_SEPARATOR_STR "0:Id" QN_PATH_SEPARATOR_STR, &nbElts,
+        &qnPaths);
     ck_assert_uint_eq(SOPC_STATUS_INVALID_PARAMETERS, status);
     ck_assert_ptr_null(qnPaths);
     status = SOPC_EventManagerUtil_cStringPathToQnPath('\0', test_data[0].input, &nbElts, &qnPaths);

tests/ClientServer/validation_tests/client/toolkit_test_client_events.c
@@ -33,11 +33,11 @@
 
 #include "sopc_assert.h"
 #include "sopc_encodeabletype.h"
+#include "sopc_helper_askpass.h"
+#include "sopc_helper_string.h"
 #include "sopc_macros.h"
 #include "sopc_mem_alloc.h"
 #include "sopc_threads.h"
-
-#include "sopc_helper_askpass.h"
 
 // Sleep timeout in milliseconds
 static const uint32_t sleepTimeout = 200;
@@ -58,30 +58,28 @@ static const SOPC_NodeId baseModelChangeEventTypeId = SOPC_NS0_NUMERIC_NODEID(Op
 static const char* monitoredItemNames[NB_MONITORED_ITEMS] = {"TestObject", "Server"};
 static uint32_t nbMIs = 0;
 
-#define NB_SELECT_CLAUSES_IN_ARRAY 6
+#define NB_SELECT_CLAUSES_IN_ARRAY 7
 #define NB_SELECT_CLAUSES 4 + NB_SELECT_CLAUSES_IN_ARRAY
 // selectClause[0]
 static const SOPC_QualifiedName qnEventId = {0, {sizeof("EventId") - 1, 1, (SOPC_Byte*) "EventId"}};
 // selectClause[1]
 #define CONDITION_TYPE_FIELD_IDX 1
 static const SOPC_QualifiedName qnConditionName = {0, {sizeof("ConditionName") - 1, 1, (SOPC_Byte*) "ConditionName"}};
-// selectClause[2] is invalid path for EventType and selectClause[3] is invalid attribute NodeId
+// selectClause[2] is invalid path for EventType and selectClause[3] is invalid attribute BrowseName
 // selectClause[4..9] are valid paths (unchecked due to base event type selected)
 #define EVENT_TYPE_FIELD_IDX 4
 #define ALARM_CONDITION_TYPE_FIELD_IDX 9
 static const char* selectClauses_4[NB_SELECT_CLAUSES - NB_SELECT_CLAUSES_IN_ARRAY] = {
     "0:EventId", "0:ConditionName", "invalid 0:ConditionName", "invalid NodeId attribute"}; // For traces only
-static const char* selectClauses_6[NB_SELECT_CLAUSES_IN_ARRAY] = {
-    "0:EventType", "0:Message", "0:Severity", "0:SourceName", "0:Time", "0:ShelvingState~0:CurrentState"};
+static const char* selectClauses_7[NB_SELECT_CLAUSES_IN_ARRAY] = {
+    "0:EventType", "0:Message", "0:Severity", "0:SourceName", "0:Time", "0:ShelvingState~0:CurrentState", ""};
 static const SOPC_StatusCode selectClausesResult[NB_SELECT_CLAUSES] = {
     SOPC_GoodGenericStatus, SOPC_GoodGenericStatus, OpcUa_BadNodeIdUnknown, OpcUa_BadAttributeIdInvalid,
     SOPC_GoodGenericStatus, SOPC_GoodGenericStatus, SOPC_GoodGenericStatus, SOPC_GoodGenericStatus,
-    SOPC_GoodGenericStatus, SOPC_GoodGenericStatus,
-};
+    SOPC_GoodGenericStatus, SOPC_GoodGenericStatus, SOPC_GoodGenericStatus};
 
-static bool notifEventFieldSetResult[NB_SELECT_CLAUSES] = {
-    true, false, false, false, true, true, true, true, true, false,
-};
+static bool notifEventFieldSetResult[NB_SELECT_CLAUSES] = {true, false, false, false, true, true,
+                                                           true, true,  true,  false, true};
 
 #define NB_SELECT_CLAUSES_INDEX_RANGE 3
 /*
@@ -146,7 +144,7 @@ static void SOPC_Client_SubscriptionNotification_Cb(const SOPC_ClientHelper_Subs
                     {
                         currentSelectClause =
                             (j < 4 ? selectClauses_4[j]
-                                   : selectClauses_6[j - (NB_SELECT_CLAUSES - NB_SELECT_CLAUSES_IN_ARRAY)]);
+                                   : selectClauses_7[j - (NB_SELECT_CLAUSES - NB_SELECT_CLAUSES_IN_ARRAY)]);
                         SOPC_CONSOLE_PRINTF("\nEventField[%" PRIi32 "]('%s'):\n", j, currentSelectClause);
                         SOPC_Variant_Print(&event->EventFields[j]);
                         // Check if event field is NULL only when expected not to be set
@@ -178,7 +176,7 @@ static void SOPC_Client_SubscriptionNotification_Cb(const SOPC_ClientHelper_Subs
                             SOPC_CONSOLE_PRINTF(
                                 "ERROR: unexpected event field [idx=%" PRIi32 "] state (set/unset): %s instead of %s\n",
                                 j, (event->EventFields[j].BuiltInTypeId != SOPC_Null_Id ? "SET" : "UNSET"),
-                                (notifEventFieldSetResult[j] ? "SET" : "UNSET"));
+                                (notifEventFieldSetIndexRangeResult[j] ? "SET" : "UNSET"));
                         }
                     }
                 }
@@ -374,19 +372,29 @@ static OpcUa_CreateMonitoredItemsResponse* create_monitored_item_event(SOPC_Clie
                 status = SOPC_EventFilter_SetSelectClause(eventFilter, 2, &baseModelChangeEventTypeId, 1,
                                                           &qnConditionName, SOPC_AttributeId_Value, NULL);
             }
-            /* select clause 3: node id attribute */
+            /* select clause 3: browse name attribute */
             if (SOPC_STATUS_OK == status)
             {
                 status = SOPC_EventFilter_SetSelectClauseFromStringPath(eventFilter, 3, NULL, '~', "",
-                                                                        SOPC_AttributeId_NodeId, NULL);
+                                                                        SOPC_AttributeId_BrowseName, NULL);
             }
         }
         /* 3-N select clauses: browses path as a string + NULL type id (base event type) */
         for (size_t i = initSelectArray; !invalidSelectClausesOnly && SOPC_STATUS_OK == status && i < nbSelectClauses;
              i++)
         {
-            status = SOPC_EventFilter_SetSelectClauseFromStringPath(
-                eventFilter, i, NULL, '~', selectClauses_6[i - initSelectArray], SOPC_AttributeId_Value, NULL);
+            if (0 != strlen(selectClauses_7[i - initSelectArray]))
+            {
+                status = SOPC_EventFilter_SetSelectClauseFromStringPath(
+                    eventFilter, i, NULL, '~', selectClauses_7[i - initSelectArray], SOPC_AttributeId_Value, NULL);
+            }
+            else
+            {
+                // Specific test of the self-instance NodeId attribute case (necessary for "ConditionId" in part 9)
+                // It should return NodeId of the instance, matching the representing Node in address space if it exists
+                status = SOPC_EventFilter_SetSelectClauseFromStringPath(
+                    eventFilter, i, NULL, '~', selectClauses_7[i - initSelectArray], SOPC_AttributeId_NodeId, NULL);
+            }
         }
 
         if (invalidSelectClausesOnly)

commit d5f1f3e3f7d6989f7eb0c7486228b70dec21d00c

Ticket #1561: EventFilter: bugfix for empty browsePath / nodeId attribute with type definition
Bugfix for bug introduced in commit 4afed9eb69303e55e74cead5fbf0a0e9a5329cb4.
It only occurs when an EventFilter is defined with an empty browsePath for nodeId attribute with type definition different from BaseEventType.
Consequence is a memory leak and failure on create monitored item

src/ClientServer/address_space/sopc_event_manager.c
@@ -132,7 +132,7 @@ static SOPC_ReturnStatus SOPC_InternalEventManagerUtil_QnPathToCString(uint16_t 
     if (0 == nbQnPath)
     {
         *qnPathStr = SOPC_strdup(QN_EMPTY_PATH_NODEID);
-        if (NULL != *qnPathStr)
+        if (NULL == *qnPathStr)
         {
             return SOPC_STATUS_OUT_OF_MEMORY;
         }