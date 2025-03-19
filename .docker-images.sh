2200f22e9adb69534b4f3cb7f560321601e81e10
monfort <vincent.monfort@systerel.fr>
2025-03-19 18:12:08 +0100

    Ticket #1548: update build docker to 1.48: expat 2.7.0

--- .docker-images.sh
+++ .docker-images.sh
@@ -20,7 +20,7 @@
 # Define the docker images used in S2OPC
 
 # Public images (registry.gitlab.com/systerel/s2opc)
-BUILD_DIGEST=registry.gitlab.com/systerel/s2opc/build@sha256:80ccf80c716d33371b3f6bb7c4027346f74a07d8259ff56090beb787ace28e80 # build:1.47
+BUILD_DIGEST=registry.gitlab.com/systerel/s2opc/build@sha256:3f8d8402c026c7b84cb059239450ac76653426d72490623f9b1a102a10a9d7b6 # build:1.48
 MINGW_DIGEST=registry.gitlab.com/systerel/s2opc/mingw_build@sha256:d158e52c51d2f97d4bd13bcc93ebbe5793025c0032dd7e6116bdf9293fc790bf # mingw_build:1.15
 RPI_DIGEST=registry.gitlab.com/systerel/s2opc/rpi-build@sha256:0c5e40993e575bb829cee97e17a4965258fdccd4eef6ee81fca1fc732f417a63 # rpi_build:1.8
 CHECK_DIGEST=registry.gitlab.com/systerel/s2opc/check@sha256:8be8c50cd484989c987b6c28ded2e03f30e81ec27bb53e41cecde8cdc3bb86c8 # check 1.22
