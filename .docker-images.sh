6c16aefb5f2e3a08311c9d5e516c5b35954956f1
monfort <vincent.monfort@systerel.fr>
2025-05-21 18:50:03 +0200

    Ticket #1548: build image: update to build 1.50 and rpi-build 1.9
    Add CMake export installed for mbedtls

--- .docker-images.sh
+++ .docker-images.sh
@@ -20,9 +20,9 @@
 # Define the docker images used in S2OPC
 
 # Public images (registry.gitlab.com/systerel/s2opc)
-BUILD_DIGEST=registry.gitlab.com/systerel/s2opc/build@sha256:5eba7fd81275c935eebcf15fa069db9750eec1b10c3c99e329e72d90ac01f530 # build:1.49
+BUILD_DIGEST=registry.gitlab.com/systerel/s2opc/build@sha256:63dd79ef954bc791fcc27c3177fbc43c2e9589f550df4a026f577abb167a1b63 # build:1.50
 MINGW_DIGEST=registry.gitlab.com/systerel/s2opc/mingw_build@sha256:d158e52c51d2f97d4bd13bcc93ebbe5793025c0032dd7e6116bdf9293fc790bf # mingw_build:1.15
-RPI_DIGEST=registry.gitlab.com/systerel/s2opc/rpi-build@sha256:0c5e40993e575bb829cee97e17a4965258fdccd4eef6ee81fca1fc732f417a63 # rpi_build:1.8
+RPI_DIGEST=registry.gitlab.com/systerel/s2opc/rpi-build@sha256:070b1ca58667ee5908902ba3eb03e5fa05993d3a86d2c8ddeb6f7f41ed62609c # rpi_build:1.9
 CHECK_DIGEST=registry.gitlab.com/systerel/s2opc/check@sha256:8be8c50cd484989c987b6c28ded2e03f30e81ec27bb53e41cecde8cdc3bb86c8 # check 1.22
 TEST_DIGEST=registry.gitlab.com/systerel/s2opc/test@sha256:8fbec254f6b8c8467b780d407c6f3c2e87d87899e141b8b2c9013ba30b4f0c8d # test:2.16
 ZEPHYR_DIGEST=registry.gitlab.com/systerel/s2opc/zephyr_build:v3.6.0-b@sha256:13069418dd7b0317f93f02986b01f285f0f66956670f2fecded9850ace85d734 # zephyr_build:v3.6.0-b
