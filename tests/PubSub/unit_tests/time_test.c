6f375d8117a0237c4700d6976b004cee27804cbd
Theo Pirouelle <theo.pirouelle@systerel.fr>
2025-11-12 16:45:13 +0100

    Ticket #1548: Taking comments into account

--- tests/PubSub/unit_tests/time_test.c
+++ tests/PubSub/unit_tests/time_test.c
@@ -17,25 +17,16 @@
  * under the License.
  */
 
-// ============================================================================
-// INCLUDE
-// ============================================================================
 #include <check.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-// The macro used in this Unit Test: triggers an error if requested.
-static bool g_fail_clock_gettime = false;
-#define clock_gettime(clk_id, tp) (g_fail_clock_gettime ? (errno = EFAULT, -1) : clock_gettime(clk_id, tp))
-
 #include "sopc_date_time.h"
 #include "sopc_enums.h"
 #include "sopc_threads.h"
 #include "sopc_time_reference.h"
-
-#undef clock_gettime
 
 // ============================================================================
 // TESTS SOPC_TimeReference_*
@@ -49,19 +40,19 @@ START_TEST(test_timeReference_getCurrent)
 {
     // Non-zero test value
     SOPC_TimeReference t1 = SOPC_TimeReference_GetCurrent();
-    ck_assert_msg(t1 > 0, "TimeReference should be > 0");
+    ck_assert_uint_gt(t1, 0);
 
     // Increasing test value
     SOPC_Sleep(1); // 1 ms
     SOPC_TimeReference t2 = SOPC_TimeReference_GetCurrent();
-    ck_assert_msg(t2 > t1, "TimeReference should increase over time");
+    ck_assert_uint_gt(t2, t1);
 
     // Monotonicity test on multiple calls
     SOPC_TimeReference prev = t2;
     for (int i = 0; i < 100; i++)
     {
         SOPC_TimeReference now = SOPC_TimeReference_GetCurrent();
-        ck_assert_msg(now >= prev, "Monotonic clock violated");
+        ck_assert_uint_ge(now, prev);
         prev = now;
     }
 }
@@ -79,22 +70,21 @@ START_TEST(test_timeReference_addMilliseconds)
     ck_assert_uint_eq(SOPC_TimeReference_AddMilliseconds(t, 500), 1500);
 
     // Overflow test
-    SOPC_TimeReference t_max = UINT64_MAX - 10;
-    SOPC_TimeReference result = SOPC_TimeReference_AddMilliseconds(t_max, 100);
-    ck_assert_msg(result == UINT64_MAX, "Expected saturation on overflow");
+    SOPC_TimeReference tMax = UINT64_MAX - 10;
+    SOPC_TimeReference result = SOPC_TimeReference_AddMilliseconds(tMax, 100);
+    ck_assert_uint_eq(result, UINT64_MAX);
 
     // Chain of additions test
     SOPC_TimeReference current = 0;
-    uint64_t total_ms = 0;
+    uint64_t totalMs = 0;
 
-    for (int i = 0; i < 100; i++)
+    for (uint64_t i = 0; i < 100; i++)
     {
-        uint64_t inc = (uint64_t)(i + 1) * 10u;
-        total_ms += inc;
+        uint64_t inc = (i + 1) * 10u;
+        totalMs += inc;
         current = SOPC_TimeReference_AddMilliseconds(current, inc);
 
-        ck_assert_msg(current == total_ms, "After cumulative additions, expected %llu but got %llu (iter %d)",
-                      (unsigned long long) total_ms, (unsigned long long) current, i);
+        ck_assert_uint_eq(current, totalMs);
     }
 
     current = UINT64_MAX - 100;
@@ -104,7 +94,7 @@ START_TEST(test_timeReference_addMilliseconds)
         SOPC_TimeReference previous = current;
         current = SOPC_TimeReference_AddMilliseconds(current, 50);
 
-        ck_assert_msg(current >= previous, "Expected monotonic increase or saturation (iter %d)", i);
+        ck_assert_uint_ge(current, previous);
 
         if (current == UINT64_MAX)
         {
@@ -113,8 +103,8 @@ START_TEST(test_timeReference_addMilliseconds)
     }
 
     // At the end of the chain, we must be saturated
-    ck_assert_msg(saturated && current == UINT64_MAX, "Expected saturation after overflow chain (final value %llu)",
-                  (unsigned long long) current);
+    ck_assert(saturated);
+    ck_assert_uint_eq(current, UINT64_MAX);
 }
 END_TEST
 
@@ -159,10 +149,13 @@ START_TEST(test_highRes_timeReference_create_getTime_delete)
     SOPC_HighRes_TimeReference* t = SOPC_HighRes_TimeReference_Create();
     ck_assert_ptr_nonnull(t);
 
-    // GetTime fills in a valid structure
     SOPC_HighRes_TimeReference_GetTime(t);
     ck_assert_ptr_nonnull(t);
-    SOPC_HighRes_TimeReference_GetTime(NULL);
+
+    // GetTime test with NULL parameter
+    SOPC_HighRes_TimeReference* tNull = NULL;
+    SOPC_HighRes_TimeReference_GetTime(tNull);
+    ck_assert_ptr_null(tNull);
 
     // Valid deletion
     SOPC_HighRes_TimeReference_Delete(&t);
@@ -196,10 +189,16 @@ START_TEST(test_highRes_timeReference_copy)
 
     // Copy ignored if pointers are NULL
     SOPC_HighRes_TimeReference_Copy(NULL, t1);
+
+    SOPC_HighRes_TimeReference* t2Before = SOPC_HighRes_TimeReference_Create();
+    SOPC_HighRes_TimeReference_Copy(t2Before, t2);
     SOPC_HighRes_TimeReference_Copy(t2, NULL);
+    int64_t deltaAfter = SOPC_HighRes_TimeReference_DeltaUs(t2Before, t2);
+    ck_assert_int_eq(deltaAfter, 0);
 
     SOPC_HighRes_TimeReference_Delete(&t1);
     SOPC_HighRes_TimeReference_Delete(&t2);
+    SOPC_HighRes_TimeReference_Delete(&t2Before);
 }
 END_TEST
 
@@ -219,19 +218,19 @@ START_TEST(test_highRes_timeReference_deltaUs)
 
     // Positive delta test
     int64_t delta = SOPC_HighRes_TimeReference_DeltaUs(t1, t2);
-    ck_assert_msg(delta > 0, "Delta should be positive");
+    ck_assert_int_gt(delta, 0);
 
     // Negative delta test
-    int64_t delta_neg = SOPC_HighRes_TimeReference_DeltaUs(t2, t1);
-    ck_assert_msg(delta_neg < 0, "Delta should be negative");
+    int64_t deltaNeg = SOPC_HighRes_TimeReference_DeltaUs(t2, t1);
+    ck_assert_int_lt(deltaNeg, 0);
 
     // Zero delta test
-    int64_t delta_zero = SOPC_HighRes_TimeReference_DeltaUs(t1, t1);
-    ck_assert_int_eq(delta_zero, 0);
+    int64_t deltaZero = SOPC_HighRes_TimeReference_DeltaUs(t1, t1);
+    ck_assert_int_eq(deltaZero, 0);
 
     // NULL delta test
-    int64_t delta_auto = SOPC_HighRes_TimeReference_DeltaUs(t1, NULL);
-    ck_assert_msg(delta_auto > 0, "Delta should be positive when comparing to current time");
+    int64_t deltaAuto = SOPC_HighRes_TimeReference_DeltaUs(t1, NULL);
+    ck_assert_int_gt(deltaAuto, 0);
 
     SOPC_HighRes_TimeReference_Delete(&t1);
     SOPC_HighRes_TimeReference_Delete(&t2);
@@ -244,52 +243,52 @@ END_TEST
  */
 START_TEST(test_highRes_timeReference_addSynchedDuration)
 {
-    SOPC_HighRes_TimeReference* t_init = SOPC_HighRes_TimeReference_Create();
-    SOPC_HighRes_TimeReference_GetTime(t_init);
+    SOPC_HighRes_TimeReference* tInit = SOPC_HighRes_TimeReference_Create();
+    SOPC_HighRes_TimeReference_GetTime(tInit);
 
-    SOPC_HighRes_TimeReference* t_test = SOPC_HighRes_TimeReference_Create();
-    SOPC_HighRes_TimeReference_Copy(t_test, t_init);
+    SOPC_HighRes_TimeReference* tTest = SOPC_HighRes_TimeReference_Create();
+    SOPC_HighRes_TimeReference_Copy(tTest, tInit);
 
-    int64_t delta_before = SOPC_HighRes_TimeReference_DeltaUs(t_init, t_test);
-    ck_assert_int_eq(delta_before, 0);
+    int64_t deltaBefore = SOPC_HighRes_TimeReference_DeltaUs(tInit, tTest);
+    ck_assert_int_eq(deltaBefore, 0);
 
     // Simple addition without offset
-    SOPC_HighRes_TimeReference_AddSynchedDuration(t_test, 100000, -1); // +100 ms
-    int64_t delta_us = SOPC_HighRes_TimeReference_DeltaUs(t_init, t_test);
-    ck_assert_msg(delta_us > 0, "Duration should increase time reference");
+    SOPC_HighRes_TimeReference_AddSynchedDuration(tTest, 100000, -1); // +100 ms
+    int64_t deltaUs = SOPC_HighRes_TimeReference_DeltaUs(tInit, tTest);
+    ck_assert_int_gt(deltaUs, 0);
 
     // Add with offset in the window (offset < duration)
-    SOPC_HighRes_TimeReference_Copy(t_test, t_init);
-    SOPC_HighRes_TimeReference_AddSynchedDuration(t_test, 100000, 20000); // période 100 ms, offset 20 ms
-    delta_us = SOPC_HighRes_TimeReference_DeltaUs(t_init, t_test);
-    ck_assert_msg(delta_us >= 0, "Offset +20ms must still move forward");
+    SOPC_HighRes_TimeReference_Copy(tTest, tInit);
+    SOPC_HighRes_TimeReference_AddSynchedDuration(tTest, 100000, 20000); // period 100 ms, offset 20 ms
+    deltaUs = SOPC_HighRes_TimeReference_DeltaUs(tInit, tTest);
+    ck_assert_int_ge(deltaUs, 0);
 
     // Borderline case: offset = 0
-    SOPC_HighRes_TimeReference_Copy(t_test, t_init);
-    SOPC_HighRes_TimeReference_AddSynchedDuration(t_test, 100000, 0);
-    delta_us = SOPC_HighRes_TimeReference_DeltaUs(t_init, t_test);
-    ck_assert_msg(delta_us >= 0, "Offset 0 should be valid and positive");
+    SOPC_HighRes_TimeReference_Copy(tTest, tInit);
+    SOPC_HighRes_TimeReference_AddSynchedDuration(tTest, 100000, 0);
+    deltaUs = SOPC_HighRes_TimeReference_DeltaUs(tInit, tTest);
+    ck_assert_int_ge(deltaUs, 0);
 
     // Borderline case: offset = duration (should return to the beginning of the next period)
-    SOPC_HighRes_TimeReference_Copy(t_test, t_init);
-    SOPC_HighRes_TimeReference_AddSynchedDuration(t_test, 100000, 100000);
-    delta_us = SOPC_HighRes_TimeReference_DeltaUs(t_init, t_test);
-    ck_assert_msg(delta_us >= 0, "Offset equal to duration should still move forward");
+    SOPC_HighRes_TimeReference_Copy(tTest, tInit);
+    SOPC_HighRes_TimeReference_AddSynchedDuration(tTest, 100000, 100000);
+    deltaUs = SOPC_HighRes_TimeReference_DeltaUs(tInit, tTest);
+    ck_assert_int_ge(deltaUs, 0);
 
     // Borderline case: negative offset (equivalent to “no window,” simple addition)
-    SOPC_HighRes_TimeReference_Copy(t_test, t_init);
-    SOPC_HighRes_TimeReference_AddSynchedDuration(t_test, 100000, -100);
-    delta_us = SOPC_HighRes_TimeReference_DeltaUs(t_init, t_test);
-    ck_assert_msg(delta_us > 0, "Negative offset should be treated as simple duration addition");
+    SOPC_HighRes_TimeReference_Copy(tTest, tInit);
+    SOPC_HighRes_TimeReference_AddSynchedDuration(tTest, 100000, -100);
+    deltaUs = SOPC_HighRes_TimeReference_DeltaUs(tInit, tTest);
+    ck_assert_int_gt(deltaUs, 0);
 
     // Duration not a multiple of 1 second (undefined behavior, check stability)
-    SOPC_HighRes_TimeReference_Copy(t_test, t_init);
-    SOPC_HighRes_TimeReference_AddSynchedDuration(t_test, 123456, -1);
-    delta_us = SOPC_HighRes_TimeReference_DeltaUs(t_init, t_test);
-    ck_assert_msg(delta_us > 0, "Non-multiple of 1s should not crash and still advance");
+    SOPC_HighRes_TimeReference_Copy(tTest, tInit);
+    SOPC_HighRes_TimeReference_AddSynchedDuration(tTest, 123456, -1);
+    deltaUs = SOPC_HighRes_TimeReference_DeltaUs(tInit, tTest);
+    ck_assert_int_gt(deltaUs, 0);
 
-    SOPC_HighRes_TimeReference_Delete(&t_init);
-    SOPC_HighRes_TimeReference_Delete(&t_test);
+    SOPC_HighRes_TimeReference_Delete(&tInit);
+    SOPC_HighRes_TimeReference_Delete(&tTest);
 }
 END_TEST
 
@@ -305,12 +304,12 @@ START_TEST(test_highRes_timeReference_isExpired)
     // Test date passed
     SOPC_HighRes_TimeReference_GetTime(t);
     SOPC_Sleep(2);
-    ck_assert_msg(SOPC_HighRes_TimeReference_IsExpired(t, NULL));
+    ck_assert(SOPC_HighRes_TimeReference_IsExpired(t, NULL));
 
     // Test future date
     SOPC_HighRes_TimeReference_GetTime(t);
     SOPC_HighRes_TimeReference_AddSynchedDuration(t, 200000, -1);
-    ck_assert_msg(!SOPC_HighRes_TimeReference_IsExpired(t, NULL));
+    ck_assert(!SOPC_HighRes_TimeReference_IsExpired(t, NULL));
 
     SOPC_Sleep(250);
     ck_assert(SOPC_HighRes_TimeReference_IsExpired(t, NULL));
@@ -320,12 +319,12 @@ START_TEST(test_highRes_timeReference_isExpired)
     SOPC_HighRes_TimeReference_GetTime(now);
 
     SOPC_HighRes_TimeReference_AddSynchedDuration(t, 100000, -1);
-    ck_assert_msg(!SOPC_HighRes_TimeReference_IsExpired(t, now));
+    ck_assert(!SOPC_HighRes_TimeReference_IsExpired(t, now));
 
     SOPC_HighRes_TimeReference_GetTime(t);
     SOPC_Sleep(3);
     SOPC_HighRes_TimeReference_GetTime(now);
-    ck_assert_msg(SOPC_HighRes_TimeReference_IsExpired(t, now));
+    ck_assert(SOPC_HighRes_TimeReference_IsExpired(t, now));
 
     SOPC_HighRes_TimeReference_Delete(&t);
     SOPC_HighRes_TimeReference_Delete(&now);
@@ -339,62 +338,29 @@ END_TEST
 START_TEST(test_highRes_timeReference_sleepUntil)
 {
     // Test future date
-    SOPC_HighRes_TimeReference* t_future = SOPC_HighRes_TimeReference_Create();
-    SOPC_HighRes_TimeReference_GetTime(t_future);
-    SOPC_HighRes_TimeReference_AddSynchedDuration(t_future, 200000, -1);
+    SOPC_HighRes_TimeReference* tFuture = SOPC_HighRes_TimeReference_Create();
+    SOPC_HighRes_TimeReference_GetTime(tFuture);
+    SOPC_HighRes_TimeReference_AddSynchedDuration(tFuture, 200000, -1);
 
-    SOPC_TimeReference start = SOPC_TimeReference_GetCurrent();
-    SOPC_HighRes_TimeReference_SleepUntil(t_future);
-    SOPC_TimeReference end = SOPC_TimeReference_GetCurrent();
+    SOPC_HighRes_TimeReference_SleepUntil(tFuture);
 
-    ck_assert_msg((end - start) >= 150);
+    // After SleepUntil, the current time must be >= tFuture
+    ck_assert(SOPC_HighRes_TimeReference_IsExpired(tFuture, NULL));
 
-    SOPC_HighRes_TimeReference_Delete(&t_future);
+    SOPC_HighRes_TimeReference_Delete(&tFuture);
 
     // Test date passed
-    SOPC_HighRes_TimeReference* t_past = SOPC_HighRes_TimeReference_Create();
-    SOPC_HighRes_TimeReference_GetTime(t_past);
+    SOPC_HighRes_TimeReference* tPast = SOPC_HighRes_TimeReference_Create();
+    SOPC_HighRes_TimeReference_GetTime(tPast);
     SOPC_Sleep(5);
 
-    start = SOPC_TimeReference_GetCurrent();
-    SOPC_HighRes_TimeReference_SleepUntil(t_past);
-    end = SOPC_TimeReference_GetCurrent();
+    SOPC_TimeReference start = SOPC_TimeReference_GetCurrent();
+    SOPC_HighRes_TimeReference_SleepUntil(tPast);
+    SOPC_TimeReference end = SOPC_TimeReference_GetCurrent();
 
-    ck_assert_msg((end - start) < 20);
+    ck_assert_uint_lt((end - start), 100);
 
-    SOPC_HighRes_TimeReference_Delete(&t_past);
-}
-END_TEST
-
-// ============================================================================
-// TESTS SOPC_ASSERT
-// ============================================================================
-
-/*
- * Testing the SOPC_HighRes_TimeReference_GetTime() function.
- * Verify that the function triggers a SOPC_ASSERT if clock_gettime fails.
- */
-START_TEST(test_highRes_timeReference_getTime_sopcAssert)
-{
-    g_fail_clock_gettime = true; // Force clock_gettime() to fail
-    SOPC_HighRes_TimeReference* t = SOPC_HighRes_TimeReference_Create();
-
-    // Must raise an assertion (SIGABRT expected)
-    SOPC_HighRes_TimeReference_GetTime(t);
-    ck_abort_msg("SOPC_ASSERT was not triggered even though clock_gettime failed");
-
-    g_fail_clock_gettime = false;
-    SOPC_HighRes_TimeReference_Delete(&t);
-}
-END_TEST
-
-/*
- * Testing the SOPC_HighRes_TimeReference_SleepUntil() function.
- * Verify that the function triggers a SOPC_ASSERT if a NULL value is passed as a parameter.
- */
-START_TEST(test_highRes_timeReference_sleepUntil_sopcAssert)
-{
-    SOPC_HighRes_TimeReference_SleepUntil(NULL);
+    SOPC_HighRes_TimeReference_Delete(&tPast);
 }
 END_TEST
 
@@ -421,11 +387,6 @@ int main(void)
     tcase_add_test(tc_highres, test_highRes_timeReference_isExpired);
     tcase_add_test(tc_highres, test_highRes_timeReference_sleepUntil);
     suite_add_tcase(suite, tc_highres);
-
-    TCase* tc_highres_assert = tcase_create("HighResTimeReferenceSOPCAssert");
-    tcase_add_exit_test(tc_highres_assert, test_highRes_timeReference_getTime_sopcAssert, EXIT_FAILURE);
-    tcase_add_test_raise_signal(tc_highres_assert, test_highRes_timeReference_sleepUntil_sopcAssert, SIGABRT);
-    suite_add_tcase(suite, tc_highres_assert);
 
     srunner_run_all(sr, CK_NORMAL);
     number_failed = srunner_ntests_failed(sr);
